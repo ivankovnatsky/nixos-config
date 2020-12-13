5b5453ea627e99c97a98fe6768c83e47686e8120
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2020-12-13 20:32:37 +0200

    System: st: font name to upper case

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "hack nerd font mono:size=10:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font Mono:size=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
