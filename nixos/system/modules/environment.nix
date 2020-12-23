86d77cb372146d03ba5ff9f2df232c6fea41deba
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2020-12-23 16:09:55 +0200

    nixos/system/modules/environment.nix: Move gtk config to ~/

--- nixos/system/modules/environment.nix
+++ nixos/system/modules/environment.nix
@@ -8,20 +8,5 @@
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
   };
 
-  environment.etc."xdg/gtk-2.0/gtkrc".text = ''
-    gtk-application-prefer-dark-theme=true
-    gtk-xft-antialias=1
-    gtk-xft-hinting=1
-    gtk-xft-hintstyle=hintslight
-  '';
-
-  environment.etc."xdg/gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-application-prefer-dark-theme=true
-    gtk-xft-antialias=1
-    gtk-xft-hinting=1
-    gtk-xft-hintstyle=hintslight
-  '';
-
   environment.shellAliases = { tg = "terragrunt"; };
 }
