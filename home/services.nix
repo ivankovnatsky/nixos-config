a71967b8c6bf627ecbd221b172294fbe736034b2
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-02-09 10:24:12 +0200

    Cleanup redshift config

--- home/services.nix
+++ home/services.nix
@@ -6,12 +6,6 @@
       enable = true;
       provider = "geoclue2";
 
-      brightness = {
-        # Note the string values below.
-        day = "1";
-        night = "1";
-      };
-
       temperature = {
         day = 5500;
         night = 3700;
