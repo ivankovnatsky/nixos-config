8a65a35f0f89f5ecccf5768fbb511e0a2783935e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-02-15 20:30:40 +0200

    Add modifier variable to i3 config

--- home/pkgs/i3.nix
+++ home/pkgs/i3.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
 let
+  modifier = "Mod4";
+
   fontName = "Font Awesome 5 Free";
   blackColorHTML = "#000000";
   whiteColorHTML = "#ffffff";
@@ -26,14 +28,17 @@ in {
         };
       };
 
+      modifier = "Mod4";
+
       keybindings = {
         "Mod1+Control+Shift+4" =
           ''exec --no-startup-id "maim -s | xclip -sel c -t image/png"'';
 
-        "Mod4+Control+Mod1+Shift+a" = "exec --no-startup-id autorandr all";
-        "Mod4+Control+Mod1+Shift+l" =
+        "${modifier}+Control+Mod1+Shift+a" =
+          "exec --no-startup-id autorandr all";
+        "${modifier}+Control+Mod1+Shift+l" =
           "exec --no-startup-id autorandr laptop && xrandr --dpi 142 && i3-msg restart";
-        "Mod4+Control+Mod1+Shift+m" =
+        "${modifier}+Control+Mod1+Shift+m" =
           "exec --no-startup-id autorandr monitor && xrandr --dpi 192 && i3-msg restart";
 
         "XF86MonBrightnessDown" =
@@ -55,27 +60,27 @@ in {
         "XF86AudioMicMute" =
           "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle";
 
-        "Mod4+Shift+Return" = "exec alacritty -e tmuxinator start home";
-        "Mod4+p" = "exec rofi -show run";
+        "${modifier}+Shift+Return" = "exec alacritty -e tmuxinator start home";
+        "${modifier}+p" = "exec rofi -show run";
 
-        "Mod4+j" = "focus left";
-        "Mod4+k" = "focus right";
+        "${modifier}+j" = "focus left";
+        "${modifier}+k" = "focus right";
 
-        "Mod4+w" = "layout tabbed";
-        "Mod4+e" = "layout toggle split";
+        "${modifier}+w" = "layout tabbed";
+        "${modifier}+e" = "layout toggle split";
 
-        "Mod4+1" = "workspace number 1";
-        "Mod4+2" = "workspace number 2";
-        "Mod4+3" = "workspace number 3";
-        "Mod4+4" = "workspace number 4";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
 
-        "Mod4+Shift+1" = "move container to workspace number 1";
-        "Mod4+Shift+2" = "move container to workspace number 2";
-        "Mod4+Shift+3" = "move container to workspace number 3";
-        "Mod4+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
 
-        "Mod4+Shift+c" = "kill";
-        "Mod4+Shift+Control+r" = "restart";
+        "${modifier}+Shift+c" = "kill";
+        "${modifier}+Shift+Control+r" = "restart";
 
         "Mod1+Tab" = "workspace back_and_forth";
       };
