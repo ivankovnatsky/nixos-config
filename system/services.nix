6258f58df8f37f8a8911ee1a836c3cea3390eb79
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-02-17 18:16:57 +0200

    Fix formatting

--- system/services.nix
+++ system/services.nix
@@ -19,8 +19,8 @@
 
     tlp = {
       enable = true;
-      settings = {
 
+      settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
