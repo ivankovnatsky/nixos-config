da4833db62eb0e7d5b3056a9349635358ab4cacf
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-03-13 12:48:28 +0200

    Move general configs to separate file

---
+++ system/general.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  documentation.enable = false;
+  i18n.defaultLocale = "en_US.UTF-8";
+  security.sudo.wheelNeedsPassword = false;
+  sound.enable = true;
+  time.timeZone = "Europe/Kiev";
+
+  networking = {
+    useDHCP = false;
+
+    networkmanager = {
+      enable = true;
+      wifi.powersave = true;
+    };
+  };
+
+  users.users.ivan = {
+    description = "Ivan Kovnatsky";
+    isNormalUser = true;
+    home = "/home/ivan";
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "docker" "networkmanager" ];
+  };
+
+  fonts = {
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = [ "Hack" ]; })
+      font-awesome
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-extra
+    ];
+  };
+}
