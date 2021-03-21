79456d6804118b1e2216b29e048f5cd7eff8cd45
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-03-21 11:48:32 +0200

    Add chromium flags feature option

--- system/packages.nix
+++ system/packages.nix
@@ -4,7 +4,10 @@
   environment.systemPackages = with pkgs; [
     clinfo
     docker
-    (chromium.override { commandLineArgs = "--force-dark-mode"; })
+    (chromium.override {
+      commandLineArgs =
+        "--force-dark-mode --flag-switches-begin --enable-features=ReaderMode,HardwareAccelerated,Vulkan,NativeNotifications --flag-switches-end";
+    })
     networkmanagerapplet
     networkmanager-l2tp
     vulkan-tools
