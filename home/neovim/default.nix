e6322212e866f2b5ab2ab45109d476972dbfe1f9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-04-30 13:00:11 +0300

    Make vim-airline separators vertical

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -53,6 +53,8 @@
         config = ''
           let g:airline#extensions#branch#enabled=1
           let g:airline_powerline_fonts = 1
+          let g:airline_left_sep=' '
+          let g:airline_right_sep=' '
         '';
       }
 
