57e5bed76841bd73bd55a72b521d29b4709dc969
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-05-12 22:38:34 +0300

    Clean xdg-desktop-portal-wlr overlay with slurp

--- system/graphics.nix
+++ system/graphics.nix
@@ -1,21 +1,6 @@
 { lib, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      xdg-desktop-portal-wlr = super.xdg-desktop-portal-wlr.overrideAttrs
-        (oldAttrs: rec {
-          nativeBuildInputs = oldAttrs.nativeBuildInputs
-            ++ [ pkgs.makeWrapper ];
-          postInstall = ''
-            wrapProgram $out/libexec/xdg-desktop-portal-wlr --prefix PATH ":" ${
-              lib.makeBinPath [ pkgs.slurp ]
-            }
-          '';
-        });
-    })
-  ];
-
   xdg = {
     icons.enable = true;
 
