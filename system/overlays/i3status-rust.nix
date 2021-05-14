b525b68c1a1400dc88f4dd5e9066659b25c39e85
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-05-14 10:38:46 +0300

    Switch to i3status-rust upstream

--- system/overlays/i3status-rust.nix
+++ system/overlays/i3status-rust.nix
@@ -3,22 +3,33 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "i3status-rust";
-  version = "6bc6c141f37ac349512a0586987232977ade010e";
+  version = "1310907c98d160f8c23d3bd6091b68d91fa8a114";
 
   src = fetchFromGitHub {
-    owner = "GladOSkar";
+    owner = "greshake";
     repo = pname;
     rev = "${version}";
-    sha256 = "sha256-i5nu56smivvu4fkSu6OnfOeScy1XdLvhxsIgvc4KT04=";
+    sha256 = "sha256-MGgVIAp1X3wBgfPCuvcsDR0VG8YcvHap5aktOW5oTa8=";
   };
 
-  cargoSha256 = "sha256-wh8LzfHobEDJnnf/SKF86GWvqlmnnp0mayA8BiuE1SQ=";
+  cargoSha256 = "sha256-hnaVv378YriHS2THUwAh1nUNJ0ijna28N/ktQWkEcsI=";
 
   nativeBuildInputs = [ pkg-config makeWrapper ];
 
   buildInputs = [ dbus libpulseaudio notmuch openssl ];
 
-  cargoBuildFlags = [ "--features=notmuch" "--features=maildir" ];
+  cargoBuildFlags =
+    [ "--features=notmuch" "--features=maildir" "--features=pulseaudio" ];
+
+  prePatch = ''
+    substituteInPlace src/util.rs \
+      --replace "/usr/share/i3status-rust" "$out/share"
+  '';
+
+  postInstall = ''
+    mkdir -p $out/share
+    cp -R files/* $out/share
+  '';
 
   postFixup = ''
     wrapProgram $out/bin/i3status-rs --prefix PATH : "${ethtool}/bin"
