845e2fe796fc95bbb954cfa527b847f4a3296872
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-05-18 17:05:51 +0300

    Move to flakes

--- system/configuration.nix
+++ system/configuration.nix
@@ -13,40 +13,7 @@
     ./packages.nix
     ./programs.nix
     ./services.nix
-
-    <home-manager/nixos>
   ];
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball
-      "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-        inherit pkgs;
-      };
-  };
-
-  home-manager.useGlobalPkgs = true;
-  home-manager.users.ivan = { ... }: {
-    imports = [
-      ../home/general.nix
-
-      ../home/neovim/default.nix
-
-      ../home/programs.nix
-
-      ../home/alacritty.nix
-      ../home/firefox.nix
-      ../home/git.nix
-      ../home/gtk.nix
-      ../home/i3status.nix
-      ../home/tmux.nix
-      ../home/zsh.nix
-
-      ../home/sway.nix
-      ../home/mako.nix
-    ];
-
-    home.stateVersion = config.system.stateVersion;
-  };
 
   nixpkgs.overlays = [ (import ./overlays/default.nix) ];
 
