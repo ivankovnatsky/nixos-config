7935ca5420cf069b8f75566c8f8dbe448b6e721d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-05-18 17:05:51 +0300

    Move to flakes

---
+++ system/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    nur.url = "github:nix-community/NUR";
+  };
+
+  outputs = { home-manager, nixpkgs, nur, ... }: {
+    nixosConfigurations = {
+      thinkpad = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./configuration.nix
+          { nixpkgs.overlays = [ nur.overlay ]; }
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ivan = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ system/home.nix
@@ -0,0 +1,20 @@
+{
+  imports = [
+    ../home/general.nix
+
+    ../home/neovim/default.nix
+
+    ../home/programs.nix
+
+    ../home/alacritty.nix
+    ../home/firefox.nix
+    ../home/git.nix
+    ../home/gtk.nix
+    ../home/i3status.nix
+    ../home/tmux.nix
+    ../home/zsh.nix
+
+    ../home/sway.nix
+    ../home/mako.nix
+  ];
+}
