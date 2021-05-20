32e1e0b4ec3991a61daef6908d97c7d5384e3a72
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-05-20 19:14:10 +0300

    Fix formatting for git config

--- home/git.nix
+++ home/git.nix
@@ -4,9 +4,16 @@
   programs.git = {
     enable = true;
 
+    includes = [{ path = "~/.config/git/config-home"; }];
+
     extraConfig = {
       commit.gpgsign = true;
       init.defaultBranch = "main";
+      merge.tool = "nvim";
+      mergetool."nvim".cmd = ''nvim -f -c "Gdiffsplit!" "$MERGED"'';
+      pull.rebase = false;
+      push.default = "current";
+
       credential.helper = "${
           pkgs.git.override { withLibsecret = true; }
         }/bin/git-credential-libsecret";
@@ -16,19 +23,10 @@
         filemode = true;
       };
 
-      include.path = "~/.config/git/config-home";
-
-      merge.tool = "nvim";
-
       mergetool = {
         keepBackup = false;
         prompt = true;
       };
-
-      mergetool."nvim".cmd = ''nvim -f -c "Gdiffsplit!" "$MERGED"'';
-
-      pull.rebase = false;
-      push.default = "current";
     };
   };
 
