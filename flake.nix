0aefe9e592aa3b7043daecaaf17b3e71fe7e20e9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-05-21 20:41:59 +0300

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nur.url = "github:nix-community/NUR";
   };
 
