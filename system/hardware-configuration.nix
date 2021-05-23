04bb02daaafdf48954e6792fa082a7e2b03ba9d1
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-05-23 14:43:14 +0300

    Use kernel 5.13-rc2

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -27,7 +27,19 @@
     kernelModules = [ "dm-snapshot" "kvm-amd" "amdgpu" ];
     kernelParams = [ "amd_iommu=pt" "iommu=soft" "acpi_backlight=native" ];
 
-    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_testing.override {
+      argsOverride = rec {
+        src = pkgs.fetchurl {
+          url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
+          sha256 = "sha256-NGc86oj35TmbcEvKzAiLPxT1gA2t8Er2qQsZw1rTd38=";
+        };
+        version = "5.13-rc2";
+        modDirVersion = "5.13.0-rc2";
+      };
+
+      ignoreConfigErrors = true;
+    });
 
     extraModprobeConfig = ''
       # idle audio card after one second
