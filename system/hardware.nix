1814e554abf1a0ed424f0149e29e55322c9ebabb
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-05-24 09:50:32 +0300

    Disable pulseaudio

--- system/hardware.nix
+++ system/hardware.nix
@@ -15,19 +15,10 @@
       powerOnBoot = false;
     };
 
-    pulseaudio = {
-      enable = true;
-      extraModules = [ pkgs.pulseaudio-modules-bt ];
-      package = pkgs.pulseaudioFull;
-    };
-
     video.hidpi.enable = true;
   };
 
   hardware.bluetooth.settings = {
     General = { Enable = "Source,Sink,Media,Socket"; };
   };
-
-  hardware.pulseaudio.extraConfig =
-    "\n  load-module module-switch-on-connect\n";
 }
