cd8334eb83f847a64e08f8eeef9847b9d119d66f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-06-02 11:29:25 +0300

    Add dashboards to grafana

--- system/monitoring.nix
+++ system/monitoring.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   services = {
     grafana = {
@@ -12,6 +14,24 @@
           isDefault = true;
           url = "http://localhost:9090";
         }];
+
+        dashboards = [
+          {
+            name = "Node Exporter Full";
+            options.path = pkgs.fetchurl {
+              url = "https://raw.githubusercontent.com/rfrail3/grafana-dashboards/3072e63640a8b026e2f62da5d2d53fb3dcfb0686/prometheus/node-exporter-full.json";
+              sha256 = "sha256-FmkCRS9E5RSHcSZvCIH2iKH773G6DQTTJQYUwnmw+BA=";
+            };
+          }
+
+          {
+            name = "Node Exporter Battery";
+            options.path = pkgs.fetchurl {
+              url = "https://gist.githubusercontent.com/ivankovnatsky/998ab1ba6a437784c4f3517daf1148aa/raw/57197452ac707da0e86ca37469b456390e068018/battery-dashboard.json";
+              sha256 = "sha256-8d1QytmZH++k11qedgbpWEl2xAPiTAn9re1t+yTtcog=";
+            };
+          }
+        ];
       };
     };
 
