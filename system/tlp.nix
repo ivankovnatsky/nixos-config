a53ba46cec6049eeeacba1c1f85a191524abba4c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-06-06 10:49:18 +0300

    Cleanup tlp battery and sound settings

--- system/tlp.nix
+++ system/tlp.nix
@@ -7,21 +7,6 @@
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        # The following prevents the battery from charging fully to
-        # preserve lifetime. Run `tlp fullcharge` to temporarily force
-        # full charge.
-        # https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
-        START_CHARGE_THRESH_BAT0 = 40;
-        STOP_CHARGE_THRESH_BAT0 = 50;
-
-        # 100 being the maximum, limit the speed of my CPU to reduce
-        # heat and increase battery usage:
-        CPU_MAX_PERF_ON_AC = 70;
-        CPU_MAX_PERF_ON_BAT = 30;
-
-        SOUND_POWER_SAVE_ON_AC = 0;
-        SOUND_POWER_SAVE_ON_BAT = 1;
-
         CONTROL_BRIGHTNESS = 1;
         BATT_BRIGHTNESS_COMMAND = "brightnessctl set 20%";
         LM_AC_BRIGHTNESS_COMMAND = "brightnessctl set 35%";
