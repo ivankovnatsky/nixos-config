5f528b3dff5cf6383be11ad9eeafb8ab00a4ad8f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-06-14 13:17:40 +0300

    Split programs into it's own file

---
+++ home/mpv.nix
@@ -0,0 +1,21 @@
+{
+  programs.mpv = {
+    enable = true;
+    config = {
+      "alang" = "eng";
+      "force-seekable" = "yes";
+      "fs" = "yes";
+      "hwdec" = "yes";
+      "opengl-pbo" = "yes";
+      "osc" = "no";
+      "osd-level" = "0";
+      "save-position-on-quit" = "yes";
+      "slang" = "eng";
+      "ytdl-format" = "bestvideo+bestaudio/best";
+      "image-display-duration" = "5";
+      "vo" = "gpu";
+      "profile" = "gpu-hq";
+      "gpu-context" = "wayland";
+    };
+  };
+}
---
+++ home/password-store.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+
+{
+  programs.password-store = {
+    enable = true;
+    package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
+  };
+}
