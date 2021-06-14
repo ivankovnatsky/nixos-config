2943e8757fed2f8ae66401c98f16f83350951d43
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-06-14 13:29:31 +0300

    Move generic nix config to a file

--- system/nix.nix
+++ system/nix.nix
@@ -4,12 +4,6 @@
   nixpkgs.config.allowUnfree = true;
 
   nix = {
-    autoOptimiseStore = true;
-    gc.automatic = true;
-    optimise.automatic = true;
-
-    useSandbox = false;
-
     package = pkgs.nixFlakes;
     extraOptions = ''
       experimental-features = nix-command flakes
