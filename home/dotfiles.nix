6f877f492ba7ded55b140cc4f332b0ef11f9ccc1
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-06-14 13:26:24 +0300

    Move dotfiles configs to a separate file

---
+++ home/dotfiles.nix
@@ -0,0 +1,15 @@
+{
+  home.file = {
+    ".terraform.d/plugin-cache/.keep" = {
+      text = ''
+        keep
+      '';
+    };
+
+    ".terraformrc" = {
+      text = ''
+        plugin_cache_dir = "$HOME/.terraform.d/plugin-cache"
+      '';
+    };
+  };
+}
--- home/general.nix
+++ home/general.nix
@@ -14,18 +14,4 @@
       };
     };
   };
-
-  home.file = {
-    ".terraform.d/plugin-cache/.keep" = {
-      text = ''
-        keep
-      '';
-    };
-
-    ".terraformrc" = {
-      text = ''
-        plugin_cache_dir = "$HOME/.terraform.d/plugin-cache"
-      '';
-    };
-  };
 }
