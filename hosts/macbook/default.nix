0635ed0692ca2f89a0cdcb9a680404da9ef3cdb1
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-06-21 21:36:36 +0300

    Move some common config to flake

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -2,9 +2,7 @@
 
 {
   imports = [
-    ../../system/environment.nix
     ../../system/homebrew.nix
-    ../../system/nix.nix
     ../../system/packages.nix
     ../../system/packages-darwin.nix
   ];
