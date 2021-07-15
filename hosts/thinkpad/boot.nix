28a95afc236254ebae987ece36e8e5967ba3074b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-07-15 13:39:36 +0300

    Set kernel version to 5.14-rc1

--- hosts/thinkpad/boot.nix
+++ hosts/thinkpad/boot.nix
@@ -18,29 +18,18 @@
       "acpi_osi=linux"
     ];
 
-    # kernelPackages = pkgs.linuxPackages_latest;
     kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_testing.override {
       argsOverride = rec {
         src = pkgs.fetchurl {
           url = "https://git.kernel.org/stable/t/linux-${version}.tar.gz";
-          sha256 = "sha256-IeEqcNhSc5NlkbZabk5ai0eLUO0PPIOobJIeZoS54AU=";
+          sha256 = "sha256-W8u6Y9O2Hokp/Q156yuwU/HfMHeO/nYTn8digcrSlyQ=";
         };
-        version = "5.13.1";
-        modDirVersion = "5.13.1";
+        version = "5.14-rc1";
+        modDirVersion = "5.14.0-rc1";
       };
 
       ignoreConfigErrors = true;
     });
-
-    kernelPatches = [
-      {
-        name = "5.13.1-S0ix-AMD-all-in-one";
-        patch = builtins.fetchurl {
-          url = "https://crazy.dev.frugalware.org/5.13.1-S0ix-AMD-all-in-one.patch";
-          sha256 = "1b5wkhcpllsgbi4cv8bh63a2k4m5cbg9l4ll99k1fysa3bafnk06";
-        };
-      }
-    ];
 
     extraModprobeConfig = ''
       # idle audio card after one second
