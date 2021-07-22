5d5fc81009e37324fcc9cca01d04dd10983c9f72
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-07-22 11:18:58 +0300

    Use nixos terraform

--- system/overlays/linux/default.nix
+++ system/overlays/linux/default.nix
@@ -1,11 +1,4 @@
 self: super: {
   inherit (super.callPackages ../openvpn.nix { })
     openvpn;
-
-  terraform = self.callPackage ../hashicorp-generic.nix {
-    name = "terraform";
-    version = "0.15.5";
-    sha256 = "sha256-OxREmeCMJFqAOQJ+srhMBJXhGfV9eej7YFhku0iJen0=";
-    system = "x86_64-linux";
-  };
 }
