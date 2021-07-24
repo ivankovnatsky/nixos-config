1c6bc11b4ec74c8e3509604e05f02f04cb0cf647
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-07-24 22:07:47 +0300

    Move gammastep to a separate file

--- home/general.nix
+++ home/general.nix
@@ -3,15 +3,5 @@
 
   services = {
     gpg-agent.enable = true;
-
-    gammastep = {
-      enable = true;
-      provider = "geoclue2";
-
-      temperature = {
-        day = 5500;
-        night = 3700;
-      };
-    };
   };
 }
