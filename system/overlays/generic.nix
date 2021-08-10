2a11d96512e19232b7ab9df9764f02b7a7c4000b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-08-10 08:06:27 +0300

    Add awscurl

---
+++ system/overlays/generic.nix
@@ -0,0 +1,31 @@
+{ name
+, owner
+, repo
+, version
+, platform
+, sha256
+
+, lib
+, stdenv
+, fetchurl
+, unzip
+, ...
+}:
+
+let
+  url = "https://github.com/${owner}/${repo}/releases/download/v${version}/${repo}_${version}_${platform}.zip";
+
+in
+stdenv.mkDerivation {
+  inherit name version;
+  src = fetchurl { inherit url sha256; };
+
+  # Our source is right where the unzip happens, not in a "src/" directory (default)
+  sourceRoot = ".";
+
+  nativeBuildInputs = [ unzip ];
+  installPhase = ''
+    mkdir -p $out/bin
+    mv ${name} $out/bin
+  '';
+}
--- system/packages.nix
+++ system/packages.nix
@@ -3,6 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     awscli2
+    awscurl
     aws-vault
     dnsutils
     docker-compose
