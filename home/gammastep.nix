3beae66a739c35689ff5682b1003137267e59557
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-08-11 08:58:30 +0300

    Move longitude and latitude to secrets

--- home/gammastep.nix
+++ home/gammastep.nix
@@ -1,9 +1,13 @@
+let
+  latitude = builtins.readFile ../.secrets/personal/latitude;
+  longitude = builtins.readFile ../.secrets/personal/longitude;
+in
 {
   services = {
     gammastep = {
       enable = true;
-      latitude = "50.45";
-      longitude = "30.52";
+      latitude = latitude;
+      longitude = longitude;
 
       temperature = {
         day = 5500;
