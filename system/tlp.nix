f34604f5d1b9510f0104638b27c3ef7520f304ae
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-08-22 16:46:57 +0300

    Cleanup brightness control for tlp

--- system/tlp.nix
+++ system/tlp.nix
@@ -6,13 +6,6 @@
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CONTROL_BRIGHTNESS = 1;
-        BATT_BRIGHTNESS_COMMAND = "brightnessctl set 20%";
-        LM_AC_BRIGHTNESS_COMMAND = "brightnessctl set 35%";
-        NOLM_AC_BRIGHTNESS_COMMAND =
-          "brightnessctl set 35%";
-        BRIGHTNESS_OUTPUT = "/sys/class/backlight/amdgpu_bl0/brightness";
       };
     };
   };
