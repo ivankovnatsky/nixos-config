e700a211d61cec68237d1a8e88be9b25fb6ad9c5
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-08-29 12:06:04 +0300

    Set default values to thinkpads in device module

--- modules/device.nix
+++ modules/device.nix
@@ -11,7 +11,7 @@ with lib; {
     cpuTempPattern = mkOption {
       type = types.enum [ "Package id 0" "CPU" ];
       description = "Type of CPU Temp search pattern in lensors command";
-      default = "Package id 0";
+      default = "CPU";
     };
 
     monitorName = mkOption {
