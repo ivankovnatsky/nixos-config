e1d3061737b4ec3c5651f53766e53c2f4d162f20
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-09-27 14:39:34 +0300

    Remove vim-sneak plugin

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -18,17 +18,6 @@
       nerdtree-git-plugin
       toggleterm-nvim
 
-      {
-        plugin = vim-sneak;
-        config = ''
-          let g:sneak#label = 1
-          map f <Plug>Sneak_f
-          map F <Plug>Sneak_F
-          map t <Plug>Sneak_t
-          map T <Plug>Sneak_T
-        '';
-      }
-
       vim-surround
       vim-tmux
       vim-visualstar
