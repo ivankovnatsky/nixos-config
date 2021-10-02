c273bfeab8430d32d8bef5de36ae6b846fa004b6
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-10-02 08:53:01 +0300

    Apply formatting to hosts/xps/hardware-configuration.nix

--- hosts/xps/hardware-configuration.nix
+++ hosts/xps/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
@@ -14,18 +15,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/48c9e3e2-b13c-446e-94a5-e2b6f8735856";
+    {
+      device = "/dev/disk/by-uuid/48c9e3e2-b13c-446e-94a5-e2b6f8735856";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4905-1042";
+    {
+      device = "/dev/disk/by-uuid/4905-1042";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/22fde164-d7fd-4bee-a7d0-569560555c08"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/22fde164-d7fd-4bee-a7d0-569560555c08"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
