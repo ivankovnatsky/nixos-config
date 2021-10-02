248f4debded9dd6f650e5156010399f2df97e925
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-10-02 11:03:09 +0300

    Move swaylock to a separate file

--- system/security.nix
+++ system/security.nix
@@ -1,7 +1,6 @@
 {
   security = {
     rtkit.enable = true;
-    pam.services.swaylock = { };
     sudo.configFile = ''
       Defaults timestamp_timeout=240
     '';
---
+++ system/swaylock.nix
@@ -0,0 +1,5 @@
+{
+  security = {
+    pam.services.swaylock = { };
+  };
+}
