425672d56c572145a6f9d4f3d84832cfa3f23261
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-10-30 10:36:32 +0300

    Move xorg dpi config to a separate file

--- system/xserver.nix
+++ system/xserver.nix
@@ -1,13 +1,6 @@
 { config, ... }:
 
 {
-  environment.variables = {
-    GDK_SCALE = "2";
-    GDK_DPI_SCALE = "0.5";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
-  };
-
   services = {
     xserver = {
       enable = true;
