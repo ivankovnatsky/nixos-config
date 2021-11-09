583379230f56bee54d37a99b7c87535f61fa45c4
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-11-09 14:31:04 +0200

    Correct tz

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
           ];
 
           i18n.defaultLocale = "en_US.UTF-8";
-          time.timeZone = "Europe/Uzhgorod";
+          time.timeZone = "Europe/Kiev";
           sound.enable = true;
 
           programs = {
