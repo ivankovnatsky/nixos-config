5f30d91a884b87dfaf610b3cb9efc0baafaba25d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-11-15 13:57:55 +0200

    Revert "Add yaml-language-server"
    This reverts commit e6ab5728ce17779eee6418a1cefd9025cd894ca2.
    
    https://github.com/redhat-developer/yaml-language-server/issues/220

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -110,7 +110,7 @@
 
           -- Use a loop to conveniently call 'setup' on multiple servers and
           -- map buffer local keybindings when the language server attaches
-          local servers = { 'bashls', 'pyright', 'rnix', 'terraformls', 'yamlls' }
+          local servers = { 'bashls', 'pyright', 'rnix', 'terraformls' }
           for _, lsp in ipairs(servers) do
             nvim_lsp[lsp].setup {
               on_attach = on_attach,
