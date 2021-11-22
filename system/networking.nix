4b9fa109d899c07bba8fdf2f5d64d24282aff244
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-11-22 10:19:15 +0200

    Move iwd config to separate file

--- system/networking.nix
+++ system/networking.nix
@@ -2,13 +2,10 @@
   networking = {
     useDHCP = false;
     networkmanager.enableStrongSwan = true;
-    wireless.iwd.enable = true;
-    wireless.iwd.settings.General.UseDefaultInterface = true;
 
     networkmanager = {
       enable = true;
 
-      wifi.backend = "iwd";
       wifi.powersave = true;
     };
   };
