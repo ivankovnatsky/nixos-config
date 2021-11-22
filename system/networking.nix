4416a08044152e88bd7cd72665ab67f3ee54c98d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-11-22 07:10:03 +0200

    Add fix for iwd issue

--- system/networking.nix
+++ system/networking.nix
@@ -3,6 +3,7 @@
     useDHCP = false;
     networkmanager.enableStrongSwan = true;
     wireless.iwd.enable = true;
+    wireless.iwd.settings.General.UseDefaultInterface = true;
 
     networkmanager = {
       enable = true;
