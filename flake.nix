585233cba2439f80ebd5c70a33c6ca6bdbfbb00e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-11-26 11:48:06 +0200

    Cleanup xdg-desktop-portal overlay call

--- flake.nix
+++ flake.nix
@@ -165,12 +165,6 @@
                 nixpkgs.overlays = [
                   inputs.self.overlay
                   inputs.nur.overlay
-
-                  (
-                    self: super: {
-                      xdg-desktop-portal = super.callPackage ./overlays/xdg-desktop-portal.nix { };
-                    }
-                  )
                 ];
 
                 system.stateVersion = "21.03";
