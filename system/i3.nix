8897573f2b9e273b3f0018d93943c8c47115a0ad
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-12-02 13:23:32 +0200

    Move generate xserver config to xserver file

--- system/i3.nix
+++ system/i3.nix
@@ -2,16 +2,7 @@
   services = {
     xserver = {
       windowManager.i3.enable = true;
-
-      displayManager = {
-        lightdm.enable = true;
-        defaultSession = "none+i3";
-
-        autoLogin = {
-          enable = true;
-          user = "ivan";
-        };
-      };
+      defaultSession = "none+i3";
     };
   };
 }
