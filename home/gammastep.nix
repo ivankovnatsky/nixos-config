917619706811c26dd789f67ed5f26e872b14703c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-12-08 17:19:39 +0200

    Cleanup gammastep settings

--- home/gammastep.nix
+++ home/gammastep.nix
@@ -6,11 +6,6 @@
       latitude = "49.80,";
       longitude = "29.98";
 
-      settings = {
-        # otherwise causes funky color tilt
-        general = { fade = 0; };
-      };
-
       temperature = {
         day = 5500;
         night = 3700;
