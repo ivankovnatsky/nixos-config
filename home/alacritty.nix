567e8bf286ab230b97bbefa98787500e32c7694a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2021-12-24 20:44:18 +0200

    Clean up alacritty cursor colors

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -25,11 +25,6 @@ in
       selection.save_to_clipboard = true;
       colors.primary.background = "#000000";
 
-      colors.cursor = {
-        text = "#FFFFFF";
-        cursor = "#FF7F50";
-      };
-
       # does not work, until implemeted: https://github.com/alacritty/alacritty/pull/4322
       live_config_reload = true;
     };
