084a7127c70a407ba9122da0e72133bcb1859fcb
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-02-07 16:51:18 +0200

    Set separate key binding for vim-sneak
    This is to differentiate between common f/t/F/T

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -79,10 +79,10 @@
         plugin = vim-sneak;
         config = ''
           let g:sneak#label = 1
-          map f <Plug>Sneak_f
-          map F <Plug>Sneak_F
-          map t <Plug>Sneak_t
-          map T <Plug>Sneak_T
+          map ff <Plug>Sneak_f
+          map FF <Plug>Sneak_F
+          map tt <Plug>Sneak_t
+          map TT <Plug>Sneak_T
         '';
       }
 
