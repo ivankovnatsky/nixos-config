c790d5ed893e2c415f096613a87ba68b765bf95f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-02-07 13:32:31 +0200

    Revert "Remove vim-sneak plugin"
    This reverts commit ec63d8413b38781e5d7dbf04dddde5483b8826a0.

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -74,6 +74,18 @@
       vim-lastplace
       vim-repeat
       vim-sensible
+
+      {
+        plugin = vim-sneak;
+        config = ''
+          let g:sneak#label = 1
+          map f <Plug>Sneak_f
+          map F <Plug>Sneak_F
+          map t <Plug>Sneak_t
+          map T <Plug>Sneak_T
+        '';
+      }
+
       vim-surround
       vim-tmux
       vim-visualstar
