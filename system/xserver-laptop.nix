82a937f0943334ad75935d6102d25368f673cdd5
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-03-14 17:08:23 +0200

    Split xserver laptop into monitor/laptop config

--- system/xserver-laptop.nix
+++ system/xserver-laptop.nix
@@ -1,10 +1,3 @@
-{ config, ... }:
-
-let
-  laptopName = if config.device.name == "xps" then "eDP-1" else "eDP";
-  monitorName = if config.device.name == "xps" then "DP-3" else "DisplayPort-1";
-
-in
 {
   services = {
     xserver = {
@@ -15,25 +8,9 @@ in
           additionalOptions = ''MatchIsTouchpad "on"'';
           disableWhileTyping = true;
           naturalScrolling = true;
-          tapping = false;
+          tapping = true;
         };
       };
-
-      extraConfig = ''
-        Section "Monitor"
-          Identifier "${laptopName}"
-          Option "RightOf" "${monitorName}"
-          Option "Position" "3840 960"
-          Option "DPMS" "true"
-        EndSection
-        Section "Monitor"
-          Identifier "${monitorName}"
-          Option "PreferredMode" "3840x2160"
-          Option "Position" "0 0"
-          Option "LeftOf" "${laptopName}"
-          Option "DPMS" "true"
-        EndSection
-      '';
     };
   };
 }
