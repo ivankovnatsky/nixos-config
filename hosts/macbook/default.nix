64181a7c8681412481e8577a2489bdb4b48dbe0a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-06-01 12:50:48 +0300

    Remove lima

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -14,7 +14,6 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    lima
     gnupg
     syncthing
   ];
