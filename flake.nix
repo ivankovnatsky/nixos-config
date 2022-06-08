a3634a2f1dd3b9a1567fde6f97d9a1e28c62bd56
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-06-08 11:38:12 +0300

    Place nur input after platform sections

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nur.url = "github:nix-community/NUR";
 
     # Mac
     nixpkgs-mac.url = "github:nixos/nixpkgs?rev=cbe587c735b734405f56803e267820ee1559e6c1";
@@ -14,6 +13,8 @@
     home-manager-mac.inputs.nixpkgs.follows = "nixpkgs-mac";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs-mac";
+
+    nur.url = "github:nix-community/NUR";
   };
 
   outputs = inputs:
