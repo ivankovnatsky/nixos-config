cdc60382c93c3e06f5bdc25999d7e2789154f401
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-07-05 10:42:20 +0300

    Update mac-related flake inputs

--- flake.nix
+++ flake.nix
@@ -69,6 +69,8 @@
 
                   ./hosts/${hostname}/home.nix
                 ] ++ homeModules;
+
+                home.stateVersion = "22.05";
               };
 
               home-manager.extraSpecialArgs = {
