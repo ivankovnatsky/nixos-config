f77ee7a2bc0c56681c9420e0337a201f16405b8b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-08-25 13:04:25 +0300

    Move boot to dedicated hosts config

--- hosts/desktop/boot.nix
+++ hosts/desktop/boot.nix
@@ -2,6 +2,17 @@
 
 {
   boot = {
+    loader = {
+      timeout = 1;
+
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 5;
+      };
+
+      efi.canTouchEfiVariables = true;
+    };
+
     initrd = {
       luks.devices.crypted = {
         device = "/dev/disk/by-uuid/ba92c270-5d3c-4681-904d-61b8fe48ef73";
