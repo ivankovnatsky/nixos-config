715092e1d71166a9736aa56a386c91c69b5b8508
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-09-08 12:22:36 +0300

    Add Finder and Messages for dockutil/hammerspoon

--- home/dockutil.nix
+++ home/dockutil.nix
@@ -5,5 +5,7 @@
     ${pkgs.dockutil}/bin/dockutil --remove all
     ${pkgs.dockutil}/bin/dockutil --add "/Applications/iTerm.app"
     ${pkgs.dockutil}/bin/dockutil --add "/Applications/Firefox.app"
+    ${pkgs.dockutil}/bin/dockutil --add "/Applications/Safari.app"
+    ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/Messages.app"
   '';
 }
