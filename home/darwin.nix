31760cd101201e22c71acb633964a0ae77b45461
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-09-18 10:39:57 +0300

    Move custom configs to home-manager manual bash one-liners

--- home/darwin.nix
+++ home/darwin.nix
@@ -2,4 +2,23 @@
   imports = [
     ./dockutil.nix
   ];
+
+  home.file = {
+    ".firefox/config".text = ''
+      mkdir -p /Applications/Firefox.app/Contents/Resources/distribution/
+      cat > /Applications/Firefox.app/Contents/Resources/distribution/policies.json << EOF
+      {
+        "policies": {
+          "DisableAppUpdate": true
+        }
+      }
+      EOF
+    '';
+
+    ".sudo/config".text = ''
+      sudo bash -c 'cat << EOF > /etc/sudoers.d/default
+      Defaults timestamp_timeout=240
+      EOF'
+    '';
+  };
 }
