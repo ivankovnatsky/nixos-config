c4977a3837cb0967d0a1ad8d410cf8effc89097f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-10-04 11:11:59 +0300

    Re-arrange flake inputs

--- flake.nix
+++ flake.nix
@@ -4,17 +4,20 @@
   inputs = {
     # Linux
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    nur.url = "github:nix-community/NUR";
+
     # Mac
     nixpkgs-mac.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager-mac.url = "github:nix-community/home-manager";
     home-manager-mac.inputs.nixpkgs.follows = "nixpkgs-mac";
+
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs-mac";
-
-    nur.url = "github:nix-community/NUR";
   };
 
   outputs = inputs:
