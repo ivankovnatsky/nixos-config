81bff2dc242e071a4bbd1c3630c82c22e30b3c81
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-11-01 10:57:20 +0200

    Add Reminders and Notes to dock

--- home/dockutil.nix
+++ home/dockutil.nix
@@ -7,6 +7,8 @@
     ${pkgs.dockutil}/bin/dockutil --add "/Applications/Firefox.app"
     ${pkgs.dockutil}/bin/dockutil --add "/Applications/Safari.app"
     ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/Messages.app"
+    ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/Reminders.app"
+    ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/Notes.app"
     ${pkgs.dockutil}/bin/dockutil --add "/Applications/Twitterrific.app"
     ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/Utilities/Activity Monitor.app"
     ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/System Preferences.app"
