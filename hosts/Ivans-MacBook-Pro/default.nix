319bec45c03035eaf401ab0e3ec0d0d64fccf315
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2022-11-30 11:06:30 +0200

    Add tor

--- hosts/Ivans-MacBook-Pro/default.nix
+++ hosts/Ivans-MacBook-Pro/default.nix
@@ -7,7 +7,11 @@
     "boz/repo"
   ];
 
+  # Remove /opt/homebrew/etc/tor/torrc.sample to make the tor work
+  # Configure your application to use SOCKS in its proxy settings directly
+  # localhost:9050
   homebrew.brews = [
+    "tor"
     "kail"
     "youtube-dl"
   ];
