39053955afd0cca06761a1ce6ac3412a733ac27c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-02-22 14:01:38 +0200

    Update nvim-lspconfig defaults

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -186,7 +186,7 @@
             vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
             vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-            vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+            vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
           end
 
           local lsp_flags = {
