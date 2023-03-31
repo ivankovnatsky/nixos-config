0ddfbabd66593f28df102b61af83d97f77f0b35f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-03-31 16:15:58 +0300

    Add netrc config for vim-rhubarb

--- home/default.nix
+++ home/default.nix
@@ -67,6 +67,12 @@ in
         document-start: disable
       '';
     };
+
+    ".netrc" = {
+      text = ''
+        default api.github.com login ivankovnatsky password ${config.secrets.gitApiTokenRepoScope}
+      '';
+    };
   };
 
   home.sessionVariables = {
