6a2d73aef90507da8335dbd8d6e0c45ae9b73e3f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-04-19 11:39:43 +0300

    Update ktop overlay

--- overlays/kubectl-ktop.nix
+++ overlays/kubectl-ktop.nix
@@ -1,23 +1,19 @@
 { lib, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
-  pname = "ktop";
-  version = "cb3a80f1ea70c6fbc1207c2b2340314aa19962b8";
+  pname = "kubectl-ktop";
+  version = "0.3.5";
 
   src = fetchFromGitHub {
     owner = "vladimirvivien";
-    repo = pname;
-    rev = "${version}";
+    repo = "ktop";
+    rev = "v${version}";
     sha256 = "sha256-nkIRVt2kqsE9QBYvvHmupohnzH+OBcwWwV16rMePw4I=";
   };
 
   vendorSha256 = "sha256-IiAMmHOq69WMT2B1q9ZV2fGDnLr7AbRm1P7ACSde2FE=";
 
-  postConfigure = ''
-    rm -rf ./.ci ./hack
-  '';
-
-  doCheck = false;
+  subPackages = [ "." ];
 
   ldflags = [
     "-s"
@@ -33,7 +29,7 @@ buildGoModule rec {
   meta = with lib; {
     description = "A top-like tool for your Kubernetes clusters";
     homepage = "https://github.com/vladimirvivien/ktop";
-    changelog = "https://github.com/vladimirvivien/ktop/releases/tag/${version}";
+    changelog = "https://github.com/vladimirvivien/ktop/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = [ maintainers.ivankovnatsky ];
   };
