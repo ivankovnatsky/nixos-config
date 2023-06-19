bb3ba6ecf24d6037167886cd728ad8ca08486976
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-06-19 13:03:14 +0300

    Set Terminal as default home terminal

--- home/dockutil.nix
+++ home/dockutil.nix
@@ -5,7 +5,7 @@
     ${pkgs.dockutil}/bin/dockutil --remove all
     ${pkgs.dockutil}/bin/dockutil --add "/Applications/iTerm.app"
     ${pkgs.dockutil}/bin/dockutil --add "/Applications/Firefox.app"
-    ${pkgs.dockutil}/bin/dockutil --add "/Applications/Chromium.app"
+    ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/Utilities/Terminal.app"
     ${pkgs.dockutil}/bin/dockutil --add "/System/Cryptexes/App/System/Applications/Safari.app"
     ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/Messages.app"
     ${pkgs.dockutil}/bin/dockutil --add "/System/Applications/Calendar.app"
