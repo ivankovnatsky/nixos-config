88487ed9f00f782623e85b4231fa170eeb691e28
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-06-19 13:03:04 +0300

    Add pass-import

--- home/default.nix
+++ home/default.nix
@@ -33,7 +33,7 @@ in
 
   programs.password-store = {
     enable = true;
-    package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
+    package = pkgs.pass.withExtensions (exts: [ exts.pass-otp exts.pass-import ]);
     settings = {
       PASSWORD_STORE_DIR = "${homeDir}/ivan/.password-store/";
     };
