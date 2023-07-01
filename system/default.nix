996573ebc2d2ed6ed347296b1dc9c99f16a406bf
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-07-01 15:55:46 +0300

    Move programs.zsh to system/default

--- system/default.nix
+++ system/default.nix
@@ -23,4 +23,13 @@
       experimental-features = nix-command flakes
     '';
   };
+
+  # error:
+  #      Failed assertions:
+  #      - users.users.ivan.shell is set to zsh, but
+  #      programs.zsh.enable is not true. This will cause the zsh
+  #      shell to lack the basic nix directories in its PATH and might make
+  #      logging in as that user impossible. You can fix it with:
+  #      programs.zsh.enable = true;
+  programs.zsh.enable = true;
 }
