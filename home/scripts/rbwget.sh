a5ed7e7917f2bd828de95da460a2bf9173678244
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-07-01 17:15:22 +0300

    Adapt rbwget for linux platform

--- home/scripts/rbwget.sh
+++ home/scripts/rbwget.sh
@@ -2,7 +2,7 @@
 
 # This script retrieves an item's field from Bitwarden using rbw,
 # filters the results with ripgrep (rg), and selects one with fzf if there are multiple matches.
-# The selected field is then copied to the clipboard with pbcopy.
+# The selected field is then copied to the clipboard with clipboard tool.
 
 # Check if the required number of arguments are provided
 if [ "$#" -ne 2 ]; then
@@ -10,12 +10,22 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+# Define clipboard tool based on platform.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    clipboard="pbcopy"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    clipboard="xclip -selection clipboard"
+else
+    echo "Error: Unsupported platform."
+    exit 1
+fi
+
 name="$1"
 item="$2"
 
 # Check if required commands are available
-for cmd in rbw rg fzf pbcopy; do
-    if ! command -v $cmd &>/dev/null; then
+for cmd in rbw rg fzf $clipboard; do
+    if ! command -v "$cmd" &>/dev/null; then
         echo "Error: $cmd not found. Please install $cmd and try again."
         exit 1
     fi
@@ -42,6 +52,6 @@ else
 fi
 
 # Extract the specified field and copy it to the clipboard
-echo "$selected" | xargs -r rbw get --field "$item" | sed 's/^.*: //' | pbcopy
+echo "$selected" | xargs -r rbw get --field "$item" | sed 's/^.*: //' | $clipboard
 
 echo "The $item field of the selected item has been copied to the clipboard."
