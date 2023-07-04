68680518a83b027c34acc3fda05a5671af3afdde
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-07-04 08:43:02 +0300

    Correct kitty tab keybindings

--- home/kitty.nix
+++ home/kitty.nix
@@ -26,13 +26,13 @@ in
 
         map cmd+1 goto_tab 1
         map cmd+2 goto_tab 2
-        map cmd+2 goto_tab 3
-        map cmd+2 goto_tab 4
-        map cmd+2 goto_tab 5
-        map cmd+2 goto_tab 6
-        map cmd+2 goto_tab 7
-        map cmd+2 goto_tab 8
-        map cmd+2 goto_tab 9
+        map cmd+3 goto_tab 3
+        map cmd+4 goto_tab 4
+        map cmd+5 goto_tab 5
+        map cmd+6 goto_tab 6
+        map cmd+7 goto_tab 7
+        map cmd+8 goto_tab 8
+        map cmd+9 goto_tab 9
       '';
     };
   };
