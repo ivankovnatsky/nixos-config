442c53ca792bd484c47568d26aca667ccfd1d1dd
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-07-07 18:52:14 +0300

    Add nvd diff to activationScripts

--- system/default.nix
+++ system/default.nix
@@ -24,6 +24,14 @@
     '';
   };
 
+  # https://github.com/luishfonseca/dotfiles/blob/main/modules/upgrade-diff.nix
+  system.activationScripts.diff = {
+    supportsDryActivation = true;
+    text = ''
+      ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+    '';
+  };
+
   # error:
   #      Failed assertions:
   #      - users.users.ivan.shell is set to zsh, but
