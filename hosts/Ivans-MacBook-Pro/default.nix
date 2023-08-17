361137e41c4bf758dbc0688132233e2722ed575d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-08-17 21:54:58 +0300

    Update aws-sso to 1.12

--- hosts/Ivans-MacBook-Pro/default.nix
+++ hosts/Ivans-MacBook-Pro/default.nix
@@ -30,6 +30,7 @@
           sha256 = "sha256-iRnO7jT2v7Fqbp/2HJX0BDw1xtcLId4n5aFTwZx+upw=";
           system = "aarch64-darwin";
         };
+        aws-sso-cli = super.callPackage ../../overlays/aws-sso-cli.nix { };
       }
     )
   ];
