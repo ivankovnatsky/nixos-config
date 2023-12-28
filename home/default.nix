bdc9cdc3eb0497a6dd9896ced876ef237eb7d646
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-12-28 17:44:05 +0200

    Use home.activation to install netrc

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, super, ... }:
+{ config, lib, pkgs, super, ... }:
 
 let
   inherit (pkgs.stdenv.targetPlatform) isDarwin;
@@ -77,12 +77,19 @@ in
         document-start: disable
       '';
     };
+  };
 
-    ".netrc" = {
-      text = ''
-        default api.github.com login ivankovnatsky password ${config.secrets.gitApiTokenRepoScope}
+  home.activation = {
+    createAndSetPermissionsNetrc =
+      let
+        netrcContent = pkgs.writeText "tmp_netrc" ''
+          default api.github.com login ivankovnatsky password ${config.secrets.gitApiTokenRepoScope}
+        '';
+      in
+      lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+        cp "${netrcContent}" "$HOME/.netrc"
+        chmod 0600 "$HOME/.netrc"
       '';
-    };
   };
 
   home.sessionVariables = {
