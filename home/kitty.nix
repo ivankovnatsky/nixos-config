d2e85a6a87ee095fbd9c70c3bea4e46bd3f8e9b0
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-01-10 14:19:27 +0200

    Remove macos_menubar_title_max_length and conditionaly set keybinds

--- home/kitty.nix
+++ home/kitty.nix
@@ -22,17 +22,19 @@ in
         max_title_length 100
         tab_title_template "{index}: {tab.active_exe}"
         macos_option_as_alt yes
-        macos_menubar_title_max_length 1
 
-        map cmd+1 goto_tab 1
-        map cmd+2 goto_tab 2
-        map cmd+3 goto_tab 3
-        map cmd+4 goto_tab 4
-        map cmd+5 goto_tab 5
-        map cmd+6 goto_tab 6
-        map cmd+7 goto_tab 7
-        map cmd+8 goto_tab 8
-        map cmd+9 goto_tab 9
+        ${if isDarwin then ''
+          map cmd+1 goto_tab 1
+          map cmd+2 goto_tab 2
+          map cmd+3 goto_tab 3
+          map cmd+4 goto_tab 4
+          map cmd+5 goto_tab 5
+          map cmd+6 goto_tab 6
+          map cmd+7 goto_tab 7
+          map cmd+8 goto_tab 8
+          map cmd+9 goto_tab 9
+        ''
+        else ""}
       '';
     };
   };
