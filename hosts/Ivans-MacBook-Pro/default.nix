3940dbc38a09ad0ac77c42ed3a7c93d1906a8785
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-01-10 21:06:14 +0200

    Use latest 23.11 terraform 1.6.4

--- hosts/Ivans-MacBook-Pro/default.nix
+++ hosts/Ivans-MacBook-Pro/default.nix
@@ -36,12 +36,12 @@
   nixpkgs.overlays = [
     (
       self: super: {
-        terraform = super.callPackage ../../overlays/hashicorp-generic.nix {
-          name = "terraform";
-          version = "1.1.7";
-          sha256 = "sha256-iRnO7jT2v7Fqbp/2HJX0BDw1xtcLId4n5aFTwZx+upw=";
-          system = "aarch64-darwin";
-        };
+        # terraform = super.callPackage ../../overlays/hashicorp-generic.nix {
+        #   name = "terraform";
+        #   version = "1.1.7";
+        #   sha256 = "sha256-iRnO7jT2v7Fqbp/2HJX0BDw1xtcLId4n5aFTwZx+upw=";
+        #   system = "aarch64-darwin";
+        # };
         # terraform = super.callPackage ../../overlays/hashicorp-generic.nix {
         #   name = "terraform";
         #   version = "1.3.7";
