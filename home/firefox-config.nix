f099d3cf9f583e58fd4c1d96a83a32e2fbdd3563
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-02-19 12:39:24 +0200

    Revert "Use home-manager to configure Firefox for all OSes"
    This reverts commit 0d1fe14531440c46e4575b975554622c82aa95fd.

---
+++ home/firefox-config.nix
@@ -0,0 +1,111 @@
+{ pkgs, ... }:
+
+let
+  inherit (pkgs.stdenv.targetPlatform) isDarwin;
+
+  configPath = if isDarwin then "Library/Application Support/Firefox" else ".mozilla/firefox";
+
+  defaultConfig = ''
+    user_pref("browser.contentblocking.category", "strict");
+    user_pref("browser.ctrlTab.recentlyUsedOrder", true);
+    user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+    user_pref("browser.download.dir", "/tmp");
+    user_pref("browser.engagement.ctrlTab.has-used", true);
+    user_pref("browser.fullscreen.autohide", false);
+    user_pref("browser.safebrowsing.appRepURL", "");
+    user_pref("browser.safebrowsing.malware.enabled", false);
+    user_pref("browser.search.hiddenOneOffs", "Google,Yahoo,Bing,Amazon.com,Twitter");
+    user_pref("browser.search.suggest.enabled", false);
+    user_pref("browser.send_pings", false);
+    user_pref("browser.startup.page", 3);
+    user_pref("browser.toolbars.bookmarks.visibility", "never");
+    user_pref("dom.security.https_only_mode", true);
+    user_pref("extensions.pocket.enabled", false);
+    user_pref("general.smoothScroll", true);
+    user_pref("geo.enabled", false);
+    user_pref("network.allow-experiments", false);
+    user_pref("network.dns.disablePrefetch", true);
+    user_pref("permissions.default.desktop-notification", 2);
+    user_pref("privacy.donottrackheader.enabled", true);
+    user_pref("privacy.donottrackheader.value", 1);
+    user_pref("security.sandbox.content.level", 3);
+    user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+    user_pref("app.update.silent", true);
+    user_pref("app.update.url.details", "https://non-existent-site");
+    user_pref("app.update.url.manual", "https://non-existent-site");
+  '';
+
+  defaultLinuxConfig = ''
+    ${defaultConfig}
+    user_pref("media.videocontrols.picture-in-picture.enabled", false);
+    user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+    user_pref("media.videocontrols.picture-in-picture.video-toggle.has-used", false);
+    user_pref("widget.wayland-dmabuf-vaapi.enabled", true);
+    user_pref("gfx.webrender.all", true);
+  '';
+
+  userChromeConfig = ''
+    #TabsToolbar {
+      visibility: collapse;
+    }
+
+    #sidebar-header {
+      display: none;
+    }
+  '';
+in
+{
+  home.file = {
+    # "${configPath}/profiles.ini" =
+    #   if isDarwin then { text = ""; }
+    #   else {
+    #     text = ''
+    #       [General]
+    #       StartWithLastProfile=1
+
+    #       [Profile0]
+    #       Default=1
+    #       IsRelative=1
+    #       Name=Home
+    #       Path=Profiles/Home
+    #     '';
+    #   };
+
+    # This thing only needed to a macOS, will check sometime on Linux.
+    # "${configPath}/installs.ini" =
+    #   if isDarwin then
+    #     {
+    #       text = ''
+    #         [2656FF1E876E9973]
+    #         Default=Profiles/Work
+    #         Locked=1
+    #       '';
+    #     }
+    #   else { text = ""; };
+
+    "${configPath}/Profiles/Home/user.js" = {
+      text =
+        if isDarwin then ''
+          ${defaultConfig}
+        '' else ''
+          ${defaultLinuxConfig}
+        '';
+    };
+
+    "${configPath}/Profiles/Home/chrome/userChrome.css" = {
+      text = ''
+        ${userChromeConfig}
+      '';
+    };
+
+    "${configPath}/Profiles/Work/user.js" =
+      if isDarwin then
+        { text = defaultConfig; }
+      else { };
+
+    "${configPath}/Profiles/Work/chrome/userChrome.css" =
+      if isDarwin then
+        { text = userChromeConfig; }
+      else { };
+  };
+}
--- home/firefox.nix
+++ home/firefox.nix
@@ -1,111 +1,25 @@
 { pkgs, ... }:
 
-let
-  inherit (pkgs.stdenv.targetPlatform) isDarwin;
-  extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-    # firefox-translations
-    # granted-containers
-    bitwarden
-    clearurls
-    darkreader
-    decentraleyes
-    duckduckgo-privacy-essentials
-    # https-everywhere
-    multi-account-containers
-    onepassword-password-manager
-    privacy-badger
-    # To disable all those tree shenanigans:
-    # https://github.com/piroor/treestyletab/issues/1544#issuecomment-522902490
-    # tree-style-tab
-    ublock-origin
-  ];
-
-  defaultSettings = {
-    "browser.contentblocking.category" = "strict";
-    "browser.ctrlTab.recentlyUsedOrder" = true;
-    "browser.ctrlTab.sortByRecentlyUsed" = true;
-    "browser.download.dir" = "/tmp";
-    "browser.engagement.ctrlTab.has-used" = true;
-    "browser.fullscreen.autohide" = false;
-    "browser.safebrowsing.appRepURL" = "";
-    "browser.safebrowsing.malware.enabled" = false;
-    "browser.search.hiddenOneOffs" = "Google,Yahoo,Bing,Amazon.com,Twitter";
-    "browser.search.suggest.enabled" = false;
-    "browser.send_pings" = false;
-    "browser.startup.page" = 3;
-    "browser.toolbars.bookmarks.visibility" = "never";
-    "dom.security.https_only_mode" = true;
-    "extensions.pocket.enabled" = false;
-    "general.smoothScroll" = true;
-    "geo.enabled" = false;
-    "network.allow-experiments" = false;
-    "network.dns.disablePrefetch" = true;
-    "permissions.default.desktop-notification" = 2;
-    "privacy.donottrackheader.enabled" = true;
-    "privacy.donottrackheader.value" = 1;
-    "security.sandbox.content.level" = 3;
-    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-    "app.update.silent" = true;
-    "app.update.url.details" = "https://non-existent-site";
-    "app.update.url.manual" = "https://non-existent-site";
-  };
-
-  defaultLinuxSettingsTemplate = {
-    "media.videocontrols.picture-in-picture.enabled" = false;
-    "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
-    "media.videocontrols.picture-in-picture.video-toggle.has-used" = false;
-    "widget.wayland-dmabuf-vaapi.enabled" = true;
-    "gfx.webrender.all" = true;
-  };
-
-  defaultLinuxSettings = defaultSettings // defaultLinuxSettingsTemplate;
-
-  userChromeSettings = ''
-    #TabsToolbar {
-      visibility: collapse;
-    }
-
-    #sidebar-header {
-      display: none;
-    }
-  '';
-in
 {
   programs.firefox = {
     enable = true;
 
-    package = null;
-
-    # Platform-specific profile settings
-    profiles =
-      if isDarwin then {
-        "Home" = {
-          id = 0;
-          isDefault = false;
-          extensions = extensions;
-          settings = defaultSettings;
-          userChrome = userChromeSettings;
-        };
-        "Work" = {
-          id = 1;
-          isDefault = true;
-          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-            granted
-            darkreader
-            multi-account-containers
-            onepassword-password-manager
-          ];
-          settings = defaultSettings;
-          userChrome = userChromeSettings;
-        };
-      } else {
-        "Home" = {
-          id = 0;
-          isDefault = true;
-          extensions = extensions;
-          settings = defaultLinuxSettings;
-          userChrome = userChromeSettings;
-        };
-      };
+    profiles.default.extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      # firefox-translations
+      # granted-containers
+      bitwarden
+      clearurls
+      darkreader
+      decentraleyes
+      duckduckgo-privacy-essentials
+      # https-everywhere
+      multi-account-containers
+      onepassword-password-manager
+      privacy-badger
+      # To disable all those tree shenanigans:
+      # https://github.com/piroor/treestyletab/issues/1544#issuecomment-522902490
+      # tree-style-tab
+      ublock-origin
+    ];
   };
 }
