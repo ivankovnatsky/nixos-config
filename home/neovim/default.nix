9beaa5a6af1addf59f192e0266fcb59adcdb7d91
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-02-19 17:12:20 +0200

    Add ukranian spellcheck dictionary to neovim

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,6 +1,14 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  nvim-spell-uk-utf8-dictionary = builtins.fetchurl {
+    url = "http://ftp.vim.org/vim/runtime/spell/uk.utf-8.spl";
+    sha256 = "05180znfdjwqhl2gfsq42jzwqadd7cgr59p9cvz6hw2dlnj6qs71";
+  };
+in
 {
+  home.file."${config.xdg.configHome}/nvim/spell/uk.utf-8.spl".source = nvim-spell-uk-utf8-dictionary;
+
   home.packages = with pkgs; [
     delta
     dhall-lsp-server
