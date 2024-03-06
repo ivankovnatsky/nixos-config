c558745a88323b9857e7ef4eeafa78407889f46c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-03-06 08:31:05 +0200

    Add MacBook-Air machine configuration

--- system/default.nix
+++ system/default.nix
@@ -26,14 +26,6 @@ in
     '';
   };
 
-  # https://github.com/luishfonseca/dotfiles/blob/main/modules/upgrade-diff.nix
-  system.activationScripts.diff = {
-    supportsDryActivation = true;
-    text = ''
-      ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
-    '';
-  };
-
   # error:
   #      Failed assertions:
   #      - users.users.ivan.shell is set to zsh, but
