d707cbb1cf7799adffe82734b89a59dea204c3f8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-03-11 16:08:19 +0200

    Add k9s config

--- home/k9s.nix
+++ home/k9s.nix
@@ -3,36 +3,29 @@
 let inherit (pkgs.stdenv.targetPlatform) isDarwin;
   k9sConfigPath = if isDarwin then "Library/Application Support" else ".config";
 
+  darkMode = false;
 in
 {
-  # Config for k9s
+  home.packages = with pkgs; [ nixpkgs-unstable.k9s ];
   # https://k9scli.io/topics/skins/
   # https://github.com/derailed/k9s/tree/master/skins
   home.file."${k9sConfigPath}/k9s/skins/transparent.yaml".source = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/derailed/k9s/master/skins/transparent.yaml";
     sha256 = "sha256-4+tCRcI5fsSwqqhnNEZiD6LAc6ZW/AaP7KZ0003/XSE=";
   };
-  home.file."${k9sConfigPath}".text = ''
+  home.file."${k9sConfigPath}/k9s/config.yaml".text = ''
     k9s:
       liveViewAutoRefresh: false
       refreshRate: 2
       maxConnRetry: 5
-      enableMouse: false
-      headless: true
-      logoless: true
-      crumbsless: true
       readOnly: false
-      noExitOnCtrlC: false
-      noIcons: true
-      skipLatestRevCheck: true
       ui:
-        skin: transparent
-      logger:
-        tail: 100
-        buffer: 5000
-        sinceSeconds: 60
-        fullScreenLogs: false
-        textWrap: false
-        showTime: false
+        enableMouse: false
+        headless: true
+        logoless: true
+        crumbsless: true
+        noIcons: true
+        # Uses skin located in your $XDG_CONFIG_HOME/skins/
+        ${if darkMode then "skin: transparent" else ""}
   '';
 }
