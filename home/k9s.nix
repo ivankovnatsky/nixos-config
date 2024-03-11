2e8f4947e1fa24355b690901bc1910b8f471daf4
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-03-11 15:48:01 +0200

    Add transparent skin to k9s

--- home/k9s.nix
+++ home/k9s.nix
@@ -1,13 +1,17 @@
 { pkgs, ... }:
 
 let inherit (pkgs.stdenv.targetPlatform) isDarwin;
-  k9sConfigPath = if isDarwin then "Library/Application Support/k9s/config.yml" else ".config/k9s/config.yml";
+  k9sConfigPath = if isDarwin then "Library/Application Support" else ".config";
 
 in
 {
   # Config for k9s
   # https://k9scli.io/topics/skins/
   # https://github.com/derailed/k9s/tree/master/skins
+  home.file."${k9sConfigPath}/k9s/skins/transparent.yaml".source = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/derailed/k9s/master/skins/transparent.yaml";
+    sha256 = "sha256-4+tCRcI5fsSwqqhnNEZiD6LAc6ZW/AaP7KZ0003/XSE=";
+  };
   home.file."${k9sConfigPath}".text = ''
     k9s:
       liveViewAutoRefresh: false
@@ -21,6 +25,8 @@ in
       noExitOnCtrlC: false
       noIcons: true
       skipLatestRevCheck: true
+      ui:
+        skin: transparent
       logger:
         tail: 100
         buffer: 5000
