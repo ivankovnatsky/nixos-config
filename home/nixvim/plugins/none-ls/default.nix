121b9e3ef77f46a5a1ec8973d9bfbd32b5061b30
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-05-24 17:44:28 +0300

    Add python formatting to none-ls

--- home/nixvim/plugins/none-ls/default.nix
+++ home/nixvim/plugins/none-ls/default.nix
@@ -10,9 +10,21 @@
             code_actions = {
               gitsigns.enable = true;
             };
+            # Normally I would wanted for native LSP to handle this, but for some
+            # reason it stopped working, I'm not sure why, but I think I recall
+            # that it worked before, at least for lua, nix and python files
+            # only using LSP formatting capabilities.
             formatting = {
               nixpkgs_fmt.enable = true;
               stylua.enable = true;
+              black = {
+                enable = true;
+                withArgs = ''
+                  {
+                    extra_args = { "--fast" },
+                  }
+                '';
+              };
             };
           };
         };
