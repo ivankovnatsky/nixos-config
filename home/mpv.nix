ace235c3534fb78e582db3086c7d77c276d0b87f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-05-26 22:05:26 +0300

    Update mpv settings with autofit in window mode

--- home/mpv.nix
+++ home/mpv.nix
@@ -6,18 +6,24 @@
   ];
 
   home.file.".config/mpv/config".text = ''
+    # Do not show video window when playing audio
     no-audio-display
     alang=eng
     force-seekable=yes
-    fs=yes
+    audio-channels=2
+
+    fs=no
     osc=no
     osd-level=1  # Default osd-level when not seeking
+    # When viewing images
+    image-display-duration=5
+    ytdl-format="bestvideo[height<=1080]+bestaudio/best"
+    keepaspect-window=no
+    autofit-larger=100%x100%
+
     save-position-on-quit=yes
     slang=eng
     sub-scale=0.5
-    ytdl-format=bestvideo+bestaudio/best
-    image-display-duration=5
-    audio-channels=2
   '';
 
   home.file.".config/mpv/scripts/osd-during-seek.lua".text = ''
