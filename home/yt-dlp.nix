edaac2c39fa27fcbeb44832814a3ba94a3753f71
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-07-11 19:29:26 +0300

    Tweak yt-dlp config to have hd quality by default

--- home/yt-dlp.nix
+++ home/yt-dlp.nix
@@ -7,8 +7,7 @@
 
   home.file = {
     "${config.xdg.configHome}/yt-dlp/config".text = ''
-      # Download videos in the best HD format available
-      -f "best[height<=1080]"
+      -S "res:1080"
     '';
   };
 }
