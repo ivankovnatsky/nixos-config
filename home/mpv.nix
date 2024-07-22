a3ebf2cbee677e0c6b9b13c422a67e16c47e0c84
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-07-22 16:55:29 +0300

    Remove mpv config showing up time duration when seeking

--- home/mpv.nix
+++ home/mpv.nix
@@ -26,29 +26,4 @@
     slang=eng
     sub-scale=0.5
   '';
-
-  home.file.".config/mpv/scripts/osd-during-seek.lua".text = ''
-    local osd_timer = nil
-
-    mp.observe_property("seeking", "bool", function(name, value)
-        if value then
-            -- When seeking, set osd-level to 3
-            mp.set_property("osd-level", 3)
-            -- Reset and disable any existing timer
-            if osd_timer then
-                osd_timer:kill()
-                osd_timer = nil
-            end
-        else
-            -- Start or restart a timer to revert osd-level after 2 seconds
-            if osd_timer then
-                osd_timer:resume()
-            else
-                osd_timer = mp.add_timeout(1, function()
-                    mp.set_property("osd-level", 1)
-                end)
-            end
-        end
-    end)
-  '';
 }
