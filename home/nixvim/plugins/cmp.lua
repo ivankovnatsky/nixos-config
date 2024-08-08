ae65f2d719b38a0ed4264421840b6c6e165b0cbc
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-08-08 21:38:52 +0300

    Fix cmp cmdline: /, ?, : completions and remove gitcommit

--- home/nixvim/plugins/cmp.lua
+++ home/nixvim/plugins/cmp.lua
@@ -1,26 +1,20 @@
 local cmp = require("cmp")
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = "buffer" },
   },
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype("gitcommit", {
-  sources = cmp.config.sources({
-    { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = "buffer" },
-  }),
-})
-
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "path" },
   }, {
     { name = "cmdline" },
   }),
+  matching = { disallow_symbol_nonprefix_matching = false },
 })
