ecde952f5e405e68102f70ab370a7354f76dbad4
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-08-08 20:54:56 +0300

    Update cmp, neo-tree, oil plugins in nixvim

---
+++ home/nixvim/plugins/cmp.lua
@@ -0,0 +1,26 @@
+local cmp = require("cmp")
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+  sources = cmp.config.sources({
+    { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = "buffer" },
+  }),
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+})
