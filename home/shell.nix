9011c06e4293e6ee2fa4bdb08cd8d6e32872c28d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-09-10 13:34:14 +0300

    Add `--no-headers` for k alias

--- home/shell.nix
+++ home/shell.nix
@@ -32,7 +32,7 @@ let
       wl-copy = lib.mkIf isLinux "${pkgs.wl-clipboard}/bin/wl-copy -n";
     } else commonShellAliases // {
       tf = "terraform";
-      k = "${pkgs.kubectl}/bin/kubectl";
+      k = "${pkgs.kubectl}/bin/kubectl --no-headers";
     };
 
   # vimPlugin = builtins.fetchurl {
