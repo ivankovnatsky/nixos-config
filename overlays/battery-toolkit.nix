170088e49a543bffaa01554f67b01af5618672b0
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-09-26 16:14:03 +0300

    Add battery-toolkit overlay

---
+++ overlays/battery-toolkit.nix
@@ -0,0 +1,33 @@
+{ lib
+, fetchzip
+, stdenv
+}:
+
+let
+  version = "1.4";
+in
+
+stdenv.mkDerivation rec {
+  pname = "battery-toolkit";
+  inherit version;
+
+  # https://github.com/mhaeuser/Battery-Toolkit/releases/download/1.4/Battery-Toolkit-1.4.zip
+  src = fetchzip {
+    url = "https://github.com/mhaeuser/Battery-Toolkit/releases/download/${version}/Battery-Toolkit-${version}.zip";
+    hash = "sha256-bTpDr83xKoBxV8EQZBMQU0g9pRXuJNHy3MtaeO1j/M0=";
+  };
+
+  installPhase = ''
+    ls -lahR .
+    mkdir -p $out/Applications
+    cp -R . "$out/Applications/Battery Toolkit.app"
+  '';
+
+  meta = with lib; {
+    description = "Battery Toolkit for macOS";
+    homepage = "https://github.com/mhaeuser/Battery-Toolkit";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ivankovnatsky ];
+    platforms = platforms.darwin;
+  };
+}
