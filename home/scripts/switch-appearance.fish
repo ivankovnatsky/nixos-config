0feb05c38ee79a83c5f2985877d8e7b6b30df47d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-09-30 17:58:47 +0300

    Add fish script: switch-appearance.fish
    This toggle macOS appearance Dark/Light and also changed solid color
    wallpaper either Stone/Silver based on Dark/Light appearance.
    
    Only thing this does not handle is: "Show on all Space", which should be
    turned on manually.

---
+++ home/scripts/switch-appearance.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/env fish
+
+# Function to get the current theme
+function get_current_theme
+    osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode'
+end
+
+# Function to set wallpaper
+function set_wallpaper
+    set color $argv[1]
+    set file_path "/System/Library/Desktop Pictures/Solid Colors/$color.png"
+    
+    osascript -e '
+    tell application "System Events"
+        tell every desktop
+            set picture to "'$file_path'"
+        end tell
+    end tell
+    '
+    
+    echo "Wallpaper set to $color"
+end
+
+# Get the current theme
+set current_theme (get_current_theme)
+
+# Toggle the theme and set wallpaper
+if test "$current_theme" = "true"
+    osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to false'
+    set_wallpaper "Silver"
+    echo "Switched to Light Mode with Silver wallpaper"
+else
+    osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
+    set_wallpaper "Stone"
+    echo "Switched to Dark Mode with Stone wallpaper"
+end
