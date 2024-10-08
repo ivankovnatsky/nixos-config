4bf841ceae51b3aecd0f8385878273296d5664e9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-10-08 10:13:58 +0300

    Add freeze-window script for mpv and re-arrange some settings

--- home/mpv.nix
+++ home/mpv.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 
+let
+  freeze-window-lua = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/occivink/mpv-image-viewer/refs/heads/master/scripts/freeze-window.lua";
+    sha256 = "sha256-33doEp13UC4xFjaaeRyweV0Rk5LTIE3JtChmDUepJsI=";
+  };
+in
 {
   home.packages = with pkgs; [
     mpv
@@ -8,22 +14,37 @@
   home.file.".config/mpv/config".text = ''
     # Do not show video window when playing audio
     no-audio-display
+    save-position-on-quit=yes
+    fs=no
+
+    # When viewing images
+    image-display-duration=5
+
     alang=eng
     force-seekable=yes
     audio-channels=2
 
-    fs=no
-    osc=no
-    osd-level=1  # Default osd-level when not seeking
-    no-border
-    # When viewing images
-    image-display-duration=5
-    ytdl-format="bestvideo[height<=1080]+bestaudio/best"
-    keepaspect-window=no
-    autofit-larger=100%x100%
-
-    save-position-on-quit=yes
     slang=eng
     sub-scale=0.5
+
+    # OnScreen Controller at the bottom, terriable UI, disable it.
+    osc=no
+
+    # https://mpv.io/manual/master/#options-osd-level
+    osd-level=3
+
+    no-border
+
+    ytdl-format="bestvideo[height<=1080]+bestaudio/best"
+
+    auto-window-resize=no
+    keep-open=yes
+
+    keepaspect-window=no
+    autofit-larger=100%x100%
   '';
+
+  home.file = {
+    ".config/mpv/scripts/freeze-window.lua".source = freeze-window-lua;
+  };
 }
