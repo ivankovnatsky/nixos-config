f12983ad25f195c6bf454287308bf2e10e4e4407
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-11-01 16:32:02 +0200

    Add vscode, zed, jujutsu, some other apps/tools, re-arrange dock entries

---
+++ home/vscode.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+    userSettings = {
+      "terminal.integrated.fontFamily" = "${config.flags.fontGeneral}";
+      "files.autoSave" = "off";
+      "[nix]"."editor.tabSize" = 2;
+      "vim.relativeLineNumbers" = true;
+      "editor.lineNumbers" = "relative";
+    };
+
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+
+      bbenoist.nix
+      jnoortheen.nix-ide
+
+      hashicorp.terraform
+    ];
+  };
+}
