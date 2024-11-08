4186b8c444562e8a6ab2e4ec08070bc6edcc531c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-11-08 09:02:02 +0200

    Add fish formatting to nixvim

--- home/nixvim/plugins/lsp/default.nix
+++ home/nixvim/plugins/lsp/default.nix
@@ -17,6 +17,8 @@
     lsp = {
       enable = true;
       servers = {
+        # FIXME: https://github.com/nix-community/nixvim/blob/3d24cb72618738130e6af9c644c81fe42aa34ebc/plugins/lsp/lsp-packages.nix#L52
+        # fish_lsp.enable = true;
         nil-ls.enable = true;
         lua-ls.enable = true;
       };
