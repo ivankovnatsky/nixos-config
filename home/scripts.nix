5823d043ab93f7d3ca15e81005ea738003b5c279
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-11-14 14:57:39 +0200

    Add backup-home-py and simplify logic for both

--- home/scripts.nix
+++ home/scripts.nix
@@ -8,16 +8,35 @@ let
       scriptPath = "${scriptsDir}/${scriptName}";
       scriptContents = builtins.readFile scriptPath;
       isFishScript = lib.hasSuffix ".fish" scriptName;
+      isPythonScript = lib.hasSuffix ".py" scriptName;
       bashShebang = "#!${pkgs.bash}/bin/bash";
       fishShebang = "#!${pkgs.fish}/bin/fish";
+      pythonShebang = "#!${pkgs.python3}/bin/python3";
+      
       scriptWithFixedShebang =
-        if isFishScript
-        then builtins.replaceStrings [ "#!/usr/bin/env fish" ] [ fishShebang ] scriptContents
-        else builtins.replaceStrings [ "#!/usr/bin/env bash" ] [ bashShebang ] scriptContents;
+        if isFishScript then
+          builtins.replaceStrings [ "#!/usr/bin/env fish" ] [ fishShebang ] scriptContents
+        else if isPythonScript then
+          builtins.replaceStrings [ "#!/usr/bin/env python3" ] [ pythonShebang ] scriptContents
+        else
+          builtins.replaceStrings [ "#!/usr/bin/env bash" ] [ bashShebang ] scriptContents;
+          
+      # Remove extension for the final binary name
+      binaryName = lib.removeSuffix 
+        (if isFishScript then ".fish"
+         else if isPythonScript then ".py"
+         else ".sh") 
+        scriptName;
     in
-    pkgs.writeScriptBin (lib.removeSuffix (if isFishScript then ".fish" else ".sh") scriptName) scriptWithFixedShebang;
+    pkgs.writeScriptBin binaryName scriptWithFixedShebang;
 
-  filteredScriptNames = lib.filter (scriptName: lib.hasSuffix ".sh" scriptName || lib.hasSuffix ".fish" scriptName) (builtins.attrNames scriptFiles);
+  # Add .py to filtered extensions
+  filteredScriptNames = lib.filter 
+    (scriptName: lib.hasSuffix ".sh" scriptName || 
+                lib.hasSuffix ".fish" scriptName || 
+                lib.hasSuffix ".py" scriptName) 
+    (builtins.attrNames scriptFiles);
+    
   scriptPackages = builtins.map processScript filteredScriptNames;
 in
 {
