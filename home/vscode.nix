9073ef27e4ee516c3cfd0f9afc97ff88a4093288
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-11-14 14:57:26 +0200

    Also configure Cursor using vscode default settings

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,25 +1,36 @@
 { config, pkgs, ... }:
 
+let
+  jsonFormat = pkgs.formats.json { };
+  
+  editorSettings = {
+    "terminal.integrated.fontFamily" = "${config.flags.fontGeneral}";
+    "files.autoSave" = "off";
+    "[nix]"."editor.tabSize" = 2;
+    "vim.relativeLineNumbers" = true;
+    "editor.lineNumbers" = "relative";
+    "scm.diffDecorations" = "all";
+    "editor.renderLineHighlight" = "all";
+    "diffEditor.renderSideBySide" = false;
+    "diffEditor.ignoreTrimWhitespace" = false;
+  };
+in
 {
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
-    userSettings = {
-      "terminal.integrated.fontFamily" = "${config.flags.fontGeneral}";
-      "files.autoSave" = "off";
-      "[nix]"."editor.tabSize" = 2;
-      "vim.relativeLineNumbers" = true;
-      "editor.lineNumbers" = "relative";
-    };
+    userSettings = editorSettings;
 
     extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
-
       jnoortheen.nix-ide
-
       hashicorp.terraform
     ];
   };
+
+  # Add Cursor settings with proper formatting
+  home.file."Library/Application Support/Cursor/User/settings.json".source = 
+    jsonFormat.generate "cursor-user-settings" editorSettings;
 }
