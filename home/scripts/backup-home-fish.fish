cf93a54a1b443296f3a154281cf41bcd8fbb1518
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-11-18 09:01:12 +0200

    Simplify both py and fish backup-home and add bash version

--- home/scripts/backup-home-fish.fish
+++ home/scripts/backup-home-fish.fish
@@ -3,10 +3,6 @@
 # Global variables
 set --global current_user (whoami)
 set --global backup_file "/tmp/$current_user.tar.gz"
-set --global min_backup_size_gb 5
-set --global max_backup_size_gb 20
-set --global min_backup_size_bytes (math "$min_backup_size_gb * 1024 * 1024 * 1024")
-set --global max_backup_size_bytes (math "$max_backup_size_gb * 1024 * 1024 * 1024")
 
 # Platform-specific excludes
 set --global darwin_excludes \
@@ -34,26 +30,7 @@ function backup_home
     pushd $HOME/..
     echo "Creating backup of home directory for $current_user..."
     sudo -v
-    
-    # Simple pipeline with basic pv
     sudo tar $darwin_excludes -cf - $current_user/ | pv | pigz > $backup_file
-    
-    if test -f $backup_file
-        set file_size (stat -f %z $backup_file)
-        if test $file_size -gt $min_backup_size_bytes
-            if test $file_size -lt $max_backup_size_bytes
-                set backup_size (du -h $backup_file | awk '{print $1}')
-                echo "Backup completed successfully: $backup_file (Size: $backup_size)"
-                return 0
-            end
-            echo "Backup file too large (> $max_backup_size_gb GB)"
-            return 1
-        end
-        echo "Backup file too small (< $min_backup_size_gb GB)"
-        return 1
-    end
-    echo "Backup file was not created"
-    return 1
 end
 
 function upload_backup
@@ -68,8 +45,8 @@ end
 
 function main
     backup_home
-    upload_backup
-    cleanup_backup
+    and upload_backup
+    and cleanup_backup
 end
 
 main
