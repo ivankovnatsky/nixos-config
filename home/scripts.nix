9fe2b5fae231264c9151f0e3f1b4c7495d572ebc
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-11-22 17:11:12 +0200

    Appy formatting to scripts.nix

--- home/scripts.nix
+++ home/scripts.nix
@@ -12,7 +12,7 @@ let
       bashShebang = "#!${pkgs.bash}/bin/bash";
       fishShebang = "#!${pkgs.fish}/bin/fish";
       pythonShebang = "#!${pkgs.python3}/bin/python3";
-      
+
       scriptWithFixedShebang =
         if isFishScript then
           builtins.replaceStrings [ "#!/usr/bin/env fish" ] [ fishShebang ] scriptContents
@@ -20,23 +20,23 @@ let
           builtins.replaceStrings [ "#!/usr/bin/env python3" ] [ pythonShebang ] scriptContents
         else
           builtins.replaceStrings [ "#!/usr/bin/env bash" ] [ bashShebang ] scriptContents;
-          
+
       # Remove extension for the final binary name
-      binaryName = lib.removeSuffix 
+      binaryName = lib.removeSuffix
         (if isFishScript then ".fish"
-         else if isPythonScript then ".py"
-         else ".sh") 
+        else if isPythonScript then ".py"
+        else ".sh")
         scriptName;
     in
     pkgs.writeScriptBin binaryName scriptWithFixedShebang;
 
   # Add .py to filtered extensions
-  filteredScriptNames = lib.filter 
-    (scriptName: lib.hasSuffix ".sh" scriptName || 
-                lib.hasSuffix ".fish" scriptName || 
-                lib.hasSuffix ".py" scriptName) 
+  filteredScriptNames = lib.filter
+    (scriptName: lib.hasSuffix ".sh" scriptName ||
+      lib.hasSuffix ".fish" scriptName ||
+      lib.hasSuffix ".py" scriptName)
     (builtins.attrNames scriptFiles);
-    
+
   scriptPackages = builtins.map processScript filteredScriptNames;
 in
 {
