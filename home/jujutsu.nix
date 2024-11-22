86439d18b8d3275d2e1730d2fddc2ce15d3d2722
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-11-22 17:06:13 +0200

    Add jujutsu config and refactor git as well

--- home/jujutsu.nix
+++ home/jujutsu.nix
@@ -1,5 +1,21 @@
+{ config, ... }:
+
 {
-    programs.jujutsu = {
-        enable = true;
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = {
+        name = config.flags.git.userName;
+        email = config.flags.git.userEmail;
+      };
+      ui = {
+        default-command = "log"; # Similar to your git log alias being frequently used
+      };
+      git = {
+        # Similar to your git config
+        push-branch-prefix = "refs/heads/";
+        push-default = "current";
+      };
     };
+  };
 }
