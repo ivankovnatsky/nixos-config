6dbdf6747ee84c973191a9ccf49cb9aadfb298f8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-11-29 16:25:52 +0200

    Keep only python script for home backup, fix launchd service

--- home/launchd-services.nix
+++ home/launchd-services.nix
@@ -6,37 +6,35 @@
       enable = true;
       config = {
         ProgramArguments = [
-          "/etc/profiles/per-user/${config.home.username}/bin/backup-home-py"
+          "/etc/profiles/per-user/${config.home.username}/bin/backup-home"
         ];
         StartCalendarInterval = [
           {
             Hour = 17;
             Minute = 0;
-            Weekday = 1;  # Monday
+            Weekday = 1; # Monday
           }
           {
             Hour = 17;
             Minute = 0;
-            Weekday = 2;  # Tuesday
+            Weekday = 2; # Tuesday
           }
           {
             Hour = 17;
             Minute = 0;
-            Weekday = 3;  # Wednesday
+            Weekday = 3; # Wednesday
           }
           {
             Hour = 17;
             Minute = 0;
-            Weekday = 4;  # Thursday
+            Weekday = 4; # Thursday
           }
           {
             Hour = 17;
             Minute = 0;
-            Weekday = 5;  # Friday
+            Weekday = 5; # Friday
           }
         ];
-        StartOnMount = true;
-        WatchPaths = [ "/" ];
 
         StandardOutPath = "/tmp/backup-home.out.log";
         StandardErrorPath = "/tmp/backup-home.err.log";
