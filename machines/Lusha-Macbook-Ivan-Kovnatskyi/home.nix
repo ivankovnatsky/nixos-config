1b35619476b6c081faa850e3b2dca4f92c45839e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-12-19 17:34:43 +0200

    Re-enable copilot-vim and add tac alias

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/home.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/home.nix
@@ -220,7 +220,7 @@
 
         # FIXME: Figure out suitable key for the completion, when you need to
         # override cmp plugins.
-        # copilot-vim.enable = true;
+        copilot-vim.enable = true;
       };
       extraPlugins = with pkgs.vimPlugins; [
         Jenkinsfile-vim-syntax
