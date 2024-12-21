08636a2222af1c4fc50f52d598c858235ae39954
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-12-21 11:40:57 +0200

    Remove Terminal neovim command

--- home/nixvim/commands/default.nix
+++ home/nixvim/commands/default.nix
@@ -1,12 +1,5 @@
 {
   programs.nixvim.userCommands = {
-    Terminal = {
-      command = "botright split | terminal"; # This uses Neovim's built-in terminal at the bottom
-      desc = "Open terminal in horizontal split";
-      bang = true; # Allows :Terminal! to force
-      bar = true; # Allows command chaining with |
-    };
-
     RebuildWatchman = {
       command = "botright split | terminal cd ~/Sources/github.com/ivankovnatsky/nixos-config && make rebuild-watchman";
       desc = "Open terminal and run watchman rebuild command";
