fb05edf2b7d7a3e0746340e4c28967e0bfaac5a5
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-12-29 17:05:26 +0200

    Move outputs and darwinConfigurations to separate files

---
+++ flake/darwin.nix
@@ -0,0 +1,83 @@
+{ inputs, ... }:
+let
+  makeDarwinConfig = { hostname, system, modules, homeModules, username }:
+    inputs.darwin.lib.darwinSystem {
+      inherit system;
+
+      modules = [
+        {
+          imports = [ ../machines/${hostname} ];
+          nixpkgs.overlays = [ inputs.self.overlay ];
+        }
+
+        inputs.home-manager.darwinModules.home-manager
+        ({ config, system, ... }: {
+          nix.nixPath.nixpkgs = "${inputs.nixpkgs}";
+
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.${username} = {
+              imports = [
+                ../machines/${hostname}/home.nix
+                inputs.nixvim.homeManagerModules.nixvim
+              ] ++ homeModules;
+            };
+
+            extraSpecialArgs = {
+              inherit inputs system;
+              super = config;
+            };
+          };
+        })
+
+        ({ config, ... }: {
+          homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+        })
+        inputs.nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = false;
+            user = username;
+            autoMigrate = true;
+            taps = {
+              "homebrew/homebrew-core" = inputs.homebrew-core;
+              "homebrew/homebrew-cask" = inputs.homebrew-cask;
+              "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
+            };
+            mutableTaps = false;
+          };
+        }
+      ] ++ modules;
+
+      specialArgs = { inherit system; };
+    };
+in
+{
+  darwinConfigurations = {
+    "Ivans-MacBook-Pro" = makeDarwinConfig {
+      hostname = "Ivans-MacBook-Pro";
+      system = "aarch64-darwin";
+      username = "ivan";
+      modules = [ ];
+      homeModules = [ ];
+    };
+
+    "Ivans-MacBook-Air" = makeDarwinConfig {
+      hostname = "Ivans-MacBook-Air";
+      system = "aarch64-darwin";
+      username = "ivan";
+      modules = [ ];
+      homeModules = [ ../home/pass.nix ];
+    };
+
+    "Lusha-Macbook-Ivan-Kovnatskyi" = makeDarwinConfig {
+      hostname = "Lusha-Macbook-Ivan-Kovnatskyi";
+      system = "aarch64-darwin";
+      username = "Ivan.Kovnatskyi";
+      modules = [ ];
+      homeModules = [ ];
+    };
+  };
+}
