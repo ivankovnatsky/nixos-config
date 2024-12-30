d2b2fff0dddb980bd34f14255b9323bff743a156
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-12-30 21:45:59 +0200

    Put avante configuration into a separate file

---
+++ home/nixvim/plugins/avante/default.nix
@@ -0,0 +1,61 @@
+{
+  programs.nixvim.plugins = {
+    # https://github.com/yetone/avante.nvim?tab=readme-ov-file#default-setup-configuration
+    # https://github.com/nix-community/nixvim/blob/f4b0b81ef9eb4e37e75f32caf1f02d5501594811/tests/test-sources/plugins/by-name/avante/default.nix#L3
+    avante = {
+      enable = true;
+      settings = {
+        debug = false;
+        provider = "claude";
+        auto_suggestions_provider = "claude";
+        tokenizer = "tiktoken";
+        system_prompt = ''
+          You are an excellent programming expert.
+        '';
+        openai = {
+          endpoint = "https://api.openai.com/v1";
+          model = "gpt-4o";
+          timeout = 30000;
+          temperature = 0;
+          max_tokens = 4096;
+        };
+        copilot = {
+          endpoint = "https://api.githubcopilot.com";
+          model = "gpt-4o-2024-05-13";
+          proxy = null;
+          allow_insecure = false;
+          timeout = 30000;
+          temperature = 0;
+          max_tokens = 4096;
+        };
+        claude = {
+          endpoint = "https://api.anthropic.com";
+          model = "claude-3-5-sonnet-20240620";
+          timeout = 30000;
+          temperature = 0;
+          max_tokens = 8000;
+        };
+        behaviour = {
+          auto_suggestions = false;
+          auto_set_highlight_group = true;
+          auto_set_keymaps = true;
+          auto_apply_diff_after_generation = false;
+          support_paste_from_clipboard = false;
+        };
+        mappings = {
+          sidebar = {
+            # Tab is currently used for Github Copilot.
+            switch_windows = "<C-k>";
+            reverse_switch_windows = "<C-j>";
+          };
+        };
+        diff = {
+          autojump = true;
+        };
+        hints = {
+          enabled = true;
+        };
+      };
+    };
+  };
+}
--- machines/Lusha-Macbook-Ivan-Kovnatskyi/home.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/home.nix
@@ -17,6 +17,7 @@
     ../../home/lsd.nix
     ../../home/mpv.nix
     ../../home/nixvim
+    ../../home/nixvim/plugins/avante
     ../../home/nixvim/plugins/gen-nvim
     ../../home/nixvim/plugins/nvim-jenkinsfile-linter
     ../../home/nushell.nix
@@ -218,63 +219,6 @@
             nix = [ "nixfmt" ];
           };
         };
-        # https://github.com/yetone/avante.nvim
-        avante = {
-          enable = true;
-          settings = {
-            debug = false;
-            provider = "claude";
-            auto_suggestions_provider = "claude";
-            tokenizer = "tiktoken";
-            system_prompt = ''
-              You are an excellent programming expert.
-            '';
-            openai = {
-              endpoint = "https://api.openai.com/v1";
-              model = "gpt-4o";
-              timeout = 30000;
-              temperature = 0;
-              max_tokens = 4096;
-            };
-            copilot = {
-              endpoint = "https://api.githubcopilot.com";
-              model = "gpt-4o-2024-05-13";
-              proxy = null;
-              allow_insecure = false;
-              timeout = 30000;
-              temperature = 0;
-              max_tokens = 4096;
-            };
-            claude = {
-              endpoint = "https://api.anthropic.com";
-              model = "claude-3-5-sonnet-20240620";
-              timeout = 30000;
-              temperature = 0;
-              max_tokens = 8000;
-            };
-            behaviour = {
-              auto_suggestions = false;
-              auto_set_highlight_group = true;
-              auto_set_keymaps = true;
-              auto_apply_diff_after_generation = false;
-              support_paste_from_clipboard = false;
-            };
-            mappings = {
-              sidebar = {
-                # Tab is currently used for Github Copilot.
-                switch_windows = "<C-k>";
-                reverse_switch_windows = "<C-j>";
-              };
-            };
-            diff = {
-              autojump = true;
-            };
-            hints = {
-              enabled = true;
-            };
-          };
-        };
-
         # FIXME: Figure out suitable key for the completion, when you need to
         # override cmp plugins.
         copilot-vim.enable = true;
