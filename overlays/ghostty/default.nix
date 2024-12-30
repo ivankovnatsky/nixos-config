9271add1f449eb78913fd27d5274ca6ff817fdc3
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-12-30 09:05:12 +0200

    Use public url for ghostty overlay

--- overlays/ghostty/default.nix
+++ overlays/ghostty/default.nix
@@ -1,52 +1,36 @@
 { lib
 , stdenv
+, fetchurl
 , unzip
 }:
 
-let
-  version = "tip";
-  homeDir = builtins.getEnv "HOME";
-in
-
 stdenv.mkDerivation rec {
   pname = "ghostty";
-  inherit version;
+  version = "tip";
 
-  # To install latest version locally:
-  #
-  # ```console
-  # mkdir -p ~/Applications
-  # cd ~/Applications
-  # rm -rf Ghostty.app
-  # curl -L \
-  #   https://github.com/ghostty-org/ghostty/releases/download/tip/ghostty-macos-universal.zip \
-  #   -o ghostty-macos-universal.zip && \
-  #   unzip -o ghostty-macos-universal.zip && \
-  #   rm ghostty-macos-universal.zip
-  # ```
-  src = builtins.path {
-    name = "ghostty-zip";
-    path = "${homeDir}/.ghostty/ghostty-macos-universal.zip";
+  src = fetchurl {
+    url = "https://github.com/ghostty-org/ghostty/releases/download/tip/ghostty-macos-universal.zip";
+    sha256 = "sha256-IBArVBC9/VrpQ/0EFkNj9jONw4A6ZGg9EYa8XUfBKPc=";
   };
 
   nativeBuildInputs = [ unzip ];
 
-  dontUnpack = true;
+  sourceRoot = ".";
+
+  unpackPhase = ''
+    unzip $src
+  '';
 
   installPhase = ''
     mkdir -p $out/Applications
-    cd $out/Applications
-    ${unzip}/bin/unzip ${src}
+    cp -r Ghostty.app $out/Applications
   '';
-
-  __noChroot = true;
-  preferLocalBuild = true;
-  allowSubstitutes = false;
 
   meta = with lib; {
     description = "A fast, feature-rich terminal emulator";
     homepage = "https://github.com/ghostty-org/ghostty";
-    license = licenses.mit;
     platforms = platforms.darwin;
+    license = licenses.mit;
+    maintainers = with maintainers; [ ivankovnatsky ];
   };
 }
