166ec128cff76b2e3b37e4d197df32c7e101f262
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-01-03 10:50:26 +0200

    Set download directory for download-torrent script

--- home/scripts/download-torrent.fish
+++ home/scripts/download-torrent.fish
@@ -4,6 +4,7 @@ function show_help
     echo "Usage: download-torrent [MAGNET_LINK|TORRENT_FILE]"
     echo ""
     echo "Downloads a torrent using aria2c from either a magnet link or torrent file"
+    echo "Default download location: ~/Downloads"
     echo ""
     echo "Arguments:"
     echo "  MAGNET_LINK    Magnet link starting with 'magnet:?'"
@@ -12,9 +13,15 @@ function show_help
 end
 
 # Show help if no arguments or help flags
-if test (count $argv) -ne 1; or contains -- "$argv[1]" "--help" "-help" "-h"
+if test (count $argv) -ne 1; or contains -- "$argv[1]" --help -help -h
     show_help
 end
 
-# Download using aria2c to current directory
-aria2c --dir ./ "$argv[1]"
+# Set default download directory to ~/Downloads
+set download_dir ~/Downloads
+
+# Create the download directory if it doesn't exist
+mkdir -p $download_dir
+
+# Download using aria2c to the specified download directory
+aria2c --dir $download_dir "$argv[1]"
