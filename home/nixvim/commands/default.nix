f4cf933ceae6ca87f437121931cfe65d588fe231
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-01-10 11:05:09 +0200

    nixvim: add CreatePr and MergePr commands

--- home/nixvim/commands/default.nix
+++ home/nixvim/commands/default.nix
@@ -2,13 +2,13 @@
   programs.nixvim.userCommands = {
     RebuildWatchman = {
       command = "botright split | terminal cd ~/Sources/github.com/ivankovnatsky/nixos-config && make rebuild-watchman";
-      desc = "Open terminal and run watchman rebuild command";
+      desc = "Open terminal and run watchman rebuild command.";
       bang = true;
       bar = true;
     };
     MouseToggle = {
       command = "if &mouse == 'a' | set mouse= | else | set mouse=a | endif";
-      desc = "Toggle mouse";
+      desc = "Toggle mouse.";
       bang = true;
       bar = true;
     };
@@ -17,13 +17,25 @@
         silent %y+
         lua require('notify')('Copied file contents to clipboard', 'info')
       '';
-      desc = "Copy file contents to clipboard";
+      desc = "Copy file contents to clipboard.";
       bang = true;
       bar = true;
     };
     ReplaceFileText = {
       command = "%d | put + | 0d | wall";
-      desc = "Replace text";
+      desc = "Replace text.";
+      bang = true;
+      bar = true;
+    };
+    CreatePr = {
+      command = "terminal create-pr";
+      desc = "Create PR with create-pr wrapper around gh CLI.";
+      bang = true;
+      bar = true;
+    };
+    MergePr = {
+      command = "terminal gh pr merge --squash";
+      desc = "Merge PR with gh CLI.";
       bang = true;
       bar = true;
     };
