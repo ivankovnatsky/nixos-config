099ce26513c28bb5219af482210ed38426d55e85
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-01-20 11:01:11 +0200

    open-gh-notifications: remove `--running` option

--- home/scripts/open-gh-notifications.fish
+++ home/scripts/open-gh-notifications.fish
@@ -4,15 +4,12 @@ function print_help
     echo "Usage: open-gh-notifications [OPTIONS]"
     echo ""
     echo "OPTIONS:"
-    echo "  -r, --running     Open notifications in a running browser window"
     echo "  -s, --show        Just print the URLs script would open in the browser"
     echo "  -h, --help        Display this help message"
     echo ""
     echo "Examples:"
     echo "  open-gh-notifications"
-    echo "  open-gh-notifications --running"
     echo "  open-gh-notifications --show"
-    echo "  open-gh-notifications --running --show"
 end
 
 function collect_urls
@@ -23,25 +20,15 @@ function collect_urls
 end
 
 function open_urls_in_batches
-    set -l urls $argv[1..-2]
-    set -l running $argv[-1]
+    set -l urls $argv[1..-1]
 
     test (count $urls) -eq 0; and return
 
-    if test "$running" = "true"
-        printf "%s\n" $urls | parallel --jobs 10 "open {}"
-    else
-        # Open first URL in new window
-        open -n $urls[1]
-        # Process remaining URLs in parallel if any exist
-        if test (count $urls) -gt 1
-            printf "%s\n" $urls[2..-1] | parallel --jobs 10 "open {}"
-        end
-    end
+    printf "%s\n" $urls | parallel --will-cite --jobs 10 "open {}"
 end
 
 function main
-    argparse h/help r/running s/show -- $argv
+    argparse h/help s/show -- $argv
     or begin
         print_help
         return 1
@@ -65,17 +52,13 @@ function main
     echo "URLs to open:"
     printf "%s\n" $all_urls
 
-    # Determine the mode (show vs open, and running vs new window)
+    # Determine the mode (show vs open)
     set -l action "Would open"
     set -l target "a new browser window"
     
-    if set -q _flag_running
-        set target "the current browser window"
-    end
-    
     if not set -q _flag_show
         set action "Opening"
-        open_urls_in_batches $all_urls (test -n "$_flag_running")
+        open_urls_in_batches $all_urls
     end
     
     echo "$action URLs in $target"
