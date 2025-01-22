c2feccfb144daa6b77661a012658fc5d0b430318
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-01-22 12:08:52 +0200

    nixvim: move all commands and keymaps to nix styled config

--- home/nixvim/keymaps/default.nix
+++ home/nixvim/keymaps/default.nix
@@ -54,5 +54,75 @@
         desc = "Save and close buffer";
       };
     }
+    # TODO: Fix visual mode selection to use actual selected text instead of word under cursor
+    # Possible approaches to investigate:
+    # 1. Use vim.fn.getline("'<", "'>") with proper handling
+    # 2. Create a helper function in Lua
+    # 3. Use vim.fn.visualmode() and vim.fn.getpos()
+    # 4. Look into how other plugins handle visual selections
+    {
+      mode = "n";
+      key = "<leader>rg";
+      action = ''
+        function()
+          require("telescope.builtin").live_grep({
+            default_text = vim.fn.expand("<cword>")
+          })
+        end
+      '';
+      lua = true;
+      options = {
+        silent = true;
+        desc = "Search word under cursor";
+      };
+    }
+    {
+      mode = "v";
+      key = "<leader>rg";
+      action = ''
+        function()
+          require("telescope.builtin").live_grep({
+            default_text = vim.fn.expand("<cword>")
+          })
+        end
+      '';
+      lua = true;
+      options = {
+        silent = true;
+        desc = "Search word under cursor";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fi";
+      action = ''
+        function()
+          require("telescope.builtin").find_files({
+            default_text = vim.fn.expand("<cword>")
+          })
+        end
+      '';
+      lua = true;
+      options = {
+        silent = true;
+        desc = "Find files with word under cursor";
+      };
+    }
+    {
+      mode = "v";
+      key = "<leader>fi";
+      action = ''
+        function()
+          require("telescope.builtin").find_files({
+            default_text = vim.fn.expand("<cword>")
+          })
+        end
+      '';
+      lua = true;
+      options = {
+        silent = true;
+        desc = "Find files with selected text";
+      };
+    }
   ];
 }
