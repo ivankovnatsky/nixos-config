a2e29c8e8cffb26686c73de8506814f912661174
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-01-22 19:27:16 +0200

    nixvim: update uk.utf-8.spl dictionary source

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -1,14 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 let
-  # FIXME: host could not be resolved?!
-  # nvim-spell-uk-utf8-dictionary = builtins.fetchurl {
-  #   url = "http://ftp.vim.org/vim/runtime/spell/uk.utf-8.spl";
-  #   sha256 = "05180znfdjwqhl2gfsq42jzwqadd7cgr59p9cvz6hw2dlnj6qs71";
-  # };
+  nvim-spell-uk-utf8-dictionary = builtins.fetchurl {
+    url = "https://ftp.nluug.nl/vim/runtime/spell/uk.utf-8.spl";
+    sha256 = "05180znfdjwqhl2gfsq42jzwqadd7cgr59p9cvz6hw2dlnj6qs71";
+  };
 in
 {
-  # home.file."${config.xdg.configHome}/nvim/spell/uk.utf-8.spl".source = nvim-spell-uk-utf8-dictionary;
+  home.file."${config.xdg.configHome}/nvim/spell/uk.utf-8.spl".source = nvim-spell-uk-utf8-dictionary;
   imports = [
     ./commands
     ./config
