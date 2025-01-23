c22677c1e174c7f22c2c19213be1f43864a84b52
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-01-23 17:01:14 +0200

    aichat: add launchd agent

---
+++ home/aichat-server.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  home.packages = [ pkgs.aichat ];
+
+  launchd.agents.aichat-server = {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        "${pkgs.aichat}/bin/aichat"
+        "--serve"
+      ];
+
+      KeepAlive = true;
+      RunAtLoad = true;
+
+      StandardOutPath = "/tmp/aichat-server.out.log";
+      StandardErrorPath = "/tmp/aichat-server.err.log";
+
+      EnvironmentVariables = {
+        HOME = config.home.homeDirectory;
+        PATH = lib.makeBinPath [ pkgs.aichat ];
+      };
+    };
+  };
+}
