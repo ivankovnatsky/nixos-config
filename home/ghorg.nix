30a874d25e1d3367f2d0382c39591555f79dd10f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-01-29 21:15:38 +0200

    home/ghorg: add skip archived

--- home/ghorg.nix
+++ home/ghorg.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 
 {
+  # https://github.com/gabrie30/ghorg/blob/master/sample-conf.yaml
   home.file.".config/ghorg/conf.yaml".text = ''
     # General Configuration
     GHORG_SCM_TYPE: github
@@ -10,6 +11,7 @@
     GHORG_FETCH_ALL: true
     GHORG_QUIET: true
     GHORG_ABSOLUTE_PATH_TO_CLONE_TO: ${config.home.homeDirectory}/Sources/github.com
+    GHORG_SKIP_ARCHIVED: true # Skip archived repositories
     
     # GitHub Configuration
     GHORG_GITHUB_TOKEN: ${config.secrets.githubToken}
