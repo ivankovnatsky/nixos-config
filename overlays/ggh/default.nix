db58cb4925af35aa43165a6f72888878e34f3e14
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-02-10 15:29:17 +0200

    machines/work: add ggh local overlay

---
+++ overlays/ggh/default.nix
@@ -0,0 +1,26 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+,
+}:
+buildGoModule rec {
+  pname = "ggh";
+  version = "0.1.4";
+
+  src = fetchFromGitHub {
+    owner = "byawitz";
+    repo = "ggh";
+    rev = "v${version}";
+    hash = "sha256-itNx/AcLUQCH99ZCOXiXPWNg3mx+UhHepidqmzPY8Oc=";
+  };
+
+  vendorHash = "sha256-WPPjpxCD3WA3E7lx5+DPvG31p8djera5xRn980eaJT8=";
+
+  meta = with lib; {
+    description = "Recall your SSH sessions";
+    homepage = "https://github.com/byawitz/ggh";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ ivankovnatsky ];
+    mainProgram = "ggh";
+  };
+}
