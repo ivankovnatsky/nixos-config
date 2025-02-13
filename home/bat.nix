2b7abe81a76f93a347f5416884e906fe2177f626
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-02-13 13:18:05 +0200

    home/bat: configure theming properly

--- home/bat.nix
+++ home/bat.nix
@@ -1,15 +1,13 @@
+{ pkgs, ... }:
 {
-  # FIXME: handle dark/light theme switch
-  programs.bat = {
-    enable = true;
-    config = {
-      theme = {
-        light = "GitHub";
-        dark = "Dracula";
-      };
-      # Other common settings
-      style = "numbers,changes,header";
-      italic-text = "always";
-    };
-  };
+  home.packages = [ pkgs.bat ];
+
+  # Manual configuration file for bat
+  home.file.".config/bat/config".text = ''
+    # Use Dracula theme for dark mode (default)
+    --theme-dark="Dracula"
+
+    # Use GitHub theme for light mode
+    --theme-light="GitHub"
+  '';
 }
