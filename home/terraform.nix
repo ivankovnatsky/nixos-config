7134d9b0e405644df1a9db639cb1f6258c48b3ea
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-02-14 17:17:08 +0200

    home/terraform.nix: pin both tf and tg to 1.9.8 and 0.68.7 respectively

--- home/terraform.nix
+++ home/terraform.nix
@@ -1,18 +1,39 @@
 { pkgs, ... }:
 
+let
+  # https://lazamar.co.uk/nix-versions/?package=terraform&version=1.9.8&fullName=terraform-1.9.8&keyName=terraform&revision=882842d2a908700540d206baa79efb922ac1c33d&channel=nixpkgs-unstable#instructions
+  # https://lazamar.co.uk/nix-versions/?package=terragrunt&version=0.68.7&fullName=terragrunt-0.68.7&keyName=terragrunt&revision=882842d2a908700540d206baa79efb922ac1c33d&channel=nixpkgs-unstable#instructions
+  pinnedPkgs =
+    import
+      (builtins.fetchGit {
+        # Descriptive name to make the store path easier to identify
+        name = "tf-1.9.8-tg-0.68.7";
+        url = "https://github.com/NixOS/nixpkgs/";
+        ref = "refs/heads/nixpkgs-unstable";
+        rev = "882842d2a908700540d206baa79efb922ac1c33d";
+      })
+      {
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = _: true;
+        };
+      };
+in
 {
   home = {
-    packages = with pkgs; [
+    packages = [
       # Manage terraform versions
       # https://github.com/tofuutils/tenv/issues/121
       #
       # ```console
       # Failure during terragrunt call : fork/exec /etc/profiles/per-user/Ivan.Kovnatskyi/bin/tenv: operation not supported by device
       # ```
-      tenv
-      # terraform
-      # terragrunt
-      tflint
+      # pkgs.tenv
+      # pkgs.terraform
+      # pkgs.terragrunt
+      pkgs.tflint
+      pinnedPkgs.terraform
+      pinnedPkgs.terragrunt
     ];
     file = {
       ".terraform.d/plugin-cache/.keep" = {
