3f6038cc8e6ee500e7fd862e039ff71cd3cbcfa5
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-05 18:55:33 +0200

    global: add home-manager and nixos rebuild changes diff

---
+++ home/rebuild-diff.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }:
+{
+  home.activation.report-changes = config.lib.dag.entryAnywhere ''
+    ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff $oldGenPath $newGenPath
+  '';
+}
