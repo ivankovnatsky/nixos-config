ba0f9f9ce2e1b8b8fd6ea09183f9b77866ac01bc
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-07 15:30:41 +0200

    machines/beelink/sonarr: add supplementary groups to Sonarr service

--- machines/beelink/sonarr.nix
+++ machines/beelink/sonarr.nix
@@ -9,5 +9,19 @@
     group = "sonarr";
     dataDir = "/var/lib/sonarr/.config/NzbDrone";
   };
-}
 
+  # Create media directories with correct permissions
+  systemd.tmpfiles.rules = [
+    "d /media/tv 0775 sonarr media -"  # Main TV shows directory
+    "d /media/downloads/movies/tv-sonarr 0775 transmission media -"  # Where Transmission puts downloaded TV shows
+  ];
+
+  # Ensure groups exist and users have correct permissions
+  users.groups.media.members = [ "sonarr" "transmission" "plex" ];
+
+  # Add supplementary groups to Sonarr service
+  systemd.services.sonarr.serviceConfig = {
+    SupplementaryGroups = [ "media" ];
+    UMask = "0002";
+  };
+}
