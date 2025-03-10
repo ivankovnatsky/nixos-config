64f149196fe4b8812e7ee8878d47871ef11501e6
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-10 21:46:42 +0200

    machines/mini: add nix-darwin syncthing service

---
+++ darwin/syncthing.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, lib, ... }:
+
+# Syncthing configuration for Darwin systems
+# 
+# Syncthing will be available at http://0.0.0.0:8384 after reboot or running:
+# launchctl kickstart -k gui/$(id -u)/net.syncthing.syncthing
+#
+# Configuration will be stored in ~/Library/Application Support/Syncthing
+# Log file will be at ~/Library/Logs/syncthing.log
+#
+# Note: This module only sets up the launchd service. You'll need to manually
+# configure Syncthing devices and folders using the web interface.
+
+let
+  username = "ivan";  # Set your username here
+  homeDir = "/Users/${username}";
+  configDir = "${homeDir}/Library/Application Support/Syncthing";
+  guiAddress = "0.0.0.0:8384";  # Accept connections from any interface
+in
+{
+
+  # Configure launchd service for Syncthing
+  launchd.user.agents.syncthing = {
+    serviceConfig = {
+      Label = "net.syncthing.syncthing";
+      ProgramArguments = [
+        "${pkgs.syncthing}/bin/syncthing"
+        "-no-browser"
+        "-no-restart"
+        "-logflags=0"
+      ];
+      EnvironmentVariables = {
+        # Set environment variables
+        STNOUPGRADE = "1";  # Disable automatic upgrades as it's managed by Nix
+        # GUI address is controlled by the user via the web interface
+      };
+      RunAtLoad = true;
+      KeepAlive = true;
+      StandardOutPath = "${homeDir}/Library/Logs/syncthing.log";
+      StandardErrorPath = "${homeDir}/Library/Logs/syncthing.log";
+      WorkingDirectory = homeDir;
+    };
+  };
+}
--- machines/Ivans-Mac-mini/default.nix
+++ machines/Ivans-Mac-mini/default.nix
@@ -1,14 +1,19 @@
 { pkgs, ... }:
 
 {
+  imports = [
+    # TODO: Create similar rebuild tmux service as in NixOS
+    ../../darwin/syncthing.nix
+  ];
+
   # error: Determinate detected, aborting activation
   # Determinate uses its own daemon to manage the Nix installation that
   # conflicts with nix-darwin’s native Nix management.
-  # 
+  #
   # To turn off nix-darwin’s management of the Nix installation, set:
-  # 
+  #
   #     nix.enable = false;
-  # 
+  #
   # This will allow you to use nix-darwin with Determinate. Some nix-darwin
   # functionality that relies on managing the Nix installation, like the
   # `nix.*` options to adjust Nix settings or configure a Linux builder,
@@ -16,9 +21,11 @@
   nix.enable = false;
 
   environment.systemPackages = with pkgs; [
+    # To avoid installing Developer Tools
+    gnumake
+
     rclone
-    syncthing
   ];
-  
+
   services.openssh.enable = true;
 }
