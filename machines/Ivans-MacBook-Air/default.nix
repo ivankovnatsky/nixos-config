d05df05413e254981239a93efce9b19c1be5a442
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-14 16:41:45 +0200

    machines/air: remove local-dns-resolver

--- machines/Ivans-MacBook-Air/default.nix
+++ machines/Ivans-MacBook-Air/default.nix
@@ -14,7 +14,6 @@ in
     ../../modules/darwin/dock
     ../../modules/darwin/pam
     ../../modules/darwin/sudo
-    ../../modules/darwin/local-dns-resolver
     ../../modules/flags
     ../../modules/secrets
     ../../darwin/darwin.nix
@@ -93,14 +92,6 @@ in
     };
     caskArgs = {
       no_quarantine = true;
-    };
-  };
-  services.local-dns-resolver = {
-    enable = true;
-    zones = {
-      "homelab" = {
-        nameserver = "192.168.50.169";
-      };
     };
   };
 
