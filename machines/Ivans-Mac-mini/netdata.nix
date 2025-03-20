2e923f3c91f225a29422cf852a92c681bda2ceba
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-20 21:33:41 +0200

    machines/beelink,mini: disable anon stats in netdata

--- machines/Ivans-Mac-mini/netdata.nix
+++ machines/Ivans-Mac-mini/netdata.nix
@@ -1,5 +1,7 @@
+{ ... }:
 {
-  services.netdata = {
-    enable = true;
-  };
+  services.netdata.enable = true;
+  
+  # Create the opt-out file in the Netdata configuration directory
+  environment.etc."netdata/.opt-out-from-anonymous-statistics".text = "";
 }
