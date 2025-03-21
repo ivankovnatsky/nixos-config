4fb133ae529ad1508c59472428c57c0e62da265c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-21 17:26:43 +0200

    machines/beelink: add home-assistant

--- machines/beelink/home-assistant.nix
+++ machines/beelink/home-assistant.nix
@@ -1,1 +1,53 @@
-{ }
+{ config, pkgs, ... }:
+{
+  services.home-assistant = {
+    enable = true;
+    package = (pkgs.home-assistant.override {
+      extraPackages = py: with py; [ 
+        psycopg2 
+        getmac
+        pyatv
+      ];
+    }).overrideAttrs (oldAttrs: {
+      doInstallCheck = false;
+    });
+    extraComponents = [
+      # Components required to complete the onboarding
+      "esphome"
+      "met"
+      "radio_browser"
+    ];
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = {};
+      recorder.db_url = "postgresql://@/hass";
+      
+      # Basic HTTP configuration
+      http = {
+        # Listen on localhost for proxy access
+        server_host = "127.0.0.1";
+        server_port = 8123;
+        # Enable proxy support
+        use_x_forwarded_for = true;
+        # Trust the Caddy proxy server
+        trusted_proxies = [
+          "127.0.0.1"
+          "::1"
+          "${config.flags.beelinkIp}"
+        ];
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8123 ];
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "hass" ];
+    ensureUsers = [{
+      name = "hass";
+      ensureDBOwnership = true;
+    }];
+  };
+}
