5f6a1943a6be3f33236b6cd027e9969aed3c7171
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-24 10:46:35 +0200

    machines/macs: disable compact tabs
    Still the compact tabs not really reliable with loosing focus and stuff.

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/system.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/system.nix
@@ -18,7 +18,7 @@
       CustomUserPreferences = {
         "com.apple.Safari" = {
           "ShowFullURLInSmartSearchField" = true;
-          "ShowStandaloneTabBar" = false; # false enables compact tabs
+          "ShowStandaloneTabBar" = true; # false enables compact tabs
         };
         "NSGlobalDomain" = {
           # My keyboard does not support Globe switch key, or I don't know how
