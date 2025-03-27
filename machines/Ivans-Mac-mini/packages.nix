7361c7c50ffa81f8c6e9762513c732635089fa3f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-27 11:49:17 +0200

    machines/mini/packages: re-arrange by logic

--- machines/Ivans-Mac-mini/packages.nix
+++ machines/Ivans-Mac-mini/packages.nix
@@ -2,10 +2,12 @@
 {
   environment.systemPackages = with pkgs; [
     gnumake # To avoid installing Developer Tools
-    rclone # Sync out stuff
     tmux # Be able to attach to tmux-rebuild session manually
     watchman # Be able to rerun rebuild manually with watching
     watchman-make # Be able to rerun rebuild manually with watching
+
+    rclone # Sync out stuff
+
     smctemp # Overlay
     stats # Desktop Application
     dust # For disk usage analysis
