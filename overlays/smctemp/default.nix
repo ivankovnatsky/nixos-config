cae8bbbed0733ae2455d4e652960c4fbf5df1190
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-03-31 12:24:18 +0300

    overlays/smctemp: update to use my own change
    It's not working currently.

--- overlays/smctemp/default.nix
+++ overlays/smctemp/default.nix
@@ -3,35 +3,34 @@
   stdenv,
   fetchFromGitHub,
   darwin,
+  gcc,
 }:
 
 stdenv.mkDerivation rec {
   pname = "smctemp";
-  version = "eebe38b4e27ca9a8b2caef0fda09694de5751874";
+  version = "39cc769b18237e153eb5d15464aa2946676b7142";
 
   src = fetchFromGitHub {
-    owner = "narugit";
+    owner = "ivankovnatsky";
     repo = pname;
     rev = version;
-    hash = "sha256-961N5bLZcBqdo3IKpIRxOCDLoG1+fL1YGJLsNfll7lE=";
+    hash = "sha256-cSQAWEBpCJHvH4vFnMgxYIv35qZWVJUgZryeBlBr6hs=";
   };
+
+  nativeBuildInputs = [
+    gcc
+    darwin.apple_sdk.frameworks.IOKit
+  ];
 
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.IOKit
   ];
 
-  # Fix the Makefile and code issues
   patchPhase = ''
-    # Fix the snprintf format string security issue
-    substituteInPlace smctemp.cc \
-      --replace 'snprintf(val.key, sizeof(val.key), key);' \
-                'snprintf(val.key, sizeof(val.key), "%s", key);'
-    
-    # Modify the Makefile to use the correct C++ compiler and install location
+    # Modify the Makefile to use the correct install location
     substituteInPlace Makefile \
-      --replace 'CXX := g++' 'CXX := c++' \
-      --replace 'CXXFLAGS := -Wall -std=c++17 -g -framework IOKit' \
-                'CXXFLAGS := -Wall -std=c++17 -g -framework IOKit -Wno-deprecated-declarations -Wno-format-security' \
+      --replace 'CXXFLAGS := -Wall -std=c++17 -g' \
+                'CXXFLAGS := -Wall -std=c++17 -g -Wno-deprecated-declarations -Wno-format-security' \
       --replace 'DEST_PREFIX := /usr/local' 'DEST_PREFIX := $(PREFIX)' \
       --replace 'PROCESS_IS_TRANSLATED := $(shell sysctl -in sysctl.proc_translated)' \
                 'PROCESS_IS_TRANSLATED := 0'
@@ -40,6 +39,12 @@ stdenv.mkDerivation rec {
   makeFlags = [
     "PREFIX=${placeholder "out"}"
   ];
+
+  buildPhase = ''
+    export CXXFLAGS="-Wall -std=c++17 -g -I${darwin.apple_sdk.frameworks.IOKit}/Library/Frameworks/IOKit.framework/Headers"
+    export LDFLAGS="-F${darwin.apple_sdk.frameworks.IOKit}/Library/Frameworks -framework IOKit"
+    make
+  '';
 
   meta = with lib; {
     description = "CLI tool to print CPU and GPU temperature on macOS";
