159a8554e488fc298cfefc8b3131ba5339a1a875
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-04-03 07:50:58 +0300

    machines/mini: disable Apple Intelligence

---
+++ machines/Ivans-Mac-mini/system.nix
@@ -0,0 +1,15 @@
+{
+  system = {
+    defaults = {
+      CustomUserPreferences = {
+        # Disable Apple Intelligence
+        # FIXME: Storage is not cleaned yet!
+        # https://www.reddit.com/r/MacOS/comments/1id8tns/turning_off_apple_intelligence_from_terminal/
+        "com.apple.CloudSubscriptionFeatures.optIn" = {
+          "device" = false;
+          "auto_opt_in" = false;
+        };
+      };
+    };
+  };
+}
