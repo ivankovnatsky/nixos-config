a2fa6bae8386fd62c5c828fa47b5253a6bed99a7
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-04-05 09:24:34 +0300

    home/scripts/power-off-homelab: init

---
+++ home/scripts/power-off-homelab.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Script to remotely power off homelab machines
+# Usage: ./power-off-homelab.sh
+# This script will power off both the Bee machine and Mac Mini
+
+set -e
+
+# Define machines list
+HOSTS=(
+  "bee"
+  "ivans-mac-mini"
+)
+
+# Function to power off a specific machine
+power_off_machine() {
+  local host=$1
+  ssh "$host" "sudo shutdown -h now"
+}
+
+# Power off all machines in the list
+for host in "${HOSTS[@]}"; do
+  power_off_machine "$host"
+done
