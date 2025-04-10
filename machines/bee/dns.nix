af1de9c6e22923c6ac6ae2742863b4269dea0f46
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-04-10 18:13:35 +0300

    modules/secrets: add externalDomain option

--- machines/bee/dns.nix
+++ machines/bee/dns.nix
@@ -102,7 +102,8 @@
 
       # Wildcard domain support
       address = [
-        "/homelab/${config.flags.beeIp}" # This will match all *.homelab domains
+        "/homelab/${config.flags.beeIp}" # This will match all *.homelab records
+        "/${config.secrets.externalDomain}/${config.flags.beeIp}" # This will match all *.externalDomain records
       ];
 
       # Log queries (useful for debugging)
