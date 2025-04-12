1cb2dc3e1883c26918ec18f4ad560a4112942b7e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-04-12 11:32:08 +0300

    machines/bee/sudo: disable password timeout

--- machines/bee/sudo.nix
+++ machines/bee/sudo.nix
@@ -5,6 +5,8 @@
     extraConfig = ''
       # Set password timeout to 4 hours (240 minutes)
       Defaults timestamp_timeout=240
+      # Wait indefinitely for password input
+      Defaults passwd_timeout=0
     '';
 
     # Configure NOPASSWD rules for specific commands
