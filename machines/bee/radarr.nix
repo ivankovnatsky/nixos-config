c16f584151f6d8eae7e43d8d4625c65f2c5ce453
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-04-15 13:39:36 +0300

    machines/bee: change media group paths and add jellyfin to media groups

--- machines/bee/radarr.nix
+++ machines/bee/radarr.nix
@@ -13,8 +13,8 @@
   # Create media directories with correct permissions
   systemd.tmpfiles.rules = [
     "d /storage/media/movies 0775 radarr media -" # Main movies directory
-    "d /storage/media/downloads/movies 0775 transmission media -" # Where Transmission puts downloaded movies
-    "d /storage/media/downloads/movies/radarr 0775 transmission media -" # Radarr's download directory
+    "d /storage/media/downloads 0775 transmission media -" # Where Transmission puts downloaded movies
+    "d /storage/media/downloads/radarr 0775 transmission media -" # Radarr's download directory
   ];
 
   # Ensure groups exist and users have correct permissions
