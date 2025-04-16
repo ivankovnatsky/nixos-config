0b999ab1b46eecf7f31cce13915f722a24a1ae23
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-04-16 15:30:45 +0300

    darwin/dock: rename chatgpt and claude to have web suffix

--- darwin/dock.nix
+++ darwin/dock.nix
@@ -80,8 +80,8 @@ in
       { path = "${homePath}/Applications/X.app/"; }
       { path = "${homePath}/Applications/WhatsApp Web.app/"; }
       { path = "${homePath}/Applications/Telegram Web.app/"; }
-      { path = "${homePath}/Applications/ChatGPT.app/"; }
-      { path = "${homePath}/Applications/Claude.app/"; }
+      { path = "${homePath}/Applications/ChatGPTWeb.app/"; }
+      { path = "${homePath}/Applications/ClaudeWeb.app/"; }
       { path = "${homePath}/Applications/OLX.ua.app/"; }
 
       {
