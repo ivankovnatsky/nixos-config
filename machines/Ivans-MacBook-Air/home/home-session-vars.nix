21456dd631eaec00179b59ee9a66fe060b00af32
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-04-16 09:45:48 +0300

    modules/secrets: add audiobookshelf api key

--- machines/Ivans-MacBook-Air/home/home-session-vars.nix
+++ machines/Ivans-MacBook-Air/home/home-session-vars.nix
@@ -4,5 +4,6 @@
     EDITOR = config.flags.editor;
     ANTHROPIC_API_KEY = "${config.secrets.anthropicApiKey}";
     OPENAI_API_KEY = "${config.secrets.openaiApiKey}";
+    ABS_API_KEY = "${config.secrets.audioBookShelfApiKey}";
   };
 }
