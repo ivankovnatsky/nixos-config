0f6859b9c1bcaaa391be1d90b19940d4386dfa54
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-04-28 21:17:42 +0300

    home/scripts/backup-home-simple: make pv show progress

--- home/scripts/backup-home-simple.sh
+++ home/scripts/backup-home-simple.sh
@@ -7,14 +7,19 @@ set -euo pipefail
 
 cd $HOME/../ || exit 1
 
+# Get size estimate with dust in bytes, removing 'B' suffix
+TOTAL_SIZE=$(dust -r -b -o b $HOME 2>/dev/null | head -1 | awk '{print $1}' | sed 's/B$//')
+
 tar \
     --exclude='**/*.sock' \
+    --exclude='**/*.socket' \
     --exclude='**/.gnupg/S.*' \
     --exclude='**/.ollama/**' \
     --exclude='**/.cache/nix/**' \
     --exclude='**/Library/Group Containers/group.com.apple.CoreSpeech/**' \
     --exclude='**/Library/Group Containers/group.com.apple.secure-control-center-preferences/**' \
+    --no-xattrs \
     -c \
     $USER | \
-    pv | \
+    pv -s $TOTAL_SIZE -petrb | \
     pigz > /tmp/$USER.tar.gz
