240a2396979bd629aebb580ff4d7b9883c91ea7a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-05-05 16:22:25 +0300

    home/scripts/backup-home-simple: remove pv and add excludes

--- home/scripts/backup-home-simple.sh
+++ home/scripts/backup-home-simple.sh
@@ -1,25 +1,42 @@
 #!/usr/bin/env bash
 
 # The purpose of this script is to only exclude directories to which user does
-# not have access in macOS.
+# not have access in macOS and which contain some data that is not needed, like
+# VMs and so.
+
+# https://stackoverflow.com/a/984259
 
 set -euo pipefail
 
-cd $HOME/../ || exit 1
-
-# Get size estimate with dust in bytes, removing 'B' suffix
-TOTAL_SIZE=$(dust -r -b -o b $HOME 2>/dev/null | head -1 | awk '{print $1}' | sed 's/B$//')
+cd "$HOME/../" || exit 1
 
 tar \
+    --exclude='**/.cache/huggingface/**' \
+    --exclude='**/.cache/nix/**' \
+    --exclude='**/.cursor/extensions/**' \
+    --exclude='**/.gnupg/S.*' \
+    --exclude='**/.npm/_cacache/**' \
+    --exclude='**/.npm/lib/**' \
+    --exclude='**/.ollama/**' \
+    --exclude='**/.terraform.d/**' \
+    --exclude='**/.Trash/**' \
     --exclude='**/*.sock' \
     --exclude='**/*.socket' \
-    --exclude='**/.gnupg/S.*' \
-    --exclude='**/.ollama/**' \
-    --exclude='**/.cache/nix/**' \
+    --exclude='**/go/**' \
+    --exclude='**/Group Containers/HUAQ24HBR6.dev.orbstack/**' \
+    --exclude='**/Library/Application Support/Google/Chrome/**' \
+    --exclude='**/Library/Caches/Google/Chrome/**' \
+    --exclude='**/Library/Caches/pypoetry/**' \
+    --exclude='**/Library/Containers/com.apple.Safari/**' \
+    --exclude='**/Library/Containers/com.apple.wallpaper.extension.video/**' \
+    --exclude='**/Library/Group Containers/BJ4HAAB9B3.ZoomClient3rd/**' \
     --exclude='**/Library/Group Containers/group.com.apple.CoreSpeech/**' \
     --exclude='**/Library/Group Containers/group.com.apple.secure-control-center-preferences/**' \
+    --exclude='**/OrbStack/**' \
+    \
     --no-xattrs \
-    -c \
-    $USER | \
-    pv -s $TOTAL_SIZE -petrb | \
-    pigz > /tmp/$USER.tar.gz
+    \
+    -cv \
+    "$USER" | \
+    \
+    pigz > "/tmp/$USER.tar.gz"
