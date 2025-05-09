c4876783d6a38c4ffe7b08c30a5f2c80e64dbc27
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-05-09 07:20:55 +0300

    machines/pro/darwin/brew: add handbrake

--- machines/Ivans-MacBook-Pro/darwin/default.nix
+++ machines/Ivans-MacBook-Pro/darwin/default.nix
@@ -4,7 +4,6 @@
     ../../../darwin/aerospace.nix
     ../../../darwin/darwin.nix
     ../../../darwin/dock.nix
-    ../../../darwin/homebrew.nix
     ../../../modules/darwin/dock
     ../../../modules/darwin/pam
     ../../../modules/darwin/sudo
@@ -16,6 +15,7 @@
     ./cloudflared.nix
     ./flags.nix
     ./fonts.nix
+    ./homebrew.nix
     ./security.nix
     ./shell.nix
     ./sudo.nix
