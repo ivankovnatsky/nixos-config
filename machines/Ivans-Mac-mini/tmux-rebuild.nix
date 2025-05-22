cb1c906d7f8d4ee051c443835758046bdfb17962
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-05-22 16:10:19 +0300

    modules/darwin/tmux-rebuild: add useSudo option

--- machines/Ivans-Mac-mini/tmux-rebuild.nix
+++ machines/Ivans-Mac-mini/tmux-rebuild.nix
@@ -3,5 +3,8 @@
     ../../modules/darwin/tmux-rebuild
   ];
 
-  local.services.tmuxRebuild.nixosConfigPath = "/Volumes/Storage/Data/Sources/github.com/ivankovnatsky/nixos-config";
+  local.services.tmuxRebuild = {
+    nixosConfigPath = "/Volumes/Storage/Data/Sources/github.com/ivankovnatsky/nixos-config";
+    useSudo = false;
+  };
 }
