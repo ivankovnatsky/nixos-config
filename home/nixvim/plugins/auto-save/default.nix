b2483a9610ff1ce39a8cc161b0c8ef9e78f6027e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-05-23 15:20:25 +0300

    home/nixvim/plugins/auto-save: init and add to work machine

---
+++ home/nixvim/plugins/auto-save/default.nix
@@ -0,0 +1,35 @@
+{
+  programs.nixvim.plugins.auto-save = {
+    enable = true;
+    settings = {
+      trigger_events = {
+        immediate_save = [
+          "BufLeave"
+          "FocusLost"
+        ];
+        defer_save = [
+          "InsertLeave"
+          "TextChanged"
+        ];
+        cancel_deferred_save = [ "InsertEnter" ];
+      };
+      write_all_buffers = false;
+      noautocmd = false;
+      lockmarks = false;
+      debounce_delay = 1000;
+      debug = false;
+
+      condition = ''
+        function(buf)
+          local fn = vim.fn
+          local utils = require("auto-save.utils.data")
+
+          if utils.not_in(fn.getbufvar(buf, "&filetype"), {'oil'}) then
+            return true
+          end
+          return false
+        end
+      '';
+    };
+  };
+}
