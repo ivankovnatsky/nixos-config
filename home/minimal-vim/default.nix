387888fabddddb9776124cbf1449fcde58d0a82a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-05-29 12:09:54 +0300

    machines/workstation/home: add minimal-vim

--- home/minimal-vim/default.nix
+++ home/minimal-vim/default.nix
@@ -1,5 +1,9 @@
 {
   home.file.".vimrc".text = ''
+    " Load vim defaults (includes cursor position restoration and many sensible defaults)
+    source $VIMRUNTIME/defaults.vim
+
+    " Our custom settings
     " Disable swap files
     set noswapfile
 
