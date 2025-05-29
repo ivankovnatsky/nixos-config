ea1ae357a6f172ca7c6f3531b67f843fc92b7eef
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-05-29 12:15:55 +0300

    home/minimal-vim: remove defaults sourcing

--- home/minimal-vim/default.nix
+++ home/minimal-vim/default.nix
@@ -1,8 +1,5 @@
 {
   home.file.".vimrc".text = ''
-    " Load vim defaults (includes cursor position restoration and many sensible defaults)
-    source $VIMRUNTIME/defaults.vim
-
     " Our custom settings
     " Disable swap files
     set noswapfile
