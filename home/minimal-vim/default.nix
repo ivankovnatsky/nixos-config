dc063259c38d2b70d363a7f8c29c9df97f732236
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-05-30 14:23:58 +0300

    home/minimal-vim: source default config as well and add auto-save back

--- home/minimal-vim/default.nix
+++ home/minimal-vim/default.nix
@@ -1,7 +1,14 @@
 {
   home.file.".vimrc".text = ''
+    " Load vim defaults (normally auto-loaded when no vimrc exists, but we need
+    " to explicitly source since we're creating one)
+    source $VIMRUNTIME/defaults.vim
+
     " Our custom settings
     " Disable swap files
     set noswapfile
+
+    " Auto save on text changes
+    autocmd TextChanged,TextChangedI <buffer> silent write
   '';
 }
