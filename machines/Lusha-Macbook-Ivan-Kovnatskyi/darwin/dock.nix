6e590d53828c9ad2efd10fb5a156d663ef5bec82
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-06-03 14:34:02 +0300

    machines/workstation/darwin/dock: add Тривога

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/dock.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/dock.nix
@@ -75,6 +75,13 @@ in
       { path = "${homePath}/Applications/Claude Web.app/"; }
 
       {
+        type = "spacer";
+        section = "apps";
+      }
+
+      { path = "${homePath}/Applications/Тривога.app/"; }
+
+      {
         path = "${config.users.users.${username}.home}/Downloads/";
         section = "others";
       }
