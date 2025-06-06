4d048e95b959da17345ce7c9ebb92a02a58532cb
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-06-06 14:55:27 +0300

    Reapply "machines/workstation/home/default: remove nixvim auto-save plugin"
    This reverts commit d569adc86d89ec5fddd6d5f94523aa1264953616.

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/home/default.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/home/default.nix
@@ -17,7 +17,6 @@
     ../../../home/minimal-vim
     ../../../home/mpv.nix
     ../../../home/nixvim
-    ../../../home/nixvim/plugins/auto-save
     ../../../home/nixvim/plugins/avante
     ../../../home/nixvim/plugins/copilot-lua
     ../../../home/nixvim/plugins/gen-nvim
