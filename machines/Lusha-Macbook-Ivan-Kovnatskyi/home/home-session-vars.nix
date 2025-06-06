ae6edf074ba4a7ad35cc487cb911849d748f1fc4
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-06-06 14:29:05 +0300

    Revert "machines/work/home/home-session-vars: remove EDITOR and VISUAL"
    This reverts commit 9b2360b5fb1826a2be7d168354f20cee7390508d.

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/home/home-session-vars.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/home/home-session-vars.nix
@@ -2,6 +2,8 @@
 {
   home = {
     sessionVariables = {
+      EDITOR = config.flags.editor;
+      VISUAL = config.flags.editor;
       OPENAI_API_KEY = "${config.secrets.openaiApiKey}";
       ANTHROPIC_API_KEY = "${config.secrets.anthropicApiKey}";
     };
