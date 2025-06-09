035b357ccb11b0b764384fda34ac85a8a44bbebb
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-06-09 12:16:23 +0300

    machines/nixos: init
    Example of OrbStack VM for NixOS.

---
+++ docs/orb-vm.md
@@ -0,0 +1,11 @@
+# orb-vm
+
+## Prepare
+
+```console
+cp /etc/nixos/* /mnt/mac/Users/$USER/Sources/github.com/ivankovnatsky/nixos-config/machines/$HOSTNAME/
+
+sudo nix-env -iA nixos.gnumake
+sudo nix-env -iA nixos.tmux
+make rebuild-nixos/generic
+```
--- flake/machines/nixos.nix
+++ flake/machines/nixos.nix
@@ -29,4 +29,34 @@
       username = "ivan";
     };
   };
+
+  "nixos" = inputs.nixos-release.lib.nixosSystem {
+    system = "aarch64-linux";
+    modules = [
+      # Import machine-specific configuration
+      ../../machines/nixos
+
+      # Basic system configuration
+      {
+        nixpkgs.overlays = [ inputs.self.overlay ];
+        nixpkgs.config.allowUnfree = true;
+        nix.nixPath = [
+          "nixpkgs=${inputs.nixos-release}"
+          "nixos-release=${inputs.nixos-release}"
+        ];
+        _module.args = {
+          flake-inputs = inputs;
+        };
+
+        # System settings
+        networking.hostName = "nixos";
+        users.users.ivan.home = "/home/ivan";
+        system.stateVersion = "24.11";
+      }
+    ];
+    specialArgs = {
+      system = "aarch64-linux";
+      username = "ivan";
+    };
+  };
 }
