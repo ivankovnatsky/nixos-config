6154bb6b33b9b7f8c296576fa33a407633c6f6aa
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-06-21 18:48:19 +0300

    darwin/dock.nix: update Steam.app path

--- darwin/dock.nix
+++ darwin/dock.nix
@@ -80,7 +80,7 @@ in
       }
 
       { path = "/Applications/Discord.app/"; }
-      { path = "${homePath}/Library/Application Support/Steam/Steam.AppBundle/Steam/"; }
+      { path = "/Applications/Steam.app/"; }
       { path = "/Applications/GeForceNOW.app/"; }
 
       {
