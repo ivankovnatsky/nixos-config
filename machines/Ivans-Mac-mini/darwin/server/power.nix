3aec13163516735165a7608ce81e672e86c4420b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-07-02 21:12:01 +0300

    machines/mini/darwin/server/power: prevent sleep

--- machines/Ivans-Mac-mini/darwin/server/power.nix
+++ machines/Ivans-Mac-mini/darwin/server/power.nix
@@ -2,8 +2,8 @@
   # https://github.com/nix-darwin/nix-darwin/blob/master/modules/power/sleep.nix
   # To prevent sleep, place "never".
   power.sleep = {
-    computer = 1; # default: 1
-    display = 10; # default: 10
-    harddisk = 10; # default: 10
+    computer = "never"; # default: 1
+    display = "never"; # default: 10
+    harddisk = "never"; # default: 10
   };
 }
