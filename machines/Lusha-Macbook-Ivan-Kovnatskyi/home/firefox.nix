15a60b232be73fc8908dea681da82415fd44f3a8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-07-23 21:47:36 +0300

    machines/work/home/firefox.nix: add ctrlTab all options to enable

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/home/firefox.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/home/firefox.nix
@@ -9,15 +9,16 @@ in
     "Library/Application Support/Firefox/Profiles/${firefoxProfile}/user.js".text = ''
       // Disable extension signature requirement for Tweety
       user_pref("xpinstall.signatures.required", false);
-      
+
       // Enable vertical tabs
       user_pref("sidebar.verticalTabs", true);
-      
+
       // Restore previous session (tabs and windows)
       user_pref("browser.startup.page", 3);
-      
+
       // Enable Ctrl+Tab to cycle through recent tabs
       user_pref("browser.ctrlTab.recentlyUsedOrder", true);
+      user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
     '';
 
     # Install Tweety extension automatically
