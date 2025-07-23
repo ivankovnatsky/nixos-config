2b28d3ca85e430f6342dde43c45df143f226cf14
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-07-23 22:27:39 +0300

    machines/mini: add gemini-cli and use claude sub for claude code

--- home/home-session-vars.nix
+++ home/home-session-vars.nix
@@ -3,8 +3,8 @@
   home.sessionVariables = {
     EDITOR = config.flags.editor;
     VISUAL = config.flags.editor;
-    ANTHROPIC_API_KEY = "${config.secrets.anthropicApiKey}";
     OPENAI_API_KEY = "${config.secrets.openaiApiKey}";
     ABS_API_KEY = "${config.secrets.audioBookShelfApiKey}";
+    GOOGLE_CLOUD_PROJECT = "${config.secrets.googleCloudProject}";
   };
 }
