da30c1c85f0ca528ddee9e9bd1540fdac20981a0
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-07-24 10:55:06 +0300

    home/scripts/tac.sh: use absolute path for tail on macOS
    This will make it will not use nixpkgs gnu coreutils.

--- home/scripts/tac.sh
+++ home/scripts/tac.sh
@@ -2,7 +2,7 @@
 
 # Check if we're on Darwin/macOS
 if [[ "$(uname)" == "Darwin" ]]; then
-    tail -r "$@"
+    /usr/bin/tail -r "$@"
 else
     # On Linux systems, use the real tac command
     command tac "$@"
