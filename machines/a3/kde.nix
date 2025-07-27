ede518b3758fd0b8eec1f3a58d0e0a3d8fcaa65a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-07-27 20:03:57 +0300

    machines/a3/kde: add rustication

--- machines/a3/kde.nix
+++ machines/a3/kde.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Trying out Plasma because it handles Steam games at higher than 1920x1080
+  # resolutions much expected, whereas in GNOME all games are capped at that
+  # resolution.
+
+  # And overall I see that configuring Qt apps are much more pleasant than GNOME.
   services = {
     desktopManager.plasma6.enable = true;
     displayManager.sddm.enable = true;
