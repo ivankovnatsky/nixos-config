1c2652fc20bf716891e572a1f446395c3b891b1f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-07-30 10:42:51 +0300

    machines/a3/home/firefox: update to use nixpkgs-master

--- machines/a3/home/firefox.nix
+++ machines/a3/home/firefox.nix
@@ -2,33 +2,33 @@
 
 {
   home.packages = [ pkgs.tweety ];
-  
+
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-devedition;
+    package = pkgs.nixpkgs-master.firefox-devedition;
     profiles."dev-edition-default" = {
       settings = {
         # Disable extension signature requirement for Tweety
         "xpinstall.signatures.required" = false;
-        
+
         # Enable vertical tabs
         "sidebar.verticalTabs" = true;
-        
+
         # Restore previous session (tabs and windows)
         "browser.startup.page" = 3;
-        
+
         # Enable Ctrl+Tab to cycle through recent tabs
         "browser.ctrlTab.recentlyUsedOrder" = true;
         "browser.ctrlTab.sortByRecentlyUsed" = true;
       };
-      
+
       extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
         darkreader
         bitwarden
       ];
     };
   };
-  
+
   home.file = {
     # Install Tweety extension manually
     ".mozilla/firefox/dev-edition-default/extensions/tweety@pomdtr.me.xpi" = {
