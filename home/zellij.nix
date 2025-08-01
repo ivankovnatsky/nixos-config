ba62fb0dbd9b5d945beba70ab90f92684706f9d3
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-01 11:56:31 +0300

    home/zellij: add copy_command setting

--- home/zellij.nix
+++ home/zellij.nix
@@ -5,12 +5,19 @@
     # zesh
   ];
   
+  # https://github.com/nix-community/home-manager/blob/master/modules/programs/zellij.nix
+  # ```console
+  # zellij setup --dump-config
+  # ```
   programs.zellij = {
     enable = true;
     
     settings = {
       # pane_frames = false;
       # simplified_ui = true;
+      
+      # Copy to clipboard configuration
+      copy_command = if pkgs.stdenv.isDarwin then "pbcopy" else "wl-copy";
     };
   };
 }
