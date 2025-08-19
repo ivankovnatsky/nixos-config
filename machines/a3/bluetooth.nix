d1cc8b729aaa6cbe96e2d053415092fc953f7872
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-19 14:20:49 +0300

    machines/a3/bluetooth.nix: enable headphones
    https://wiki.nixos.org/wiki/Bluetooth#Enabling_A2DP_Sink.

--- machines/a3/bluetooth.nix
+++ machines/a3/bluetooth.nix
@@ -2,5 +2,11 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        Experimental = true;
+      };
+    };
   };
 }
