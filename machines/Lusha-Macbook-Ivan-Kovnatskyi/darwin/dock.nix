61174d47ee88475dbc1fbfa84ad28ba6e568c6ce
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-19 15:03:58 +0300

    machines/work/darwin/dock: re-format

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/dock.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/dock.nix
@@ -11,65 +11,67 @@ let
 in
 {
   local = {
-    dock.enable = true;
-    dock.username = username;
-    # TODO: can dock be stretched 100% horizontally?
-    dock.entries = [
-      # Default macOS apps
-      { path = "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app/"; }
-      { path = "/System/Applications/Calendar.app/"; }
-      { path = "/System/Applications/System Settings.app/"; }
+    dock = {
+      enable = true;
+      inherit username;
+      # TODO: can dock be stretched 100% horizontally?
+      entries = [
+        # Default macOS apps
+        { path = "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app/"; }
+        { path = "/System/Applications/Calendar.app/"; }
+        { path = "/System/Applications/System Settings.app/"; }
 
-      {
-        type = "spacer";
-        section = "apps";
-      }
+        {
+          type = "spacer";
+          section = "apps";
+        }
 
-      # Additional macOS apps
-      { path = "/System/Applications/Utilities/Terminal.app/"; }
-      { path = "/System/Applications/Preview.app"; }
-      { path = "/System/Applications/Passwords.app/"; }
-      { path = "/System/Library/CoreServices/Applications/Keychain Access.app"; }
-      { path = "/System/Applications/Utilities/Activity Monitor.app/"; }
+        # Additional macOS apps
+        { path = "/System/Applications/Utilities/Terminal.app/"; }
+        { path = "/System/Applications/Preview.app"; }
+        { path = "/System/Applications/Passwords.app/"; }
+        { path = "/System/Library/CoreServices/Applications/Keychain Access.app"; }
+        { path = "/System/Applications/Utilities/Activity Monitor.app/"; }
 
-      {
-        type = "spacer";
-        section = "apps";
-      }
+        {
+          type = "spacer";
+          section = "apps";
+        }
 
-      # Installed using Kandji
-      { path = "/Applications/Google Chrome.app/"; }
-      { path = "/Applications/Slack.app/"; }
-      { path = "/Applications/zoom.us.app/"; }
+        # Installed using Kandji
+        { path = "/Applications/Google Chrome.app/"; }
+        { path = "/Applications/Slack.app/"; }
+        { path = "/Applications/zoom.us.app/"; }
 
-      {
-        type = "spacer";
-        section = "apps";
-      }
+        {
+          type = "spacer";
+          section = "apps";
+        }
 
-      # Installed using homebrew
-      { path = "/Applications/Ghostty.app/"; }
-      { path = "/Applications/Firefox Developer Edition.app/"; }
-      { path = "/Applications/Chromium.app/"; }
-      { path = "/Applications/DBeaver.app/"; }
-      { path = "/Applications/Visual Studio Code.app/"; }
-      { path = "/Applications/Cursor.app/"; }
-      { path = "/Applications/Windsurf.app/"; }
-      { path = "/Applications/KeyCastr.app/"; }
+        # Installed using homebrew
+        { path = "/Applications/Ghostty.app/"; }
+        { path = "/Applications/Firefox Developer Edition.app/"; }
+        { path = "/Applications/Chromium.app/"; }
+        { path = "/Applications/DBeaver.app/"; }
+        { path = "/Applications/Visual Studio Code.app/"; }
+        { path = "/Applications/Cursor.app/"; }
+        { path = "/Applications/Windsurf.app/"; }
+        { path = "/Applications/KeyCastr.app/"; }
 
-      {
-        type = "spacer";
-        section = "apps";
-      }
+        {
+          type = "spacer";
+          section = "apps";
+        }
 
-      # Safari Web Apps
-      { path = "${homePath}/Applications/ChatGPT Web.app/"; }
-      { path = "${homePath}/Applications/Claude Web.app/"; }
+        # Safari Web Apps
+        { path = "${homePath}/Applications/ChatGPT Web.app/"; }
+        { path = "${homePath}/Applications/Claude Web.app/"; }
 
-      {
-        path = "${config.users.users.${username}.home}/Downloads/";
-        section = "others";
-      }
-    ];
+        {
+          path = "${config.users.users.${username}.home}/Downloads/";
+          section = "others";
+        }
+      ];
+    };
   };
 }
