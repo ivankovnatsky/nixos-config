cb6a4b7711977438cc039fbe50ea5b11ce674e91
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-19 16:48:22 +0300

    home/home-session-vars.nix: add anthropicApiKey back

--- home/home-session-vars.nix
+++ home/home-session-vars.nix
@@ -6,5 +6,6 @@
     OPENAI_API_KEY = "${config.secrets.openaiApiKey}";
     ABS_API_KEY = "${config.secrets.audioBookShelfApiKey}";
     GOOGLE_CLOUD_PROJECT = "${config.secrets.googleCloudProject}";
+    ANTHROPIC_API_KEY = "${config.secrets.anthropicApiKey}";
   };
 }
