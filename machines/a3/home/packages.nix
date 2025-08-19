0c0c6800ca48d967f62693fe385bb1491e62c7f9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-19 10:02:37 +0300

    machines/a3: remove windsurf

--- machines/a3/home/packages.nix
+++ machines/a3/home/packages.nix
@@ -6,15 +6,6 @@
     lsof  # List open files
     wl-clipboard  # Wayland clipboard utilities
 
-    # Add Wayland flags to Windsurf to fix scaling issues
-    # (windsurf.override {
-    #   # Use commandLineArgs to add Wayland flags
-    #   commandLineArgs = [
-    #     "--ozone-platform=wayland"
-    #   ];
-    # })
-    nixpkgs-master.windsurf
-
     # Hardware monitoring tools
     lm_sensors  # Provides the 'sensors' command for monitoring temperatures
     nixpkgs-master.claude-code
