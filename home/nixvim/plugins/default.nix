b05856c03cd04f74b963905dbf2c357b8cf5bbe2
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-25 18:29:55 +0300

    Revert "home/nixvim: remove cmp custom binds"
    This reverts commit 9ecc1d8f29972254724d4af005e1f74f4c7d8bf5.

--- home/nixvim/plugins/default.nix
+++ home/nixvim/plugins/default.nix
@@ -128,6 +128,7 @@
         enable = true;
         settings = {
           enable_autosnippets = true;
+          store_selection_keys = "<Tab>";
         };
         fromVscode = [
           {
@@ -180,6 +181,18 @@
               keywordLength = 3;
             }
           ];
+          mapping = {
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<C-j>" = "cmp.mapping.select_next_item()";
+            "<C-k>" = "cmp.mapping.select_prev_item()";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+          };
         };
       };
       cmp-nvim-lsp.enable = true; # lsp
