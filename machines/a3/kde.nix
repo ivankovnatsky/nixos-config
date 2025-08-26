f810b892e0d87ffdc66b7637c0c88e8ccb9fd987
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-26 16:49:03 +0300

    machines/a3/kde: add comment for gh issue for part manager

--- machines/a3/kde.nix
+++ machines/a3/kde.nix
@@ -13,5 +13,6 @@
   };
 
   # Enable KDE Partition Manager with proper D-Bus access
+  # https://github.com/NixOS/nixpkgs/issues/273659#issuecomment-1852402674
   programs.partition-manager.enable = true;
 }
