c2218a4594064a6315d6eca3b45f4193214020fd
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-26 15:32:00 +0300

    machines/a3/boot: set timeout to 1

--- machines/a3/boot.nix
+++ machines/a3/boot.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.loader.timeout = 0;
+  # Always use at least 1 second timeout to be able to boot from prev nix generation in case of nuclear war
+  boot.loader.timeout = 1;
 }
