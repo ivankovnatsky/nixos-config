dca2cd761982523c34023e4d38bcf8bd95bed902
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-26 16:08:21 +0300

    machines/a3/home/gpg: add pinentry-program

--- machines/a3/home/gpg.nix
+++ machines/a3/home/gpg.nix
@@ -8,13 +8,16 @@
   };
 
   services = {
-    # Configure GPG agent with Qt pinentry for KDE Wallet
+    # Configure GPG agent with kwallet pinentry for KDE Wallet integration
     gpg-agent = {
       enable = true;
-      pinentry.package = pkgs.pinentry-qt;
       enableSshSupport = true;
       defaultCacheTtl = 86400; # 24 hours
       maxCacheTtl = 86400;
+      extraConfig = ''
+        allow-preset-passphrase
+        pinentry-program ${pkgs.kwalletcli}/bin/pinentry-kwallet
+      '';
     };
   };
 }
