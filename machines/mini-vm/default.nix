1dad730f92252b19a926019ce6e3fda571c6359a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-29 11:04:54 +0300

    machine/bee/nix-serve.nix: init

--- machines/mini-vm/default.nix
+++ machines/mini-vm/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ../../modules/nixos/tmux-rebuild
+    ../../nixos/nix-cache.nix
     ./configuration.nix
     # ./open-webui.nix  # Temporarily disabled - chromadb package is broken
     ./tmux-rebuild.nix
