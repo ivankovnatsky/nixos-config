6e278e387686ffb54cf769e23decc592fe579696
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-29 11:10:44 +0300

    flake/machines/nixos.nix: remove orb-nixos and utm-nixos

--- flake/machines/nixos.nix
+++ flake/machines/nixos.nix
@@ -111,36 +111,6 @@
     };
   };
 
-  "orb-nixos" = inputs.nixos-release.lib.nixosSystem {
-    system = "aarch64-linux";
-    modules = [
-      # Import machine-specific configuration
-      ../../machines/orb-nixos
-
-      # Basic system configuration
-      {
-        nixpkgs.overlays = [ inputs.self.overlay ];
-        nixpkgs.config.allowUnfree = true;
-        nix.nixPath = [
-          "nixpkgs=${inputs.nixos-release}"
-          "nixos-release=${inputs.nixos-release}"
-        ];
-        _module.args = {
-          flake-inputs = inputs;
-        };
-
-        # System settings
-        networking.hostName = "orb-nixos";
-        users.users.ivan.home = "/home/ivan";
-        system.stateVersion = "24.11";
-      }
-    ];
-    specialArgs = {
-      system = "aarch64-linux";
-      username = "ivan";
-    };
-  };
-
   "mini-vm" = inputs.nixos-release.lib.nixosSystem {
     system = "aarch64-linux";
     modules = [
@@ -158,36 +128,6 @@
         _module.args = {
           flake-inputs = inputs;
         };
-      }
-    ];
-    specialArgs = {
-      system = "aarch64-linux";
-      username = "ivan";
-    };
-  };
-
-  "utm-nixos" = inputs.nixos-release.lib.nixosSystem {
-    system = "aarch64-linux";
-    modules = [
-      # Import machine-specific configuration
-      ../../machines/utm-nixos
-
-      # Basic system configuration
-      {
-        nixpkgs.overlays = [ inputs.self.overlay ];
-        nixpkgs.config.allowUnfree = true;
-        nix.nixPath = [
-          "nixpkgs=${inputs.nixos-release}"
-          "nixos-release=${inputs.nixos-release}"
-        ];
-        _module.args = {
-          flake-inputs = inputs;
-        };
-
-        # System settings
-        networking.hostName = "utm-nixos";
-        users.users.ivan.home = "/home/ivan";
-        system.stateVersion = "25.05";
       }
     ];
     specialArgs = {
