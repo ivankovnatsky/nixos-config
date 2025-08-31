de0d8631cc69d162f864bd0793c25b33f3082759
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-08-31 17:18:45 +0300

    nixos/dwm/files/suckless/st: make font 13

--- nixos/dwm/files/suckless/st/config.h
+++ nixos/dwm/files/suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:size=16:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font Mono:size=13:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
