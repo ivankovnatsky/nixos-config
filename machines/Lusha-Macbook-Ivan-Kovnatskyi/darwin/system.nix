ac3fcc48909b2e33bdefc127e22b5024a34fecd5
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-09-18 13:37:49 +0300

    machines/work/darwin/system: add auto icon style switching

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/system.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/system.nix
@@ -35,6 +35,8 @@
           "DeveloperPreferences" = 836;
         };
         "NSGlobalDomain" = {
+          # Auto-switch icon style based on appearance mode
+          AppleIconAppearanceTheme = "RegularAutomatic";
           # My keyboard does not support Globe switch key, or I don't know how
           # to use it, don't want to use karabiner-elements for now.
           "NSUserKeyEquivalents" = {
