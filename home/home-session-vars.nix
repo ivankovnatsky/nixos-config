91b6b5e8ae57b42614b63bbb0e07b65f22a2b861
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-09-29 16:39:46 +0300

    home/session-vars.nix: add abs url

--- home/home-session-vars.nix
+++ home/home-session-vars.nix
@@ -5,6 +5,7 @@
     VISUAL = config.flags.editor;
     OPENAI_API_KEY = "${config.secrets.openaiApiKey}";
     ABS_API_KEY = "${config.secrets.audioBookShelfApiKey}";
+    ABS_URL = "${config.secrets.audioBookShelfUrl}";
     GOOGLE_CLOUD_PROJECT = "${config.secrets.googleCloudProject}";
     ANTHROPIC_API_KEY = "${config.secrets.anthropicApiKey}";
     BW_SESSION = "${config.secrets.bitwardenSession}";
