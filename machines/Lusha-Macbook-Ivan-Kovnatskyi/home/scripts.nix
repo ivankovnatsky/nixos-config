5fd96a3de11876f7dca66192af833e79704ec843
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-03 13:32:18 +0300

    machines/work/scripts: add jira-custom tool
    - Add jira-custom.py for searching Jira filters via API
    - Create machine-specific scripts.nix with dependency handling
    - Uses JIRA_SERVER, JIRA_EMAIL, JIRA_API_TOKEN from environment
    - Python script wrapped with jira library using withPackages

---
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/home/scripts.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+let
+  scriptPath = ./scripts;
+
+  # Define scripts that need external dependencies
+  scriptsWithDeps = {
+    "jira-custom.py" = {
+      type = "python";
+      deps = ps: [ ps.jira ];
+    };
+  };
+
+  # Wrap Python script with dependencies
+  wrapPythonScript = scriptName: config:
+    let binaryName = pkgs.lib.removeSuffix ".py" scriptName;
+    in pkgs.writeShellScriptBin binaryName ''
+      exec ${pkgs.python3.withPackages config.deps}/bin/python ${scriptPath}/${scriptName} "$@"
+    '';
+
+  # Create packages for scripts with dependencies
+  depsPackages = builtins.attrValues (builtins.mapAttrs (name: config:
+    if config.type == "python" then
+      wrapPythonScript name config
+    else
+      throw "Unknown script type: ${config.type}"
+  ) scriptsWithDeps);
+in
+{
+  home.packages = depsPackages;
+}
