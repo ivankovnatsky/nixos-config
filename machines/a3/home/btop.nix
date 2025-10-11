ecdd2f0334f8bec2a8d618f8f971f04a2f914424
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-11 21:26:49 +0300

    machines/a3/home/btop.nix: add enable 🤦

--- machines/a3/home/btop.nix
+++ machines/a3/home/btop.nix
@@ -2,6 +2,7 @@
 
 {
   programs.btop = {
+    enable = true;
     # Enable NVIDIA GPU support (adds runtime path for nvidia-ml library)
     package = pkgs.btop.override {
       cudaSupport = true;
