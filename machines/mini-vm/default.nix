763b93bf5e6d6ccc8b2e65ddbfb013ddd37cd5c4
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-11 13:11:57 +0300

    machines/mini-vm: remove unnecessary packages and server config
    Remove packages.nix (gnumake), server directory (rebuild-diff), and audiobookshelf-mgmt module as part of VM decommissioning. Keep tmux-rebuild-poll for development convenience.

--- machines/mini-vm/default.nix
+++ machines/mini-vm/default.nix
@@ -3,12 +3,9 @@
     ../../modules/flags
     ../../modules/secrets
     ../../modules/nixos/tmux-rebuild-poll
-    ../../modules/nixos/audiobookshelf-mgmt
     ./networking.nix
     ./configuration.nix
     ./tmux-rebuild-poll.nix
-    ./packages.nix
     ./jellyfin.nix
-    ./server
   ];
 }
--- machines/mini-vm/server/default.nix
+++
@@ -1,5 +0,0 @@
-{
-  imports = [
-    ../../../nixos/rebuild-diff.nix
-  ];
-}
