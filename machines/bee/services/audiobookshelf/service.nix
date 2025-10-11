b15f7c7d7251eed038735d63de4d452945448fd1
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-11 10:07:05 +0300

    machines/bee/audiobookshelf: move podcasts from /storage to /var/lib
    Move podcast media directory from deprecated /storage/Data/media/podcasts
    to /var/lib/audiobookshelf/media/podcasts.
    
    Context: /storage mount was deprecated and removed (see STORAGE-DEPRECATION.md).
    The external Samsung T7 disk is no longer mounted on bee.
    
    Changes:
    - Update service.nix to create /var/lib/audiobookshelf/media/podcasts
    - Update mgmt.nix library folder path
    - Update comments with new path
    
    After rebuild, the podcasts directory will be created in the correct location.
    Existing /storage/Data directory (created by previous tmpfiles rules) can be
    removed manually.

--- machines/bee/services/audiobookshelf/service.nix
+++ machines/bee/services/audiobookshelf/service.nix
@@ -4,7 +4,7 @@
   # 1. Access the web UI at https://audiobookshelf.{externalDomain}
   # 2. Create your root user during the initial setup process
   # 3. Add libraries for your audiobooks and podcasts, pointing to:
-  #    - Podcasts library: /storage/Data/media/podcasts
+  #    - Podcasts library: /var/lib/audiobookshelf/media/podcasts
   # 4. Configure metadata agents and other settings according to your preferences
 
   # Enable Audiobookshelf server
@@ -28,13 +28,13 @@
   # Create media directories with correct permissions if they don't exist
   systemd.tmpfiles.rules = [
     # Create podcasts directory with proper ownership
-    "d /storage/Data/media/podcasts 2775 audiobookshelf media -"
+    "d /var/lib/audiobookshelf/media/podcasts 2775 audiobookshelf media -"
 
     # Set default ACLs for new files in the podcasts directory
-    "A+ /storage/Data/media/podcasts - - - - default:user::rwx,default:group::rwx,default:other::r-x"
+    "A+ /var/lib/audiobookshelf/media/podcasts - - - - default:user::rwx,default:group::rwx,default:other::r-x"
 
     # Set ACLs for existing files in the podcasts directory
-    "A+ /storage/Data/media/podcasts - - - - user::rwx,group::rwx,other::r-x"
+    "A+ /var/lib/audiobookshelf/media/podcasts - - - - user::rwx,group::rwx,other::r-x"
   ];
 
   # Make sure the media group exists
