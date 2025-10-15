c9fa229d0b18ac58a2ec42f4950719fc403d6f11
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-15 21:40:21 +0300

    machines/mini/darwin/sudo: add NOPASSWD:SETENV for darwin-rebuild
    Enable password-less sudo with environment preservation for
    darwin-rebuild switch command. Matches work machine configuration
    to avoid password prompts and allow sudo -E flag.

---
+++ machines/Ivans-Mac-mini/sudo.nix
@@ -0,0 +1,13 @@
+{ username, ... }:
+{
+  local.sudo = {
+    nopasswd = {
+      enable = true;
+      user = username;
+      setenv = true;
+      commands = [
+        "/run/current-system/sw/bin/darwin-rebuild switch *"
+      ];
+    };
+  };
+}
