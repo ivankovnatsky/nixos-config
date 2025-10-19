d4bbd5dc106b9c48b0d14fec620da29a1ab5610b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-19 11:24:29 +0300

    machines/a3/home/firefox: add ctrl+1-9 tab switching extension
    - Create firefox-selecttab overlay for custom extension enabling
      Ctrl+1-9 tab switching on Linux
    - Add Firefox policies to auto-install unsigned extensions (selecttab
      and tweety) without manual approval
    - Keep xpinstall.signatures.required=false only in dev-edition profile
    - Add Firefox Developer Edition to home.packages and Plasma dock
    - Install selecttab extension in both default and dev-edition profiles

---
+++ overlays/firefox-selecttab/default.nix
@@ -0,0 +1,40 @@
+{ lib, stdenv, zip }:
+
+# https://gist.github.com/zbraniecki/000268ea27154bbccaad190dd479d226
+stdenv.mkDerivation {
+  pname = "firefox-selecttab";
+  version = "1.0";
+
+  src = ./.;
+
+  nativeBuildInputs = [ zip ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p extension
+    cp ${./manifest.json} extension/manifest.json
+    cp ${./background.js} extension/background.js
+
+    cd extension
+    ${zip}/bin/zip -r ../firefox-selecttab.xpi .
+    cd ..
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/extensions
+    cp firefox-selecttab.xpi $out/share/extensions/firefox.zip
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Firefox extension to enable Ctrl+1-9 tab switching on Linux";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ivankovnatsky ];
+  };
+}
