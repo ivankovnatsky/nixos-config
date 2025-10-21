9d8a0c02126c9935e55b4df9ff841c6a7ca8d83f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-21 17:38:05 +0300

    machines/a3/nixos: move all system configs into subdirectory

--- machines/a3/default.nix
+++ machines/a3/default.nix
@@ -1,33 +1,5 @@
 {
   imports = [
-    ../../modules/secrets
-    ../../modules/flags
-
-    ../../modules/nixos/tmux-rebuild
-    ../../system/nix.nix
-    ../../system/scripts
-    ../../nixos/chromium.nix
-    ./bluetooth.nix
-    ./boot.nix
-    ./configuration.nix
-    ./default-apps.nix
-    ./cryptenroll.nix # TPM2 support for LUKS encryption
-    ./fonts.nix
-
-    # ./gamemode.nix
-    # ./gamescope.nix
-    ./desktop.nix
-
-    ./networking.nix
-    ./nixpkgs.nix
-    # ./nvidia.nix
-    ./power-management.nix
-    ./security.nix
-    # ./smb.nix
-    ./steam.nix
-    ./sudo.nix
-    ./tmux-rebuild.nix
-    ./tpm2.nix
-    ./user.nix
+    ./nixos
   ];
 }
---
+++ machines/a3/nixos/default.nix
@@ -0,0 +1,30 @@
+{
+  imports = [
+    # ./gamemode.nix
+    # ./gamescope.nix
+    # ./nvidia.nix
+    # ./smb.nix
+    ../../../modules/flags
+    ../../../modules/nixos/tmux-rebuild
+    ../../../modules/secrets
+    ../../../nixos/chromium.nix
+    ../../../system/nix.nix
+    ../../../system/scripts
+    ./bluetooth.nix
+    ./boot.nix
+    ./configuration.nix
+    ./cryptenroll.nix # TPM2 support for LUKS encryption
+    ./default-apps.nix
+    ./desktop.nix
+    ./fonts.nix
+    ./networking.nix
+    ./nixpkgs.nix
+    ./power-management.nix
+    ./security.nix
+    ./steam.nix
+    ./sudo.nix
+    ./tmux-rebuild.nix
+    ./tpm2.nix
+    ./user.nix
+  ];
+}
