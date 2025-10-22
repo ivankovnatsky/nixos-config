6119f5142aa1b0d179ada396edc2d0cd0ba8f602
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-22 20:51:07 +0300

    machines/bee/nixos: put back hixos/home structure

---
+++ machines/bee/nixos/default.nix
@@ -0,0 +1,41 @@
+{
+  imports = [
+    ../../../modules/flags
+    ../../../modules/nixos/audiobookshelf-mgmt
+    ../../../modules/nixos/beszel-agent
+    ../../../modules/nixos/mautrix-discord
+    ../../../modules/nixos/mautrix-linkedin
+    ../../../modules/nixos/mautrix-whatsapp
+    ../../../modules/nixos/tmux-rebuild
+    ../../../modules/secrets
+    ../../../modules/shared/arr-mgmt
+    ../../../modules/shared/nextdns-mgmt
+    ../../../nixos/rebuild-diff.nix
+    ../../../system/documentation.nix # Disable documentation to avoid mautrix module mismatch issues
+    ../../../system/nix.nix
+    ./audiobookshelf
+    ./beszel.nix # Monitoring
+    ./configuration.nix # Base configuration.
+    ./cryptenroll.nix # Uncomment after enrolling TPM2 (see docs/bee.md for instructions)
+    ./dns.nix
+    ./doh.nix
+    ./home-automation
+    ./http.nix
+    ./journald.nix # Logging
+    ./loader.nix
+    ./logrotate.nix
+    ./matrix
+    ./networking.nix # FIXME: Did not work yet
+    ./nextdns
+    ./open-webui.nix
+    ./packages.nix
+    ./power.nix
+    ./ssh.nix
+    ./sudo.nix # Security
+    ./syncthing.nix
+    ./tailscale.nix
+    ./tmux-rebuild.nix
+    ./tpm2.nix # Enable TPM2 support (required for TPM2 enrollment)
+    ./user.nix # User configuration
+  ];
+}
