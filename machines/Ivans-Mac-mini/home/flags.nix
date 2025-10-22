abef0591526b1ada3138b11934ff9f9e22d2305f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-22 21:01:45 +0300

    Revert "machines/mini/home: remove all home-manager configuration"
    This reverts commit b99a1d40b72f29ca5ba995930196c33a4b0f788f.

---
+++ machines/Ivans-Mac-mini/home/flags.nix
@@ -0,0 +1,39 @@
+{ config, ... }:
+{
+  flags = {
+    enableFishShell = true;
+    purpose = "home";
+    editor = "nvim";
+    darkMode = true;
+    homeWorkPath = "/Volumes/Storage/Data";
+    hotkeys = {
+      terminal = "Terminal";
+      browser = "Safari";
+      shortcuts = [
+        {
+          key = "1";
+          app = "Finder";
+        }
+        {
+          key = "2";
+          app = config.flags.hotkeys.terminal;
+        }
+        {
+          key = "3";
+          app = config.flags.hotkeys.browser;
+        }
+        {
+          key = "4";
+          app = "Cursor";
+        }
+        {
+          key = "9";
+          app = "System Settings";
+        }
+      ];
+    };
+    apps = {
+      vscode.enable = false;
+    };
+  };
+}
---
+++ machines/Ivans-Mac-mini/home/tmux.nix
@@ -0,0 +1,41 @@
+{ osConfig, ... }:
+
+{
+  home.file = {
+    ".config/tmuxinator/${osConfig.networking.hostName}-dev.yml" = {
+      text = ''
+        name: ${osConfig.networking.hostName}-dev
+        startup_window: 0
+        root: /Volumes/Storage/Data
+
+        windows:
+          - crypt:
+              root: /Volumes/Storage/Data/Drive/Crypt
+              layout: main-horizontal
+              panes:
+                - ls -lah
+                - make sync
+          - textcast:
+              root: /Volumes/Storage/Data/Textcast/Texts
+              layout: main-horizontal
+              panes:
+                - nvim Texts.txt
+                - |
+                  cd /Volumes/Storage/Data/Sources/github.com/ivankovnatsky/textcast
+                  make watch-cast
+          - audiobookshelf:
+              root: /Volumes/Storage/Data/AudioBookShelf
+              layout: main-horizontal
+              panes:
+                - nvim List.txt
+                - make watch
+          - youtube:
+              root: /Volumes/Storage/Data/Youtube
+              layout: main-horizontal
+              panes:
+                - nvim List.txt
+                - make watch
+      '';
+    };
+  };
+}
