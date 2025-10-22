07d32d6217ed320059decf1524e1e1f9598803b9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-22 21:46:23 +0300

    home/go: adapt to both current release and unstable

--- home/go.nix
+++ home/go.nix
@@ -1,5 +1,5 @@
 {
-  config,
+  lib,
   osConfig,
   pkgs,
   ...
@@ -10,15 +10,38 @@ let
   useAbsolutePath = hostName == "Ivans-Mac-mini";
   absoluteGoPath = "/Volumes/Storage/Data/go";
   relativeGoPath = "go";
+
+  # Machines using home-manager release (25.05) - use old goPath format
+  # Machines using home-manager unstable - use new env format
+  isRelease = lib.elem hostName [ "Ivans-Mac-mini" "bee" ];
 in
 {
-  programs.go = {
+  programs.go = if isRelease then {
+    # Old format for release (home-manager 25.05)
+    enable = true;
+    goPath = if useAbsolutePath then null else relativeGoPath;
+  } else {
+    # New format for unstable (home-manager with env support)
     enable = true;
     env = {
       GOPATH = if useAbsolutePath then absoluteGoPath else "$HOME/${relativeGoPath}";
       GO111MODULE = "on";
     };
   };
+
+  home.sessionVariables = if isRelease then (
+    {
+      GO111MODULE = "on";
+    }
+    // (
+      if useAbsolutePath then
+        {
+          GOPATH = absoluteGoPath;
+        }
+      else
+        { }
+    )
+  ) else { };
 
   home.sessionPath = [
     (if useAbsolutePath then "${absoluteGoPath}/bin" else "$HOME/${relativeGoPath}/bin")
