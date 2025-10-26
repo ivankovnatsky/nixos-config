d7cf859b63ffb17cc42d3a7a8093868a73d719d8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-10-26 12:06:25 +0200

    machines/bee/nixos/tmux-rebuild: disable in favor of rebuild-daemon

--- machines/bee/nixos/tmux-rebuild.nix
+++ machines/bee/nixos/tmux-rebuild.nix
@@ -1,8 +1,9 @@
 { username, ... }:
 {
   # Configure the tmux rebuild service
+  # Disabled in favor of rebuild-daemon
   local.services.tmuxRebuild = {
-    enable = true;
+    enable = false;
     username = username; # Use the username variable from the flake
     nixosConfigPath = "/home/ivan/Sources/github.com/ivankovnatsky/nixos-config";
   };
