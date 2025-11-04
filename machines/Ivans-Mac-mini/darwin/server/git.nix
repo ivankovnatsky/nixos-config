68b6f233024f164c54886f3295642a159c5de2bb
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-11-04 15:01:38 +0200

    machines/mini/darwin/server/git: add wildcard for worktrees

--- machines/Ivans-Mac-mini/darwin/server/git.nix
+++ machines/Ivans-Mac-mini/darwin/server/git.nix
@@ -2,6 +2,6 @@
   environment.etc."gitconfig".text = ''
     [safe]
     	directory = /Volumes/Storage/Data/Sources/github.com/ivankovnatsky/nixos-config
-    	directory = /Volumes/Storage/Data/Sources/github.com/ivankovnatsky/nixos-config-worktrees
+    	directory = /Volumes/Storage/Data/Sources/github.com/ivankovnatsky/nixos-config-worktrees/*
   '';
 }
