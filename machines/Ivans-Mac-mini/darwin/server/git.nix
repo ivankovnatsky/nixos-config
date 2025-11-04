736b9156adea255cec5ff79f21ada7746c5c67a6
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-11-04 15:02:46 +0200

    machines/mini/darwin/server/git: trust all directories

--- machines/Ivans-Mac-mini/darwin/server/git.nix
+++ machines/Ivans-Mac-mini/darwin/server/git.nix
@@ -1,7 +1,6 @@
 {
   environment.etc."gitconfig".text = ''
     [safe]
-    	directory = /Volumes/Storage/Data/Sources/github.com/ivankovnatsky/nixos-config
-    	directory = /Volumes/Storage/Data/Sources/github.com/ivankovnatsky/nixos-config-worktrees/*
+    	directory = *
   '';
 }
