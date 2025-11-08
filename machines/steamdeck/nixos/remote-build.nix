29145418e36a79ed411935a5f434128e8ed241d0
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-11-08 17:08:56 +0200

    machines/a3,steamdeck: setup remote build configuration
    Configure steamdeck to use a3 as remote build host:
    
    - a3: Create nixbuilder system user with bash shell
    - a3: Add SSH authorized keys for root@steamdeck and ivan@steamdeck
    - a3: Add nixbuilder and username to trusted-users
    - steamdeck: Generate SSH key for root (/root/.ssh/nixbuilder)
    - steamdeck: Configure SSH to connect as nixbuilder@a3
    
    Enables on-demand remote builds via: make rebuild-nixos/a3

---
+++ machines/steamdeck/nixos/remote-build.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+{
+  # Generate SSH key for root to connect to remote builder
+  systemd.services.generate-nixbuilder-ssh-key = {
+    description = "Generate SSH key for root remote builds";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      if [ ! -f /root/.ssh/nixbuilder ]; then
+        mkdir -p /root/.ssh
+        ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f /root/.ssh/nixbuilder -N "" -C "root@steamdeck"
+        chmod 600 /root/.ssh/nixbuilder
+        chmod 644 /root/.ssh/nixbuilder.pub
+      fi
+    '';
+  };
+
+  # Configure root's SSH to connect to a3's nixbuilder user
+  programs.ssh.extraConfig = ''
+    Host a3
+      IdentitiesOnly yes
+      IdentityFile /root/.ssh/nixbuilder
+      User nixbuilder
+      StrictHostKeyChecking accept-new
+  '';
+}
