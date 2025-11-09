fb4c584fbc0055955913b79795e81695b4ffef86
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-11-09 20:48:22 +0200

    machines/pro/darwin/homebrew.nix: add mullvad-vpn

--- machines/Ivans-MacBook-Pro/darwin/homebrew.nix
+++ machines/Ivans-MacBook-Pro/darwin/homebrew.nix
@@ -1,7 +1,13 @@
+{ pkgs, ... }:
 {
   # MacBook Pro specific MAS apps only
   # Base homebrew configuration comes from ../../darwin/homebrew.nix
-  homebrew.masApps = {
+  homebrew = {
+    casks = [
+      "mullvad-vpn"
+    ];
+    masApps = {
     "Xcode" = 497799835;
+    };
   };
 }
