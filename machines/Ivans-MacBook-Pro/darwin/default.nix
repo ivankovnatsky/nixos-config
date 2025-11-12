3d31f24e7ec928fedbfe20bb7cfa07030318d2f1
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-11-12 18:48:59 +0200

    machines/pro/darwin: enable rebuild-daemon

--- machines/Ivans-MacBook-Pro/darwin/default.nix
+++ machines/Ivans-MacBook-Pro/darwin/default.nix
@@ -2,8 +2,11 @@
   imports = [
     ./dock.nix
     ./homebrew.nix
+    ../../../modules/darwin/rebuild-daemon
     ./nextdns
     ./syncthing-mgmt.nix
+    ./rebuild-daemon.nix
+    ./git.nix
     ../../../darwin/no-dock-restart.nix
   ];
 }
