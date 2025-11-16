51fd81080010960066c05db2db365c149d4439d7
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-11-16 13:15:01 +0200

    machines/steamdeck/nixos/security: fix kwallet autounlock with jovian
    Add kwallet-pam package to login service and enable systemd-user service
    to handle autologin scenarios when jovian.steam.autoStart bypasses SDDM.

--- machines/steamdeck/nixos/security.nix
+++ machines/steamdeck/nixos/security.nix
@@ -12,6 +12,14 @@
         login = {
           kwallet = {
             enable = true;
+            package = pkgs.kdePackages.kwallet-pam;
+          };
+        };
+        # Enable for systemd-user to handle jovian autoStart autologin scenarios
+        systemd-user = {
+          kwallet = {
+            enable = true;
+            package = pkgs.kdePackages.kwallet-pam;
           };
         };
         kwallet = {
