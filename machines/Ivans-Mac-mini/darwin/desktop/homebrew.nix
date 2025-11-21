ace8d27d1fa661dac86799ca1384a5380d29df73
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-11-21 16:48:10 +0200

    machines/mini/darwin/desktop/homebrew: init and remove stats

---
+++ machines/Ivans-Mac-mini/darwin/desktop/homebrew.nix
@@ -0,0 +1,41 @@
+{
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = false;
+      cleanup = "zap";
+    };
+    global.brewfile = true;
+    brews = [
+      # Since nix places it's new installs under newly generated nix store
+      # path, we can't relay on nixpkgs pam-reattach, because after nixpkgs
+      # upgrades PAM auth is broken for a common user. To fix it we need to
+      # enable root user and edit /private/etc/pam.d/sudo to unblock auth.
+      "pam-reattach"
+    ];
+    casks = [
+      "amethyst"
+      "bitwarden"
+      "chromium"
+      "coconutbattery"
+      "cursor"
+      "firefox"
+      "ghostty"
+      "hammerspoon"
+      "keycastr"
+      "kitty"
+      "mos"
+      "silicon-labs-vcp-driver"
+      "windsurf"
+    ];
+    masApps = {
+      "Numbers" = 409203825;
+      "Pages" = 409201541;
+      "Bitwarden" = 1352778147;
+      "Dark Reader for Safari" = 1438243180;
+    };
+    caskArgs = {
+      no_quarantine = true;
+    };
+  };
+}
