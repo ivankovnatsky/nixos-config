dadb305c32067ba166a828bb792d05cf68c30db1
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-11-23 13:15:52 +0200

    system/scripts/ps-top: fix linux compat

--- system/scripts/ps-top.sh
+++ system/scripts/ps-top.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 export COLUMNS=500
-ps -eo pid,%cpu,command -r | head -11
+if [[ "$(uname)" == "Darwin" ]]; then
+    ps -eo pid,%cpu,command -r | head -11
+else
+    ps -eo pid,%cpu,command --sort=-%cpu | head -11
+fi
