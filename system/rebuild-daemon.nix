e885985509d35622ba3427d12f6d4184db6f5708
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-13 17:29:24 +0200

    system/rebuild-daemon: always use home directory
    TCC blocks daemons from accessing /Volumes on macOS Sequoia.

--- system/rebuild-daemon.nix
+++ system/rebuild-daemon.nix
@@ -1,19 +1,8 @@
-{ config, lib, username ? null, ... }:
-let
-  # Determine the base path for the config based on hostname
-  # Ivans-Mac-mini uses miniStoragePath, others use user home directory
-  hostName = config.networking.hostName or "";
-
-  basePath =
-    if hostName == "Ivans-Mac-mini" then
-      config.flags.miniStoragePath
-    else
-      "${config.users.users.${username}.home}";
-
-in
+{ config, username ? null, ... }:
 {
   local.services.rebuildDaemon = {
     enable = true;
-    configPath = "${basePath}/Sources/github.com/ivankovnatsky/nixos-config";
+    # Always use home directory - TCC blocks daemons from /Volumes on macOS
+    configPath = "${config.users.users.${username}.home}/Sources/github.com/ivankovnatsky/nixos-config";
   };
 }
