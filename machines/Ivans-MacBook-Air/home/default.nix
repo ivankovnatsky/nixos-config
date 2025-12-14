8ad8655a23b05f46297b5319e233e968f8436c9c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-14 19:24:03 +0200

    machines/air/home: re-sort imports

--- machines/Ivans-MacBook-Air/home/default.nix
+++ machines/Ivans-MacBook-Air/home/default.nix
@@ -1,19 +1,14 @@
 {
   imports = [
-    ./packages.nix
-    ./manual-packages.nix
-    ../../../home/flags.nix
-    ../../../home/scripts.nix
-    ../../../modules/home/manual-packages
-    ../../../home/nixvim.nix
-    ../../../home/ccstatusline.nix
-    ../../../home/claude.nix
-    ../../../home/packages-darwin.nix
-    ../../../home/amethyst
     # ../../../home/bash.nix
+    # ../../../home/nixvim/plugins/copilot-lua
+    ../../../home/amethyst
     ../../../home/bat.nix
     ../../../home/btop.nix
+    ../../../home/ccstatusline.nix
+    ../../../home/claude.nix
     ../../../home/direnv.nix
+    ../../../home/flags.nix
     ../../../home/ghostty.nix
     ../../../home/giffer.nix
     ../../../home/git
@@ -24,11 +19,13 @@
     ../../../home/lsd.nix
     ../../../home/mpv.nix
     ../../../home/nixvim
-    # ../../../home/nixvim/plugins/copilot-lua
+    ../../../home/nixvim.nix
     ../../../home/npm.nix
+    ../../../home/packages-darwin.nix
     ../../../home/pass.nix
     ../../../home/rebuild-diff.nix
     ../../../home/safari.nix
+    ../../../home/scripts.nix
     ../../../home/shell.nix
     ../../../home/sops.nix
     ../../../home/starship
@@ -37,5 +34,8 @@
     ../../../home/vscode.nix
     ../../../home/yt-dlp.nix
     ../../../modules/flags
+    ../../../modules/home/manual-packages
+    ./manual-packages.nix
+    ./packages.nix
   ];
 }
