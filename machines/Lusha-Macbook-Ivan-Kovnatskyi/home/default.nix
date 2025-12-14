eb4cd348e393ab73ba068aaf0c6af817dae83ac3
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-14 20:50:16 +0200

    machines/work/home/default.nix: re-sort imports

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/home/default.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/home/default.nix
@@ -1,17 +1,18 @@
 {
   imports = [
-    ../../../home/scripts.nix
+    # ../../../home/aider.nix
+    # ../../../home/codex.nix
+    # ../../../home/nixvim/plugins/copilot-lua
+    # ./prevent-sleep.nix
     ../../../home/aichat-server.nix
     ../../../home/aichat.nix
-    # ../../../home/aider.nix
     ../../../home/amethyst
     ../../../home/bat.nix
     ../../../home/btop.nix
     ../../../home/ccstatusline.nix
     ../../../home/claude.nix
-    # ../../../home/codex.nix
-    ../../../home/gemini.nix
     ../../../home/direnv.nix
+    ../../../home/gemini.nix
     ../../../home/ghorg.nix
     ../../../home/ghostty.nix
     ../../../home/git
@@ -25,21 +26,22 @@
     ../../../home/lsd.nix
     ../../../home/mpv.nix
     ../../../home/nixvim
-    # ../../../home/nixvim/plugins/copilot-lua
     ../../../home/nixvim/plugins/gen-nvim
     ../../../home/nixvim/plugins/octo-nvim
     ../../../home/nixvim/plugins/unified-nvim
     ../../../home/nushell.nix
     ../../../home/pass.nix
+    ../../../home/pay-respects.nix
     ../../../home/pgcli.nix
     ../../../home/pyenv.nix
     ../../../home/rebuild-diff.nix
     ../../../home/safari.nix
+    ../../../home/scripts.nix
     ../../../home/shell.nix
+    ../../../home/sops.nix
     ../../../home/starship
     ../../../home/syncthing.nix
     ../../../home/terraform.nix
-    ../../../home/pay-respects.nix
     ../../../home/tmux.nix
     ../../../home/tweety.nix
     ../../../home/vscode.nix
@@ -48,13 +50,11 @@
     ../../../home/zellij.nix
     ../../../modules/flags
     ../../../modules/home/manual-packages
-    ./manual-packages.nix
     ./firefox.nix
     ./flags.nix
+    ./manual-packages.nix
     ./nixvim.nix
     ./packages.nix
-    # ./prevent-sleep.nix
     ./programs.nix
-    ../../../home/sops.nix
   ];
 }
