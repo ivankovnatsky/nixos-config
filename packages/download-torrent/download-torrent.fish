ea8070e10636c7dd29dc3229ddee69b6560c6946
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-15 13:52:50 +0200

    packages/download-torrent: support multiple files
    Allow passing multiple torrent files or globs (e.g., *.torrent) by
    iterating over all arguments instead of accepting only one.

--- packages/download-torrent/download-torrent.fish
+++ packages/download-torrent/download-torrent.fish
@@ -1,24 +1,26 @@
 #!/usr/bin/env fish
 
 function show_help
-    echo "Usage: download-torrent [MAGNET_LINK|TORRENT_FILE]"
+    echo "Usage: download-torrent [MAGNET_LINK|TORRENT_FILE...]"
     echo ""
-    echo "Downloads a torrent using aria2c from either a magnet link or torrent file"
-    echo "Default download location: ~/Downloads"
+    echo "Downloads torrents using aria2c from magnet links or torrent files"
+    echo "Default download location: current directory"
     echo ""
     echo "Arguments:"
     echo "  MAGNET_LINK    Magnet link starting with 'magnet:?'"
-    echo "  TORRENT_FILE   Path to a .torrent file"
+    echo "  TORRENT_FILE   Path to a .torrent file (supports multiple files/globs)"
     exit 1
 end
 
 # Show help if no arguments or help flags
-if test (count $argv) -ne 1; or contains -- "$argv[1]" --help -help -h
+if test (count $argv) -lt 1; or contains -- "$argv[1]" --help -help -h
     show_help
 end
 
 # Set default download directory to current $PWD
 set download_dir .
 
-# Download using aria2c to the specified download directory
-aria2c --dir $download_dir "$argv[1]"
+# Download each argument using aria2c
+for arg in $argv
+    aria2c --dir $download_dir "$arg"
+end
