1fa7148413f6281b3ee67d254962fc0ed4ac23b6
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-16 15:05:19 +0200

    home/vlc: fix language settings and add continue playback
    - Set audio-language and sub-language to en,eng (ISO-639 codes only)
    - Enable macosx-continue-playback=1 (Always resume)
    - Add VLC source code references in default.nix for documentation

--- home/vlc/default.nix
+++ home/vlc/default.nix
@@ -2,5 +2,12 @@
 
 {
   # delta home/vlc/vlcrc ~/Library/Preferences/org.videolan.vlc/vlcrc
+  #
+  # VLC source references (~/Sources/code.videolan.org/videolan/vlc):
+  # - audio-language, sub-language: src/libvlc-module.c:661-668
+  #   "comma separated, two or three letter ISO-639 country code"
+  # - macosx-continue-playback: modules/gui/macosx/main/macosx.m:122-126
+  #   0 = Ask, 1 = Always, 2 = Never
+  # - keybindings: src/libvlc-module.c:2574+ (key-toggle-fullscreen, key-audio-track, etc.)
   home.file."Library/Preferences/org.videolan.vlc/vlcrc".source = ./vlcrc;
 }
