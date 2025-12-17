ff2759bc29793d22fe80e8f63d38fa888b242dbf
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-17 06:44:43 +0200

    darwin/nextdns: re-use for both air and pro

--- darwin/nextdns.nix
+++ darwin/nextdns.nix
@@ -28,27 +28,27 @@
     enable = true;
     apiKeyFile = config.sops.secrets.nextdns-api-key.path;
     profileIdFile = config.sops.secrets.nextdns-profile-pro.path;
-    profileFile = ../../../../configs/nextdns-profile.json;
+    profileFile = ../configs/nextdns-profile.json;
   };
 
   local.services.nextdns-mgmt.air = {
     enable = true;
     apiKeyFile = config.sops.secrets.nextdns-api-key.path;
     profileIdFile = config.sops.secrets.nextdns-profile-air.path;
-    profileFile = ../../../../configs/nextdns-profile.json;
+    profileFile = ../configs/nextdns-profile.json;
   };
 
   local.services.nextdns-mgmt.phone = {
     enable = true;
     apiKeyFile = config.sops.secrets.nextdns-api-key.path;
     profileIdFile = config.sops.secrets.nextdns-profile-phone.path;
-    profileFile = ../../../../configs/nextdns-profile.json;
+    profileFile = ../configs/nextdns-profile.json;
   };
 
   local.services.nextdns-mgmt.asus = {
     enable = true;
     apiKeyFile = config.sops.secrets.nextdns-api-key.path;
     profileIdFile = config.sops.secrets.nextdns-profile-asus.path;
-    profileFile = ../../../../configs/nextdns-profile.json;
+    profileFile = ../configs/nextdns-profile.json;
   };
 }
