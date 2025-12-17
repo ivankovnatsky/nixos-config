26a7e33d33e36a5c9bdb087d466cecc8d623658f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-17 01:50:24 -0800

    machines/mini/darwin/server/http: update caddy hash

--- machines/Ivans-Mac-mini/darwin/server/http.nix
+++ machines/Ivans-Mac-mini/darwin/server/http.nix
@@ -34,7 +34,7 @@ let
   caddyWithPlugins = pkgs.caddy-with-plugins.withPlugins {
     # https://github.com/caddy-dns/cloudflare/issues/97#issuecomment-2784508762
     plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20250214163716-188b4850c0f2" ];
-    hash = "sha256-q0Y5l2Dan7lqNDLB/G7IYsBa1a9Vc/bCLyymOCTH/Jg=";
+    hash = "sha256-fqDv9fVFrHgt/Dl+9iYqr00ZCGb2dnTakcqGQoEhlxg=";
   };
 
   # Path to the Caddyfile template
