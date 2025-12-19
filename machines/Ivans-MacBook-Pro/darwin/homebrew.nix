517e2fd0fbe4d751285e17250e2b2acfde2f697b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-19 17:27:13 +0200

    machines/pro/darwin/homebrew: add ghostty@tip and kitty@nightly

--- machines/Ivans-MacBook-Pro/darwin/homebrew.nix
+++ machines/Ivans-MacBook-Pro/darwin/homebrew.nix
@@ -7,6 +7,15 @@
       "displayplacer"
     ];
     casks = [
+      # Needed to `brew uninstall --cask ghostty kitty`
+      {
+        name = "ghostty@tip";
+        greedy = true;
+      }
+      {
+        name = "kitty@nightly";
+        greedy = true;
+      }
       "mullvad-vpn"
     ];
     masApps = {
