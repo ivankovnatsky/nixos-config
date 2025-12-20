a3e8222600458ad02ed4dbe5fa53c5070ffecaac
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-20 22:37:22 +0200

    system/scripts/power-off-homelab: remove bee

--- system/scripts/power-off-homelab.sh
+++ system/scripts/power-off-homelab.sh
@@ -2,9 +2,8 @@
 
 # Script to remotely power off homelab machines
 # Usage: ./power-off-homelab.sh
-# This script will power off both the Bee machine and the Mini
+# This script will power off the Mini
 
 set -e
 
-ssh ivan@bee "sudo shutdown -h now"
 ssh ivan@ivans-mac-mini.local "sudo shutdown -h now"
