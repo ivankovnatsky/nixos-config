eaca6e3f469c7c4e53e7be568f12e68489a8a4df
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-26 13:07:35 +0200

    packages/settings/settings.py: add MacBookPro18,1 scaling support

--- packages/settings/settings.py
+++ packages/settings/settings.py
@@ -342,6 +342,7 @@ def cmd_menubar(args: argparse.Namespace) -> int:
 SCALING_MODEL_RESOLUTIONS = {
     "Mac14,2": ("1470x956", "1280x832"),  # MacBook Air 13" M2
     "Mac15,7": ("1728x1117", "1496x967"),  # MacBook Pro 16" M3
+    "MacBookPro18,1": ("1728x1117", "1496x967"),  # MacBook Pro 16" M1 Pro/Max
 }
 
 
