039479cbb275d047c27a44ebbeeaff03aee3a49f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-26 16:47:52 +0200

    darwin/shell: add early Homebrew PATH for fish

--- darwin/shell.nix
+++ darwin/shell.nix
@@ -8,5 +8,17 @@
   #      logging in as that user impossible. you can fix it with:
   #      programs.zsh.enable = true;
   programs.zsh.enable = true;
-  programs.fish.enable = config.flags.enableFishShell;
+  programs.fish = {
+    enable = config.flags.enableFishShell;
+    shellInit = ''
+      # Add Homebrew path early to prevent "Unknown command: brew" errors
+      # This runs before nix-darwin's interactiveShellInit which calls brew shellenv
+      if test -d /opt/homebrew/bin
+          fish_add_path --prepend /opt/homebrew/bin
+      end
+      if test -d /usr/local/bin
+          fish_add_path --prepend /usr/local/bin
+      end
+    '';
+  };
 }
