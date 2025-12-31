98cb06c1873c614fd93ae27a43a097b0f5f583ea
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-31 21:29:14 +0200

    machines/pro/darwin: remove rebuild-daemon

--- machines/Ivans-MacBook-Pro/darwin/default.nix
+++ machines/Ivans-MacBook-Pro/darwin/default.nix
@@ -21,14 +21,12 @@
     ../../../modules/darwin/launchd
     ../../../modules/darwin/nextdns-mgmt
     ../../../modules/darwin/pam
-    ../../../modules/darwin/rebuild-daemon
     ../../../modules/darwin/rebuild-terminal
     ../../../modules/darwin/sudo
     ../../../modules/darwin/syncthing-mgmt
     ../../../modules/flags
     ../../../system/documentation.nix
     ../../../system/nix.nix
-    ../../../system/rebuild-daemon.nix
     ../../../system/scripts
     ../../../darwin/dns-clear.nix
     ./dock.nix
