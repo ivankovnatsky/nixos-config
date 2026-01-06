3359bf4c379465b56af32823a9ba43e36363da3a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-01-06 21:37:48 +0200

    system/scripts/ghq: remove

--- system/scripts/ghq.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Wrapper for ghq get with auto-update enabled
-# ghq doesn't support config-based auto-update, only -u CLI flag
-# https://github.com/x-motemen/ghq
-exec ghq get -u "$@"
