b536f1b4f407ec851983ec6a5ddec82eae494b92
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-01-14 11:14:48 +0200

    machines/work/darwin/close-zscaler: use settings windows

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/close-zscaler.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/close-zscaler.nix
@@ -1,19 +1,15 @@
-# Requires: System Settings > Privacy & Security > Accessibility > allow bash
+# Requires: System Settings > Privacy & Security > Accessibility > allow Terminal
+{ pkgs, ... }:
+
 {
   local.launchd.services.close-zscaler = {
     enable = true;
     type = "user-agent";
-    command = builtins.concatStringsSep " " [
-      "/usr/bin/osascript -e"
-      "'tell app \"Terminal\" to do script"
-      "\"sleep 60;"
-      "osascript -e"
-      "'\"'\"'tell application \\\"System Events\\\""
-      "to tell process \\\"Zscaler\\\""
-      "to if exists (window 1)"
-      "then click button 1 of window 1'\"'\"';"
-      "exit\"'"
-    ];
+    command = "${pkgs.settings}/bin/settings windows close Zscaler";
+    preStart = ''
+      # Wait for Zscaler to fully start and show its window
+      sleep 90
+    '';
     runAtLoad = true;
     keepAlive = false;
   };
