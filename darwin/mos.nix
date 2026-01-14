02a45d5182d650a242443c1063aba50be2c4f098
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-01-14 11:27:20 +0200

    Revert "darwin/linearmouse: replace mos with linearmouse"
    This reverts commit 8f1db53418179679ec6a43e16766e356cae68d2e.

--- darwin/mos.nix
+++ darwin/mos.nix
@@ -1,8 +1,8 @@
 {
-  local.launchd.services.linearmouse = {
+  local.launchd.services.mos = {
     enable = true;
     type = "user-agent";
-    command = "/usr/bin/open -a LinearMouse";
+    command = "/usr/bin/open -a Mos";
     runAtLoad = true;
     keepAlive = false;
   };
