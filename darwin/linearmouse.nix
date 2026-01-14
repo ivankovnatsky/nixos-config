8f1db53418179679ec6a43e16766e356cae68d2e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-01-14 11:11:42 +0200

    darwin/linearmouse: replace mos with linearmouse
    Mos stopped working after macOS update. Maybe that just a matter of
    re-granting perms but let's try another alternative.

--- darwin/linearmouse.nix
+++ darwin/linearmouse.nix
@@ -1,8 +1,8 @@
 {
-  local.launchd.services.mos = {
+  local.launchd.services.linearmouse = {
     enable = true;
     type = "user-agent";
-    command = "/usr/bin/open -a Mos";
+    command = "/usr/bin/open -a LinearMouse";
     runAtLoad = true;
     keepAlive = false;
   };
