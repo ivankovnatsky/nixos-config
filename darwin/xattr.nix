ed19d8fef26bf2b789e100527446e31c367500fb
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-01-28 21:36:04 +0200

    darwin/xattr: add taskchamp path and refactor to list
    Use Nix list for pinned paths to make future additions easier.

--- darwin/xattr.nix
+++ darwin/xattr.nix
@@ -1,9 +1,16 @@
+let
+  pinnedPaths = [
+    "Library/Mobile Documents/com~apple~CloudDocs/Data/Notes"
+    "Library/Mobile Documents/iCloud~com~mav~taskchamp"
+  ];
+  pathArgs = builtins.concatStringsSep " " (map (p: ''"$HOME/${p}"'') pinnedPaths);
+in
 {
   local.launchd.services.xattr-pin-notes = {
     enable = true;
     type = "user-agent";
     runAtLoad = true;
     keepAlive = false;
-    command = "/bin/bash -c '/usr/bin/xattr -w \"com.apple.fileprovider.pinned#PX\" 1 \"$HOME/Library/Mobile Documents/com~apple~CloudDocs/Data/Notes\"'";
+    command = "/bin/bash -c '/usr/bin/xattr -w \"com.apple.fileprovider.pinned#PX\" 1 ${pathArgs}'";
   };
 }
