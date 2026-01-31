4568791a604cb93150d1e3258a23ffdd0bc4f48a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-01-31 22:10:35 +0200

    Revert "machines/a3/home/plasma: update mouse to Glorious"
    This reverts commit e8b276fa7ac459b51d6160799f1cb64f1a81e89a.

--- machines/a3/home/gpg.nix
+++ machines/a3/home/gpg.nix
@@ -8,13 +8,16 @@
   };
 
   services = {
-    # Configure GPG agent with tty pinentry
+    # Configure GPG agent with kwallet pinentry for KDE Wallet integration
     gpg-agent = {
       enable = true;
       enableSshSupport = true;
       defaultCacheTtl = 86400; # 24 hours
       maxCacheTtl = 86400;
-      pinentry.package = pkgs.pinentry-tty;
+      extraConfig = ''
+        allow-preset-passphrase
+        pinentry-program ${pkgs.kwalletcli}/bin/pinentry-kwallet
+      '';
     };
   };
 }
