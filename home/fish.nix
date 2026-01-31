1e3650592252c36701f321d93022dcbf7ddebe94
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-01-31 22:23:06 +0200

    home/fish: update with some other warnings

--- home/fish.nix
+++ home/fish.nix
@@ -4,4 +4,6 @@
 # * Color variables are no longer set in universal scope by default.
 #   Migrated them to global variables set in ~/.config/fish/conf.d/fish_frozen_theme.fish
 #   To restore syntax highlighting in other fish sessions, please restart them.
+# * The fish_key_bindings variable is no longer set in universal scope by default.
+#   Migrated it to a global variable set in  ~/.config/fish/conf.d/fish_frozen_key_bindings.fish
 # See also the release notes (type `help relnotes`).
