397abfef17e2bc1676eefde77370302b35acf02d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-01-31 22:05:54 +0200

    home/fish: init

---
+++ home/fish.nix
@@ -0,0 +1,7 @@
+{}
+
+# fish: upgraded to version 4.3:
+# * Color variables are no longer set in universal scope by default.
+#   Migrated them to global variables set in ~/.config/fish/conf.d/fish_frozen_theme.fish
+#   To restore syntax highlighting in other fish sessions, please restart them.
+# See also the release notes (type `help relnotes`).
