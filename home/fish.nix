1bf1bd1c91ceefede40c2a8f75d1c6b5d8c39832
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-02 17:10:52 +0200

    home/aliases: extract shared shell aliases

--- home/fish.nix
+++ home/fish.nix
@@ -6,38 +6,9 @@
 }:
 
 let
-  inherit (pkgs.stdenv.targetPlatform) isDarwin isLinux;
+  inherit (pkgs.stdenv.targetPlatform) isDarwin;
 
-  syncthingHomeDir =
-    if isDarwin then "~/Library/Application\\ Support/Syncthing" else "~/.config/syncthing";
-
-  commonShellAliases = {
-    # TODO: add function nix-prefetch-url $url | xargs nix hash to-sri --type sha256
-    g = "${pkgs.git}/bin/git";
-    erd = "${pkgs.erdtree}/bin/erd --color auto --human -L 1 --layout inverted --icons --long --hidden";
-    # Let's not use GNU Coreutils mkdir for now.
-    mkdir = "mkdir -p";
-    less = "less -RS";
-    syncthing = "${pkgs.syncthing}/bin/syncthing serve --no-browser";
-    stc = "${pkgs.stc-cli}/bin/stc -homedir ${syncthingHomeDir}";
-  };
-
-  shellAliases =
-    if config.flags.purpose == "home" then
-      commonShellAliases
-      // {
-        rclone = "${pkgs.rclone}/bin/rclone -P";
-        wl-copy = lib.mkIf isLinux "${pkgs.wl-clipboard}/bin/wl-copy -n";
-      }
-    else
-      commonShellAliases
-      // {
-        # We tenv version manager so pkgs interpolation is not needed.
-        tf = "tofu";
-        tg = "terragrunt";
-        k = "${pkgs.kubectl}/bin/kubectl";
-        argocd = "${pkgs.argocd}/bin/argocd --grpc-web";
-      };
+  shellAliases = import ./aliases.nix { inherit config lib pkgs; };
 
 in
 {
