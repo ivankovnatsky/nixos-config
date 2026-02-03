3246b6f15e95a4a136b7d7f7be95a0d922119811
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-03 14:00:05 +0200

    system/scripts/reboot: init

---
+++ system/scripts/reboot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+case "$(uname -s)" in
+  Darwin)
+    sudo /sbin/reboot
+    ;;
+  Linux)
+    if [[ -d /run/current-system ]]; then
+      sudo /run/current-system/sw/bin/systemctl reboot
+    else
+      sudo /sbin/reboot
+    fi
+    ;;
+  *)
+    echo "Unsupported OS: $(uname -s)" >&2
+    exit 1
+    ;;
+esac
