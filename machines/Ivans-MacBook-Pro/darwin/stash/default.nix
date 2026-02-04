9bcb6a0c0aa72aa6ab5e513918c101b2b4129adb
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-04 18:59:30 +0200

    machines/pro/darwin: enable stash

---
+++ machines/Ivans-MacBook-Pro/darwin/stash/default.nix
@@ -0,0 +1,82 @@
+{
+  config,
+  pkgs,
+  username,
+  ...
+}:
+
+let
+  dataDir = "${config.flags.externalStoragePath}/.stash";
+  stashDir = "${config.flags.externalStoragePath}/Media/Stash";
+
+  # TODO:
+  # - Hide sidebar in options permanently
+  #   See: https://github.com/stashapp/stash/issues/2879
+
+  # Template path (all substitutions happen at runtime to avoid secrets in /nix/store)
+  stashConfigTemplate = ../../../../templates/stash-config.yml;
+
+  # Generate stash paths YAML for single directory
+  stashPathsYaml = ''
+    - excludeimage: false
+      excludevideo: false
+      path: ${stashDir}'';
+in
+{
+  sops.secrets = {
+    stash-username = {
+      key = "stash/username";
+      owner = username;
+    };
+    stash-password = {
+      key = "stash/password";
+      owner = username;
+    };
+  };
+
+  local.launchd.services.stash = {
+    enable = true;
+    waitForPath = config.flags.externalStoragePath;
+    inherit dataDir;
+    extraDirs = [
+      "${dataDir}/logs"
+      "${dataDir}/config"
+      "${dataDir}/generated"
+      "${dataDir}/cache"
+      "${dataDir}/blobs"
+      "${dataDir}/metadata"
+    ];
+    preStart = ''
+      export PATH="${pkgs.ffmpeg}/bin:$PATH"
+
+      if [ -f "${dataDir}/config/config.yml" ]; then
+        cp "${dataDir}/config/config.yml" "${dataDir}/config/config.yml.backup.$(date +%Y%m%d-%H%M%S)"
+      fi
+
+      # Read secrets from sops-decrypted files at runtime
+      STASH_USERNAME=$(cat ${config.sops.secrets.stash-username.path})
+      STASH_PASSWORD=$(cat ${config.sops.secrets.stash-password.path})
+
+      # Generate stash paths section
+      STASH_PATHS=$(cat <<'STASH_PATHS_EOF'
+      ${stashPathsYaml}
+      STASH_PATHS_EOF
+      )
+
+      # Substitute all values from template (keeps secrets out of /nix/store)
+      sed -e "s|@dataDir@|${dataDir}|g" \
+          -e "s|@host@|${config.flags.proIp}|g" \
+          -e "s|@port@|9999|g" \
+          -e "s|@username@|$STASH_USERNAME|g" \
+          -e "s|@password@|$STASH_PASSWORD|g" \
+          -e "/@stashPaths@/r /dev/stdin" \
+          -e "/@stashPaths@/d" \
+          ${stashConfigTemplate} <<< "$STASH_PATHS" > ${dataDir}/config/config.yml
+
+      chmod 644 ${dataDir}/config/config.yml
+    '';
+    command = ''
+      script -q /dev/null ${pkgs.stash}/bin/stash --config ${dataDir}/config/config.yml
+    '';
+  };
+}
