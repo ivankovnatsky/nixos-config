17fdbad5fc38ca37500c86607ea1c65776aca3d9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-07 14:05:16 +0200

    darwin/xattr: add obsidian to always keep

--- darwin/xattr.nix
+++ darwin/xattr.nix
@@ -2,6 +2,7 @@ let
   pinnedPaths = [
     "Library/Mobile Documents/com~apple~CloudDocs/Data/Notes"
     "Library/Mobile Documents/iCloud~com~mav~taskchamp"
+    "Library/Mobile Documents/iCloud~md~obsidian"
   ];
   pathArgs = builtins.concatStringsSep " " (map (p: ''"$HOME/${p}"'') pinnedPaths);
 in
