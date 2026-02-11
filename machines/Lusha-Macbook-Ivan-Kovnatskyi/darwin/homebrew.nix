0a7d5ec10f13fee4cca10eacc35175f2d4388f89
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-11 16:09:46 +0200

    machines/work/darwin/homebrew: remove orbstack
    For some reason failed to start/work. Keep using Rancher Desktop.

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/homebrew.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/homebrew.nix
@@ -45,7 +45,6 @@
       }
       "mos"
       "obsidian"
-      "orbstack"
       "stats"
       # Need to keep twingate always latest version, sometimes it fails to
       # connect no matter what.
