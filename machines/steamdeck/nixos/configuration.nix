f655e3f1974e59e8cbb810e679b5cf06a61062fa
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-20 20:27:14 +0200

    machines/steamdeck/nixos: update hardware and config for reinstall
    New UUIDs from fresh NixOS install, enable pipewire, libinput,
    firewall ports for syncthing, and latest kernel.

--- machines/steamdeck/nixos/configuration.nix
+++ machines/steamdeck/nixos/configuration.nix
@@ -2,13 +2,13 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -18,16 +18,19 @@
   boot = {
     initrd = {
       luks.devices.crypted = {
-        device = "/dev/disk/by-uuid/2b9052dc-f819-4ff3-98e6-661a45a2cc3e";
+        device = "/dev/disk/by-uuid/2dc67f0b-3182-4780-90de-a8e8ca94e370";
         preLVM = true;
       };
     };
   };
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "steamdeck"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Europe/Kyiv";
@@ -45,10 +48,7 @@
   # };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-
-
+  # services.xserver.enable = true;
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -81,10 +81,10 @@
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -100,8 +100,14 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22000 8384 ];
-  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+  networking.firewall.allowedTCPPorts = [
+    22000
+    8384
+  ];
+  networking.firewall.allowedUDPPorts = [
+    22000
+    21027
+  ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
@@ -128,6 +134,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
-
