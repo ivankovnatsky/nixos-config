131c800f249d0d5d6333b64777b3155e3da787f8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-20 18:30:32 +0200

    Revert "machines/steamdeck: remove"
    This reverts commit c4707166b899dad05b1fe1c3100ca16ab57832bd.

---
+++ machines/steamdeck/nixos/cryptenroll.nix
@@ -0,0 +1,17 @@
+{
+  boot = {
+    initrd = {
+      luks.devices = {
+        "crypted" = {
+          device = "/dev/disk/by-uuid/2b9052dc-f819-4ff3-98e6-661a45a2cc3e";
+          crypttabExtraOpts = [ "tpm2-device=auto" ];
+        };
+      };
+
+      systemd = {
+        enable = true;
+        emergencyAccess = true;
+      };
+    };
+  };
+}
---
+++ machines/steamdeck/nixos/tpm2.nix
@@ -0,0 +1,3 @@
+{
+  security.tpm2.enable = true;
+}
