5ad0512f52dc876a3b1e150766f3828338522f4e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-20 22:15:33 +0200

    machines/a3/nixos/user: remove attrs already in configuration.nix

--- machines/a3/nixos/user.nix
+++ machines/a3/nixos/user.nix
@@ -6,11 +6,8 @@
 }:
 
 {
-  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.${username} = {
-    isNormalUser = true;
     shell = pkgs.fish;
-    extraGroups = [ "wheel" ]; # Enable 'sudo' for the user.
     linger = true;
     openssh.authorizedKeys.keys = [
       config.flags.sshKeys.air
