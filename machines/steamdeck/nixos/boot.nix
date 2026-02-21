4ee243f6e1d26a70157e276d14db55df59e59b8d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-21 11:38:41 +0200

    machines/steamdeck/nixos/boot: remove unused args, add comment

--- machines/steamdeck/nixos/boot.nix
+++ machines/steamdeck/nixos/boot.nix
@@ -1,10 +1,8 @@
 {
-  config,
-  lib,
-  pkgs,
   ...
 }:
 
 {
+  # Always use at least 1 second timeout to be able to boot from prev nix generation in case of nuclear war
   boot.loader.timeout = 1;
 }
