ccf3649f3e2a3cba4093acf91001c60caa6c58b1
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-21 11:38:27 +0200

    machines/steamdeck/home/desktop: init

---
+++ machines/steamdeck/home/desktop.nix
@@ -0,0 +1,15 @@
+{
+  # Desktop environment home-manager configuration
+  # Comment/uncomment the session you want to use
+
+  imports = [
+    # Desktop environments
+    ./kwinoutput # KDE window output config
+    ./plasma.nix # KDE Plasma config
+
+    # ../../../home/gnome.nix # GNOME config
+
+    # Minimal window managers
+    # (dwm typically doesn't need home-manager config)
+  ];
+}
