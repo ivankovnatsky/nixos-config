e8a2ba7bdccacbeecd5e67d0853c388381cd37b7
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-21 11:56:45 +0200

    machines/a3/nixos/remote-build: use flags for SSH keys

--- machines/a3/nixos/remote-build.nix
+++ machines/a3/nixos/remote-build.nix
@@ -1,4 +1,10 @@
-{ username, pkgs, ... }:
+{
+  config,
+  lib,
+  username,
+  pkgs,
+  ...
+}:
 {
   # Create a dedicated user for remote builds
   users.users.nixbuilder = {
@@ -8,9 +14,9 @@
     home = "/var/lib/nixbuilder";
     createHome = true;
     shell = pkgs.bash;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINduoENZ5s/uUim5k74kdZRDavAcIdtoY/txnn+ueXOQ root@steamdeck"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAVbd/fRD0/cnBznyI7AtsCXVeM1Mr2hzQXY7nufT4S ivan@steamdeck"
+    openssh.authorizedKeys.keys = lib.filter (k: k != "") [
+      config.flags.sshKeys.air
+      config.flags.sshKeys.steamdeck
     ];
   };
 
