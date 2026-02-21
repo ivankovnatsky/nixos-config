1d0064743adf0113913178270ae50e6ea2ca2623
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-21 11:56:52 +0200

    machines/steamdeck/nixos/remote-build: use ivan user instead of root

--- machines/steamdeck/nixos/remote-build.nix
+++ machines/steamdeck/nixos/remote-build.nix
@@ -1,35 +1,46 @@
 {
   config,
-  lib,
   pkgs,
+  username,
   ...
 }:
+
+let
+  homeDir = config.users.users.${username}.home;
+  keyPath = "${homeDir}/.ssh/nixbuilder";
+in
 {
-  # FIXME: why do we use root here?
-  # Generate SSH key for root to connect to remote builder
+  # Generate SSH key for ivan to connect to remote builder (idempotent)
   systemd.services.generate-nixbuilder-ssh-key = {
-    description = "Generate SSH key for root remote builds";
+    description = "Generate SSH key for remote builds";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
+      User = username;
+      Group = "users";
     };
     script = ''
-      if [ ! -f /root/.ssh/nixbuilder ]; then
-        mkdir -p /root/.ssh
-        ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f /root/.ssh/nixbuilder -N "" -C "root@steamdeck"
-        chmod 600 /root/.ssh/nixbuilder
-        chmod 644 /root/.ssh/nixbuilder.pub
+      if [ ! -f ${keyPath} ]; then
+        mkdir -p ${homeDir}/.ssh
+        ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f ${keyPath} -N "" -C "${username}@steamdeck"
+        chmod 600 ${keyPath}
+        chmod 644 ${keyPath}.pub
       fi
     '';
   };
 
-  # Configure root's SSH to connect to a3's nixbuilder user
+  # To manually generate the key:
+  # ssh-keygen -t ed25519 -f ~/.ssh/nixbuilder -N "" -C "ivan@steamdeck"
+  # Then copy the public key to modules/flags/default.nix:
+  # cat ~/.ssh/nixbuilder.pub
+
+  # Configure SSH to connect to a3's nixbuilder user
   programs.ssh.extraConfig = ''
     Host a3
-      HostName 192.168.50.6
+      HostName ${config.flags.a3Ip}
       IdentitiesOnly yes
-      IdentityFile /root/.ssh/nixbuilder
+      IdentityFile ${keyPath}
       User nixbuilder
       StrictHostKeyChecking accept-new
   '';
