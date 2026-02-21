2b71ba36cdc5a2994127c8cbf40351268751674e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-21 15:57:59 +0200

    home/ssh: always print age public key on activation

--- home/ssh.nix
+++ home/ssh.nix
@@ -19,11 +19,8 @@
     if [ ! -f "${config.home.homeDirectory}/.ssh/id_ed25519" ]; then
       $DRY_RUN_CMD mkdir -p "${config.home.homeDirectory}/.ssh"
       $DRY_RUN_CMD ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f "${config.home.homeDirectory}/.ssh/id_ed25519" -N ""
-      echo ""
-      echo "Generated new SSH key. To use with sops-nix, add the age public key to .sops.yaml:"
-      echo ""
-      $DRY_RUN_CMD ${pkgs.ssh-to-age}/bin/ssh-to-age < "${config.home.homeDirectory}/.ssh/id_ed25519.pub"
-      echo ""
+      echo "Generated new SSH key."
     fi
+    echo "age public key: $($DRY_RUN_CMD ${pkgs.ssh-to-age}/bin/ssh-to-age < "${config.home.homeDirectory}/.ssh/id_ed25519.pub")"
   '';
 }
