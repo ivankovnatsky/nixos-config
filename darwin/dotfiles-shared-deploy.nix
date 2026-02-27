416832031846c6dd3c73d7a6a265f1ee679c243e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-27 21:56:19 +0200

    darwin/dotfiles-shared-deploy: use preStart for init

--- darwin/dotfiles-shared-deploy.nix
+++ darwin/dotfiles-shared-deploy.nix
@@ -6,7 +6,8 @@
   local.launchd.services.dotfiles-shared-deploy = {
     enable = true;
     type = "user-agent";
-    command = "${pkgs.dotfiles}/bin/dotfiles shared init && ${pkgs.dotfiles}/bin/dotfiles shared deploy";
+    preStart = "${pkgs.dotfiles}/bin/dotfiles shared init";
+    command = "${pkgs.dotfiles}/bin/dotfiles shared deploy";
     runAtLoad = true;
     keepAlive = false;
     extraServiceConfig = {
