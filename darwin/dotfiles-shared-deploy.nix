cc2997d18c79136a8810334104c08dca0bccdb88
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-02-27 22:18:39 +0200

    darwin/dotfiles-shared-deploy: remove init preStart

--- darwin/dotfiles-shared-deploy.nix
+++ darwin/dotfiles-shared-deploy.nix
@@ -6,7 +6,6 @@
   local.launchd.services.dotfiles-shared-deploy = {
     enable = true;
     type = "user-agent";
-    preStart = "${pkgs.dotfiles}/bin/dotfiles shared init";
     command = "${pkgs.dotfiles}/bin/dotfiles shared deploy";
     runAtLoad = true;
     keepAlive = false;
