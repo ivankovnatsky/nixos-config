02d9560282a08ee8c5f9243a8effcdde6b99e767
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-11 20:46:54 +0200

    overlays/taskwarrior-web: update sha256

--- overlays/taskwarrior-web/default.nix
+++ overlays/taskwarrior-web/default.nix
@@ -39,7 +39,7 @@ let
     '';
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
-    outputHash = "sha256-i7LvbsJ0N86UHQgo2MtgkCfOYBUIOOK8e0hQxO2qteg=";
+    outputHash = "sha256-OuCWuJEr2s6UHjwcSFFJzExvM6fQq9u0/Lp/jtpvewg=";
     impureEnvVars = lib.fetchers.proxyImpureEnvVars;
   };
 in
