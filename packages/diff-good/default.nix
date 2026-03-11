b0ddd03b12447b46a2b9e9b61483add769181677
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-11 13:32:11 +0200

    packages/diff-good: init

---
+++ packages/diff-good/default.nix
@@ -0,0 +1,14 @@
+{ pkgs }:
+
+pkgs.writeShellScriptBin "diff-good" ''
+  EXCLUDES=(
+    .terraform
+  )
+
+  EXCLUDE_ARGS=""
+  for e in "''${EXCLUDES[@]}"; do
+    EXCLUDE_ARGS="$EXCLUDE_ARGS --exclude=$e"
+  done
+
+  ${pkgs.diffutils}/bin/diff -ru $EXCLUDE_ARGS "$@" | ${pkgs.delta}/bin/delta
+''
