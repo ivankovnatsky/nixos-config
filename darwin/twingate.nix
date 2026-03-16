eda2d161446905b21635e6f8c6e7231c8797f8d7
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-16 11:00:12 +0200

    darwin/twingate: remove twingate service

--- darwin/twingate.nix
+++
@@ -1,9 +0,0 @@
-{
-  local.launchd.services.twingate = {
-    enable = true;
-    type = "user-agent";
-    command = "/usr/bin/open -a Twingate";
-    runAtLoad = true;
-    keepAlive = false;
-  };
-}
