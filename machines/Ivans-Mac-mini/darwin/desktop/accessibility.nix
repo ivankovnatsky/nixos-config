f75474d82c7ecba01e5293e8e6aa749ea6dbdb1f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-20 12:47:46 +0200

    machines/mini/darwin/desktop/accessibility: init

---
+++ machines/Ivans-Mac-mini/darwin/desktop/accessibility.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }:
+
+{
+  system.activationScripts.postActivation.text = ''
+    ${pkgs.settings}/bin/settings accessibility --enable "Amethyst,Discord,Ghostty,Hammerspoon,Mac Mouse Fix Helper,Terminal"
+  '';
+}
