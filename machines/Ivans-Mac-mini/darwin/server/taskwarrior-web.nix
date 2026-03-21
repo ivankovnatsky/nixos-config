052388a7a7b7146ebf78c558891681cc2c8bac96
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-21 20:49:18 +0200

    machines/mini/darwin/server/taskwarrior-web: add task to PATH

--- machines/Ivans-Mac-mini/darwin/server/taskwarrior-web.nix
+++ machines/Ivans-Mac-mini/darwin/server/taskwarrior-web.nix
@@ -7,6 +7,7 @@
     environment = {
       TWK_SERVER_ADDR = config.flags.machineBindAddress;
       TWK_SERVER_PORT = "8087";
+      PATH = "${pkgs.taskwarrior3}/bin";
     };
     command = ''
       ${pkgs.taskwarrior-web}/bin/taskwarrior-web
