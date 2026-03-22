6aad9333ee275d2668c1206a5a22d4dcfe3ca71d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-22 11:53:45 +0200

    machines/pro/darwin/syncthing-mgmt: remove git syncing folders

--- machines/Ivans-MacBook-Pro/darwin/syncthing-mgmt.nix
+++ machines/Ivans-MacBook-Pro/darwin/syncthing-mgmt.nix
@@ -4,14 +4,5 @@ let
   homePath = "${config.users.users.${username}.home}";
 in
 {
-  local.services.syncthing-mgmt.folders = {
-    "dotfiles-shared" = {
-      path = "${homePath}/Sources/github.com/ivankovnatsky-local/dotfiles-shared";
-      label = "Sources/github.com/ivankovnatsky-local/dotfiles-shared";
-      devices = [
-        "Ivans-MacBook-Pro"
-        "Lusha-Macbook-Ivan-Kovnatskyi"
-      ];
-    };
-  };
+  local.services.syncthing-mgmt.folders = { };
 }
