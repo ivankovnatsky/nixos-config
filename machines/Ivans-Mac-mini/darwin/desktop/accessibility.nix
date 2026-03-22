11fabe8966889e2a244b01d4cc142910f1d4ad66
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-22 13:29:02 +0200

    machines/mini/darwin/desktop/accessibility: add kitty

--- machines/Ivans-Mac-mini/darwin/desktop/accessibility.nix
+++ machines/Ivans-Mac-mini/darwin/desktop/accessibility.nix
@@ -2,6 +2,6 @@
 
 {
   system.activationScripts.postActivation.text = ''
-    ${pkgs.settings}/bin/settings accessibility --enable "Amethyst,Discord,Ghostty,Hammerspoon,Mac Mouse Fix Helper,Terminal"
+    ${pkgs.settings}/bin/settings accessibility --enable "Amethyst,Discord,Ghostty,Hammerspoon,Mac Mouse Fix Helper,Terminal,kitty"
   '';
 }
