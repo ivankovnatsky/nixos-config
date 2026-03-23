19e58aaa8411caae082d735f001c9884f3f87c57
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-23 11:44:32 +0200

    home/gpg: fix gpg-agent config on macOS
    services.gpg-agent is Linux-only (systemd). On macOS, write
    gpg-agent.conf directly via home.file with pinentry-mac.

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  inherit (pkgs.stdenv.targetPlatform) isDarwin isLinux;
+in
 {
   programs = {
     gpg = {
@@ -6,12 +9,20 @@
     };
   };
 
-  services = {
+  services = lib.mkIf isLinux {
     gpg-agent = {
       enable = true;
       pinentry.package = pkgs.pinentry-tty;
       defaultCacheTtl = 43200;
       maxCacheTtl = 43200;
     };
+  };
+
+  home.file.".gnupg/gpg-agent.conf" = lib.mkIf isDarwin {
+    text = ''
+      default-cache-ttl 43200
+      max-cache-ttl 43200
+      pinentry-program ${pkgs.pinentry_mac}/bin/pinentry-mac
+    '';
   };
 }
