536d0ce2d61c5a325d635415068d4f9b74f2dcc5
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-26 13:35:51 +0200

    packages/forgejo-mgmt: add gnupg to PATH for GPG key ID extraction

--- packages/forgejo-mgmt/default.nix
+++ packages/forgejo-mgmt/default.nix
@@ -1,5 +1,6 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "forgejo-mgmt" ''
+  export PATH="${pkgs.lib.makeBinPath [ pkgs.gnupg ]}:$PATH"
   exec ${pkgs.python3.withPackages (ps: [ ps.requests ])}/bin/python ${./forgejo-mgmt.py} "$@"
 ''
