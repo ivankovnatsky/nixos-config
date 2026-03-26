bf673a8e728cf7571deb780311d6d09019dd3b5f
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-26 13:17:22 +0200

    machines/Ivans-MacBook-Pro/darwin: enable reposync

--- machines/Ivans-MacBook-Pro/darwin/default.nix
+++ machines/Ivans-MacBook-Pro/darwin/default.nix
@@ -27,13 +27,16 @@
     ../../../modules/darwin/nextdns-mgmt
     ../../../modules/darwin/taskwarrior-notify
     ../../../modules/darwin/pam
+    ../../../modules/darwin/reposync
     ../../../modules/darwin/sudo
     ../../../modules/darwin/syncthing-mgmt
     ../../../modules/flags
     ../../../system/documentation.nix
     ../../../system/nix.nix
     ../../../system/sops-secrets.nix
+    ../../../system/reposync.nix
     ../../Ivans-MacBook-Air/darwin/dock.nix
+    ../../Ivans-MacBook-Air/darwin/reposync.nix
     ../../Ivans-MacBook-Air/darwin/users.nix
     ./syncthing-mgmt.nix
   ];
