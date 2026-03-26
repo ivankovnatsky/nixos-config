4491c91b56985c6defd74b92c9d0f0df213e2cd1
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-26 13:17:14 +0200

    machines/Ivans-Mac-mini/darwin/server: add home and nix-config to reposync

--- machines/Ivans-Mac-mini/darwin/server/reposync.nix
+++ machines/Ivans-Mac-mini/darwin/server/reposync.nix
@@ -1,22 +1,11 @@
 { config, username, ... }:
 {
-  sops.secrets.discord-webhook-reposync = {
-    key = "discord/webhookChannelMonitoringRepoSync";
-    owner = username;
-  };
-
-  local.services.reposync = {
-    enable = true;
-    domainFile = config.sops.secrets.external-domain.path;
-    discordWebhookFile = config.sops.secrets.discord-webhook-reposync.path;
-
-    repositories = [
-      {
-        path = "${config.users.users.${username}.home}/Notes";
-        remote = "origin";
-        remoteUrl = "https://forgejo.@domain@/forgejouser/notes.git";
-        branch = "main";
-      }
-    ];
-  };
+  local.services.reposync.repositories = [
+    {
+      path = "${config.flags.externalStoragePath}/Sources/github.com/ivankovnatsky/nix-config";
+      remote = "origin";
+      remoteUrl = "https://github.com/ivankovnatsky/nix-config.git";
+      branch = "main";
+    }
+  ];
 }
