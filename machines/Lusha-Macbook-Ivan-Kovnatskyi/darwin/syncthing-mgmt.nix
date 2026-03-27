660aaec4ccfd50ae2f562ddc15f536388180f141
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-27 16:38:08 +0200

    m/work/drw/syncthing-mgmt: remove nix-config-ignored-files folder

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/syncthing-mgmt.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/syncthing-mgmt.nix
@@ -32,19 +32,7 @@
     ];
 
     # Folders can reference devices by name (resolved from deviceDefinitionsFile)
-    folders = {
-      "nix-config-ignored-files" = {
-        path = "${config.users.users.${username}.home}/Sources/github.com/ivankovnatsky/nix-config-ignored-files";
-        label = "nix-config-ignored-files";
-        devices = [
-          "Ivans-Mac-mini"
-          "Ivans-MacBook-Pro"
-          "Ivans-MacBook-Air"
-          "Lusha-Macbook-Ivan-Kovnatskyi"
-          "a3"
-        ];
-      };
-    };
+    folders = { };
 
     restart = false;
   };
