53a1472b39591a481a89110a280b04e89b0479b9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-27 15:50:16 +0200

    home/packages-darwin: remove packages now in shared packages

--- home/packages-darwin.nix
+++ home/packages-darwin.nix
@@ -4,45 +4,34 @@
     bat
     battery-toolkit # Local overlay
     btop
-    launchd-mgmt
     cargo
-    delta
-    dust
     duf
     erdtree
     exiftool
     fzf
     gallery-dl
-    genpass
     home-manager
     imagemagick
-    jq
+    launchd-mgmt
     magic-wormhole
     mkpasswd
     nixfmt-rfc-style
-    nodejs
-    gh-notifications
     pandoc
     parallel
-    pigz
     pv
     rclone
     rems
     ripgrep
     rust-analyzer
     rustc
-    dns
     smctemp # Local overlay
-    settings
     swiftformat
     syncthing
-    syncthing-mgmt
+    taskwarrior-web # Local overlay
     typst
     typstyle
-    username # Installed as flake
     watchman
     watchman-make
-    wget
     zsh-forgit
   ];
 }
