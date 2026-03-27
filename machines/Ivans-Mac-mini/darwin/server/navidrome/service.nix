2d07977da48eb3528556083bb939136690254dc2
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-03-27 21:58:07 +0200

    machines/mini/darwin/server/navidrome: add Navidrome music server
    Native launchd service streaming music from
    /Volumes/Storage/Data/Music on port 4533 with
    transcoding enabled and 15-minute scan interval.

---
+++ machines/Ivans-Mac-mini/darwin/server/navidrome/service.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+let
+  dataDir = "${config.flags.externalStoragePath}/.navidrome";
+  musicDir = "${config.flags.externalStoragePath}/Music";
+in
+{
+  local.launchd.services.navidrome = {
+    enable = true;
+    waitForPath = config.flags.externalStoragePath;
+    inherit dataDir;
+    command = ''
+      ${pkgs.navidrome}/bin/navidrome \
+        --datafolder "${dataDir}" \
+        --musicfolder "${musicDir}" \
+        --address ${config.flags.machineBindAddress} \
+        --port 4533
+    '';
+    environment = {
+      # Enable transcoding via ffmpeg (bundled with nixpkgs navidrome)
+      ND_ENABLETRANSCODINGCONFIG = "true";
+      ND_SCANNER_SCHEDULE = "@every 15m";
+    };
+  };
+}
