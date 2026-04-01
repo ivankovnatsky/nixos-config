4598840c0e07235cb6547d129b5a587c9837d48d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-01 17:24:23 +0300

    home/whisper: add mlx-whisper wrapper and use .cache/ for models

--- home/whisper.nix
+++ home/whisper.nix
@@ -1,15 +1,26 @@
 { config, pkgs, ... }:
 
 let
+  cacheDir = "${config.flags.externalStoragePath}/.cache";
+
   whisperWrapped = pkgs.writeShellScriptBin "whisper" ''
     exec ${pkgs.openai-whisper}/bin/whisper \
       --model turbo \
-      --model_dir ${config.flags.externalStoragePath}/.whisper \
+      --model_dir ${cacheDir}/whisper \
+      "$@"
+  '';
+
+  mlxWhisperWrapped = pkgs.writeShellScriptBin "mlx_whisper" ''
+    export HF_HOME="${cacheDir}/huggingface"
+    exec ${pkgs.mlx-whisper}/bin/mlx_whisper \
+      --model mlx-community/whisper-turbo \
       "$@"
   '';
 in
 {
   home.packages = [
     whisperWrapped
+    mlxWhisperWrapped
   ];
+
 }
