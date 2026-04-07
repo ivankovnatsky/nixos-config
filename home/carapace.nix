1515b07218f8a796f341b3b3594a46212806e823
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-07 15:04:57 +0300

    home/carapace: exclude task from completions to preserve taskwarrior

--- home/carapace.nix
+++ home/carapace.nix
@@ -3,4 +3,10 @@
     enable = true;
     enableFishIntegration = true;
   };
+
+  # Exclude "task" from carapace completions so it doesn't override
+  # taskwarrior's "task" command with go-task completions.
+  home.sessionVariables = {
+    CARAPACE_EXCLUDES = "task";
+  };
 }
