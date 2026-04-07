9f76b77be2da72fddf22ceeb74fa3c41cd15d272
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-07 15:47:21 +0300

    m/mini/drw/srv/pkg: remove packages duplicated in home configs
    Remove watchman, watchman-make, rebuild, rclone,
    pigz, smctemp, dust, wget, gallery-dl,
    asusrouter-cli already in home/packages.nix or
    home/packages-darwin.nix.

--- machines/Ivans-Mac-mini/darwin/server/packages.nix
+++ machines/Ivans-Mac-mini/darwin/server/packages.nix
@@ -3,22 +3,9 @@
   environment.systemPackages = with pkgs; [
     gnumake # To avoid installing Developer Tools
     tmux
-    watchman # Be able to rerun rebuild manually with watching
-    watchman-make # Be able to rerun rebuild manually with watching
-    rebuild # Nix rebuild tool with simple and watch modes
     git
 
-    rclone # Sync out stuff
-    pigz # To help with Sources/ packing
-
-    smctemp # Overlay
-    dust # For disk usage analysis
-
-    wget
     nixpkgs-darwin-master-ytdlp.yt-dlp
-    gallery-dl
-
-    asusrouter-cli
     mas
 
     # Required for Uptime Kuma tailscale-ping monitors
