7a4828b6cdfbedc90f1860a4d5be801d7739cf5c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-08 13:16:09 +0300

    home/nixos/packages: replace nixfmt-rfc-style with nixfmt

--- home/nixos/packages.nix
+++ home/nixos/packages.nix
@@ -12,7 +12,7 @@
     libnotify # Provides notify-send command
     lm_sensors # Provides the 'sensors' command for monitoring temperatures
     lsof # List open files
-    nixfmt-rfc-style
+    nixfmt
     sesh
     smartmontools # Disk health monitoring (smartctl)
     wl-clipboard # Wayland clipboard utilities
