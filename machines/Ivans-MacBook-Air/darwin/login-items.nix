8b159265852460231f7e917b0f8912d6c40a42ca
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-09 15:49:38 +0300

    machines/air/darwin/login-items: remove Amethyst

--- machines/Ivans-MacBook-Air/darwin/login-items.nix
+++ machines/Ivans-MacBook-Air/darwin/login-items.nix
@@ -2,6 +2,6 @@
 
 {
   system.activationScripts.postActivation.text = ''
-    ${pkgs.settings}/bin/settings login add "Amethyst,Hammerspoon,Mac Mouse Fix,Stats" || true
+    ${pkgs.settings}/bin/settings login add "Hammerspoon,Mac Mouse Fix,Stats" || true
   '';
 }
