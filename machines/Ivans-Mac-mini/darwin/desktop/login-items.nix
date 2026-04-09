c88752a1f7c55ea453ea899c9b86b1442e4c7fb2
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-09 15:49:34 +0300

    machines/mini/darwin/desktop/login-items: remove Amethyst

--- machines/Ivans-Mac-mini/darwin/desktop/login-items.nix
+++ machines/Ivans-Mac-mini/darwin/desktop/login-items.nix
@@ -2,6 +2,6 @@
 
 {
   system.activationScripts.postActivation.text = ''
-    ${pkgs.settings}/bin/settings login add "Amethyst,Hammerspoon" || true
+    ${pkgs.settings}/bin/settings login add "Hammerspoon" || true
   '';
 }
