5c8fb61517ad4381ef171a5e2401f9b34d1d0824
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-09 15:49:33 +0300

    machines/mini/darwin/desktop/accessibility: remove Amethyst

--- machines/Ivans-Mac-mini/darwin/desktop/accessibility.nix
+++ machines/Ivans-Mac-mini/darwin/desktop/accessibility.nix
@@ -2,6 +2,6 @@
 
 {
   system.activationScripts.postActivation.text = ''
-    ${pkgs.settings}/bin/settings accessibility --enable "Amethyst,Ghostty,Hammerspoon,Mac Mouse Fix Helper,Terminal,kitty"
+    ${pkgs.settings}/bin/settings accessibility --enable "Ghostty,Hammerspoon,Mac Mouse Fix Helper,Terminal,kitty"
   '';
 }
