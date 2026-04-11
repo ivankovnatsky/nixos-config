130af4b3bc307c09d263dbfdd819a2f32dcd0978
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-11 22:32:18 +0300

    home/nextdns: add a3 profile to nextdns-mgmt

--- home/nextdns.nix
+++ home/nextdns.nix
@@ -34,6 +34,9 @@ in
     nextdns-profile-lgphone = {
       key = "nextDnsProfileLgPhone";
     };
+    nextdns-profile-a3 = {
+      key = "nextDnsProfileA3";
+    };
   };
 
   local.services.nextdns-mgmt.pro = {
@@ -85,6 +88,15 @@ in
     enable = true;
     apiKeyFile = config.sops.secrets.nextdns-api-key.path;
     profileIdFile = config.sops.secrets.nextdns-profile-lgphone.path;
+    profileFile = ../configs/nextdns-profile.json;
+    vars = commonVars;
+    varsFiles = commonVarsFiles;
+  };
+
+  local.services.nextdns-mgmt.a3 = {
+    enable = true;
+    apiKeyFile = config.sops.secrets.nextdns-api-key.path;
+    profileIdFile = config.sops.secrets.nextdns-profile-a3.path;
     profileFile = ../configs/nextdns-profile.json;
     vars = commonVars;
     varsFiles = commonVarsFiles;
