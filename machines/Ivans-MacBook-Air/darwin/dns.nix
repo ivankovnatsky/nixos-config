e1ec705c9223c74883df044205c94aa044eac2f5
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-11 22:31:55 +0300

    machines/air/darwin/dns: init
    NextDNS DNS from sops via nextdns-dns module.

---
+++ machines/Ivans-MacBook-Air/darwin/dns.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  networking.knownNetworkServices = [
+    "AX88179A"
+    "Ethernet"
+    "Thunderbolt Ethernet Slot 0"
+    "Thunderbolt Bridge"
+    "Wi-Fi"
+  ];
+
+  local.nextdns-dns = {
+    enable = true;
+    machine = "Air";
+  };
+}
