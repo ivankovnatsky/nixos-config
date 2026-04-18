08a94a910216024629478fc9844dc3e87bd706b0
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-18 18:45:37 +0300

    system/sops-secrets: tighten secret permissions from 0444 to 0440

--- system/sops-secrets.nix
+++ system/sops-secrets.nix
@@ -5,11 +5,11 @@
 
   sops.secrets.external-domain = {
     key = "externalDomain";
-    mode = "0444"; # Readable by all services
+    mode = "0440";
   };
 
   sops.secrets.forgejo-user-name = {
     key = "forgejo/users/forgejoUser/name";
-    mode = "0444"; # Readable by all services
+    mode = "0440";
   };
 }
