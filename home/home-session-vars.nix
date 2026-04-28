a877b7a7db3aceea67433432462b0b94d749acae
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-28 15:37:59 +0300

    home/home-session-vars: export MTASKS_ROOT from notesPath

--- home/home-session-vars.nix
+++ home/home-session-vars.nix
@@ -1,7 +1,15 @@
 { config, ... }:
+let
+  notesPath =
+    if config.flags.notesPath != "" then
+      config.flags.notesPath
+    else
+      "${config.home.homeDirectory}/Notes";
+in
 {
   home.sessionVariables = {
     EDITOR = config.flags.editor;
     VISUAL = config.flags.editor;
+    MTASKS_ROOT = "${notesPath}/Tasks";
   };
 }
