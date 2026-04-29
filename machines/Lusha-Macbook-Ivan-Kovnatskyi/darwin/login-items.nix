a348aaeab67f91cfc2b6b790de606876a77592b9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-29 21:08:49 +0300

    machines/work/darwin/login-items: switch to declarative set

--- machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/login-items.nix
+++ machines/Lusha-Macbook-Ivan-Kovnatskyi/darwin/login-items.nix
@@ -2,6 +2,6 @@
 
 {
   system.activationScripts.postActivation.text = ''
-    ${pkgs.settings}/bin/settings login add "Hammerspoon,Mac Mouse Fix,Stats" || true
+    ${pkgs.settings}/bin/settings login set "Hammerspoon,Mac Mouse Fix,Stats" || true
   '';
 }
