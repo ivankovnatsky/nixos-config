c10a1451e16d59d3ed25b82a9d4342fae38904bc
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-01 11:56:11 +0300

    machines/air/home/dock: add cmux to dock

--- machines/Ivans-MacBook-Air/home/dock.nix
+++ machines/Ivans-MacBook-Air/home/dock.nix
@@ -65,6 +65,7 @@ in
       # Brew casks
       { path = "/Applications/kitty.app/"; }
       { path = "/Applications/Ghostty.app/"; }
+      { path = "/Applications/cmux.app/"; }
       { path = "/Applications/Firefox.app/"; }
       { path = "/Applications/Chromium.app/"; }
       { path = "/Applications/Google Chrome.app/"; }
