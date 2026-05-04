39ac03c10837f9f44b64d80c8b349bb05be315ce
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-04 16:26:07 +0300

    modules/home/notifications: add launchd service for battery notifs

---
+++ modules/home/notifications/default.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.local.services.notifications;
+in
+{
+  options.local.services.notifications = {
+    battery = {
+      enable = mkEnableOption "periodic battery state notifications to Discord";
+
+      interval = mkOption {
+        type = types.int;
+        default = 30 * 60;
+        description = "Interval in seconds between battery notifications (default: 30 minutes)";
+      };
+
+      runAtLoad = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Send a notification immediately when the launchd job is loaded.";
+      };
+
+      discordWebhookFile = mkOption {
+        type = types.str;
+        description = "Path to file containing the Discord webhook URL.";
+      };
+    };
+  };
+
+  config = mkIf cfg.battery.enable {
+    local.launchd.services.notifications-battery = {
+      enable = true;
+      keepAlive = false;
+      inherit (cfg.battery) runAtLoad;
+      waitForSecrets = true;
+
+      command =
+        let
+          script = pkgs.writeShellScript "notifications-battery-run" ''
+            set -e
+            exec ${pkgs.notifications}/bin/notifications battery \
+              --webhook-file "${cfg.battery.discordWebhookFile}"
+          '';
+        in
+        "${script}";
+
+      extraServiceConfig = {
+        StartInterval = cfg.battery.interval;
+      };
+    };
+  };
+}
