d69b4b384139ca4fc5c5ebb43d5eca319a17d9e8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-12 10:27:30 +0300

    machines/a3/nixos/storage-disk: unmount and clean /storage

--- machines/a3/nixos/storage-disk.nix
+++ machines/a3/nixos/storage-disk.nix
@@ -1,15 +1,1 @@
-{
-  boot.initrd.luks.devices."crypted-data" = {
-    device = "/dev/disk/by-uuid/a8787cc5-d5bb-4dfb-b4e0-2bccbffd9fe4";
-    crypttabExtraOpts = [ "tpm2-device=auto" ];
-  };
-
-  fileSystems."/storage" = {
-    device = "/dev/mapper/crypted-data";
-    fsType = "ext4";
-  };
-
-  systemd.tmpfiles.rules = [
-    "d /storage/data 0755 ivan users -"
-  ];
-}
+{ }
