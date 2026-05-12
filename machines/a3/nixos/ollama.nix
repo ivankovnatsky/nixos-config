427e933d228792cdf9d1edaeaf85a1720b9c5908
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-12 18:07:37 +0300

    m/a3/nixos/ollama: carry over OLLAMA_CONTEXT_LENGTH=8192 from mini

--- machines/a3/nixos/ollama.nix
+++ machines/a3/nixos/ollama.nix
@@ -5,6 +5,9 @@
     package = pkgs.nixpkgs-nixos-master-ollama.ollama-cuda;
     host = "0.0.0.0";
     openFirewall = true;
+    environmentVariables = {
+      OLLAMA_CONTEXT_LENGTH = "8192";
+    };
     loadModels = [
       "gemma3:27b"
       "gemma4:31b"
