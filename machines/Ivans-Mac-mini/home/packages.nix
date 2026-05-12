a0c0648fb6863f69616567ab52728996b11704a4
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-12 18:28:01 +0300

    machines/mini/home/packages: remove openclaw

--- machines/Ivans-Mac-mini/home/packages.nix
+++ machines/Ivans-Mac-mini/home/packages.nix
@@ -1,30 +1,19 @@
 {
-  inputs,
   pkgs,
-  system,
   ...
 }:
-let
-  steipeteTools = inputs.nix-steipete-tools.packages.${system};
-in
 {
-  home.packages =
-    (with pkgs; [
-      (python313.withPackages (
-        ps: with ps; [
-          grip
-          # markitdown
-        ]
-      ))
-      gh-repos-sync
-      macmon
-      music-export
-      sesh
-      treefmt
-    ])
-    ++ (with steipeteTools; [
-      peekaboo
-      sag
-      summarize
-    ]);
+  home.packages = with pkgs; [
+    (python313.withPackages (
+      ps: with ps; [
+        grip
+        # markitdown
+      ]
+    ))
+    gh-repos-sync
+    macmon
+    music-export
+    sesh
+    treefmt
+  ];
 }
