5abd3f1d50ad3adde7dd3dadef9c8a15e7921fb4
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-12 15:28:51 +0300

    machines/a3/nixos/storage-disk: restore with new 4TB nvme1n1 LUKS UUID

--- machines/a3/nixos/storage-disk.nix
+++ machines/a3/nixos/storage-disk.nix
@@ -1,1 +1,15 @@
-{ }
+{
+  boot.initrd.luks.devices."crypted-storage" = {
+    device = "/dev/disk/by-uuid/8b23078c-481c-410f-a90c-88348b982ec1";
+    crypttabExtraOpts = [ "tpm2-device=auto" ];
+  };
+
+  fileSystems."/storage" = {
+    device = "/dev/mapper/crypted-storage";
+    fsType = "ext4";
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /storage/data 0755 ivan users -"
+  ];
+}
