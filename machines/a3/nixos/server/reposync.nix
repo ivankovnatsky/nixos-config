465d7e6744ce7c06c37bd0da4d746df16a2a1166
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-13 11:17:31 +0300

    machines/a3/nixos/server/reposync: remove openclaw/openclaw

--- machines/a3/nixos/server/reposync.nix
+++ machines/a3/nixos/server/reposync.nix
@@ -8,13 +8,6 @@
       branch = "main";
     }
     {
-      path = "${config.users.users.${username}.home}/Sources/github.com/openclaw/openclaw";
-      remote = "origin";
-      remoteUrl = "https://github.com/openclaw/openclaw.git";
-      branch = "main";
-      syncMode = "pull-only";
-    }
-    {
       path = "${config.users.users.${username}.home}/.openclaw/workspace";
       remote = "origin";
       remoteUrl = "https://forgejo.@domain@/@username@/workspace.git";
