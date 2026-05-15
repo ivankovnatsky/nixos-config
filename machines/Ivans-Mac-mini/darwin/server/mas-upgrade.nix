975de10f5d1449a965fe4c6edc0b10bbff4d462a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-15 11:36:32 +0300

    m/mini/drw/srv/mas-upgrade: run mas as user ivan to fix sudo uid error

--- machines/Ivans-Mac-mini/darwin/server/mas-upgrade.nix
+++ machines/Ivans-Mac-mini/darwin/server/mas-upgrade.nix
@@ -3,7 +3,7 @@
   system.activationScripts.postActivation.text = ''
     if [ "$(date +%d)" = "15" ]; then
       echo "Upgrading App Store apps..."
-      ${pkgs.mas}/bin/mas upgrade || true
+      sudo -u ivan ${pkgs.mas}/bin/mas upgrade || true
     fi
   '';
 }
