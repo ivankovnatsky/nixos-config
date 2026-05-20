5acc3d4a70b58830fc0773ab5de9612a5605b5db
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-20 18:20:59 +0300

    machines/a3: remove unused desktop and home modules
    gamescope-session mangohud gamemode and gnome were never imported

--- machines/a3/nixos/desktop/gamemode.nix
+++
@@ -1,4 +0,0 @@
-{
-  # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/nixos/modules/programs/gamemode.nix
-  programs.gamemode.enable = true;
-}
--- machines/a3/nixos/desktop/gnome.nix
+++
@@ -1,5 +0,0 @@
-{
-  # Disable GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-}
