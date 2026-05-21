5ab9cd555506bea71e52eb819e1fec956150ab95
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-21 12:57:46 +0300

    packages/nix-gc: remove no-op home-manager gc
    HM as nix-darwin/NixOS module no longer creates numbered per-user
    profile generations (enableLegacyProfileManagement defaults to false),
    so home-manager generations is empty and the loop was a no-op.

--- packages/nix-gc/nix-gc.sh
+++ packages/nix-gc/nix-gc.sh
@@ -1,32 +1,5 @@
 #!/usr/bin/env bash
 
-# Since home-manager does not support nix gc of it's generations:
-#
-# * https://github.com/nix-community/home-manager/issues/4204#issue-1790502052
-# * https://github.com/nix-community/home-manager/issues/3450
-#
-# We can clean it up combining two commands:
-#
-# 1.
-#
-# ```console
-# ❯ home-manager generations
-# 2023-10-18 12:39 : id 1163 -> /nix/store/jhnjszgsni1a94aff7f31jj3hzg7n757-home-manager-generation
-# ...
-# 2023-10-12 19:20 : id 1144 -> /nix/store/44la87nfymkvczy6lnfjz8i78r9d6fgc-home-manager-generation
-#
-# for g in {1144..1162};do home-manager remove-generations $g;done
-# ```
-#
-# 2.
-#
-# ```console
-# nix-collect-garbage -d
-# ```
-#
-# nix gc is the second command, because it will clean it up home-manager
-# generations once we delete them.
-
 # Disable pager globally.
 export PAGER=""
 
@@ -44,26 +17,4 @@ nix_gc() {
   sudo nix-collect-garbage -d
 }
 
-home_manager_gc() {
-  local generations
-  local current_generation
-
-  # Get and show the current (first) generation
-  current_generation=$(home-manager generations | head -n 1)
-  echo "Current home-manager generation:"
-  echo "$current_generation"
-  echo
-
-  # Get all generations except the current one
-  generations=$(home-manager generations | tail -n +2 | awk '{print $5}')
-  if [[ -n $generations ]]; then
-    for g in $generations; do
-      home-manager remove-generations "$g"
-    done
-  else
-    echo "No home-manager generations to remove"
-  fi
-}
-
-home_manager_gc
 nix_gc
