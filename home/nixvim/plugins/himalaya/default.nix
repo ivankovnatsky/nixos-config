0a36ef25577c764e74db1a3a2c445d46c9e58729
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-23 13:42:49 +0300

    home/nixvim/plugins/himalaya: add knownasnaffy/himalaya.nvim

--- home/nixvim/plugins/himalaya/default.nix
+++ home/nixvim/plugins/himalaya/default.nix
@@ -2,8 +2,18 @@
 
 {
   programs.nixvim = {
-    extraPlugins = with pkgs.vimPlugins; [
-      himalaya-vim
+    extraPlugins = [
+      pkgs.vimPlugins.himalaya-vim
+      (pkgs.vimUtils.buildVimPlugin rec {
+        pname = "himalaya.nvim";
+        version = "d56a177e6ed6152da02d051e39d67ecdcf0de6ce";
+        src = pkgs.fetchFromGitHub {
+          owner = "knownasnaffy";
+          repo = "himalaya.nvim";
+          rev = version;
+          hash = "sha256-kamZKE/VpMPenc67JtjS/jsmNrYx7VgxJAuBbvKJ1aM=";
+        };
+      })
     ];
   };
 }
