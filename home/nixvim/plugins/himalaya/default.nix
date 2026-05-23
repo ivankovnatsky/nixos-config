05795502744371dc6ebb13ab95692cec0e36df03
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-23 13:41:56 +0300

    home/nixvim/plugins/himalaya: add himalaya.nvim plugin
    Add knownasnaffy/himalaya.nvim (Neovim interface for Himalaya CLI)
    alongside the existing himalaya-vim and vim-himalaya-ui plugins.

--- home/nixvim/plugins/himalaya/default.nix
+++ home/nixvim/plugins/himalaya/default.nix
@@ -14,6 +14,16 @@
           hash = "sha256-usXHM6ISGoLPSQsT0lBaYX9y1bJXjPoRhNath/lKkGg=";
         };
       })
+      (buildVimPlugin rec {
+        pname = "himalaya.nvim";
+        version = "d56a177e6ed6152da02d051e39d67ecdcf0de6ce";
+        src = pkgs.fetchFromGitHub {
+          owner = "knownasnaffy";
+          repo = "himalaya.nvim";
+          rev = version;
+          hash = "sha256-kamZKE/VpMPenc67JtjS/jsmNrYx7VgxJAuBbvKJ1aM=";
+        };
+      })
     ];
   };
 }
