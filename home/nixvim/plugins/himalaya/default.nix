3c483f18aef7e79ec88f1b9791f0d1ff7cf3e0c8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-23 13:40:43 +0300

    home/nixvim/plugins/himalaya: init
    Bundle pimalaya/himalaya-vim (in nixpkgs) and aliyss/vim-himalaya-ui
    (pinned via buildVimPlugin) for use with the himalaya CLI.

---
+++ home/nixvim/plugins/himalaya/default.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+  programs.nixvim = {
+    extraPlugins = with pkgs.vimUtils; [
+      pkgs.vimPlugins.himalaya-vim
+      (buildVimPlugin rec {
+        pname = "vim-himalaya-ui";
+        version = "7f84b8174aa781af7bc3c4b9c7e4bb39e20475e2";
+        src = pkgs.fetchFromGitHub {
+          owner = "aliyss";
+          repo = "vim-himalaya-ui";
+          rev = version;
+          hash = "sha256-usXHM6ISGoLPSQsT0lBaYX9y1bJXjPoRhNath/lKkGg=";
+        };
+      })
+    ];
+  };
+}
