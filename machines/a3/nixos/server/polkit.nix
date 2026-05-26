a9883d0aa00bf5a427731b775fe13b3a84e0befd
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-26 18:50:13 +0300

    machines/a3/nixos/server/polkit: require admin auth for poweroff

--- machines/a3/nixos/server/polkit.nix
+++ machines/a3/nixos/server/polkit.nix
@@ -7,7 +7,7 @@
         (action.id == "org.freedesktop.login1.power-off" ||
          action.id == "org.freedesktop.login1.power-off-multiple-sessions")
       ) {
-        return polkit.Result.YES;
+        return polkit.Result.AUTH_ADMIN_KEEP;
       }
     });
   '';
