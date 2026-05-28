90e92fa75155a1e228612af7c2e108d4fe73b93a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-05-28 15:10:43 +0300

    home/nixvim/plugins/himalaya: use `pkgs.vimPlugins.himalaya-vim`

--- home/nixvim/plugins/himalaya/default.nix
+++ home/nixvim/plugins/himalaya/default.nix
@@ -3,18 +3,7 @@
 {
   programs.nixvim = {
     extraPlugins = [
-      pkgs.vimPlugins.nui-nvim
-      (pkgs.vimUtils.buildVimPlugin rec {
-        pname = "himalaya.nvim";
-        version = "d56a177e6ed6152da02d051e39d67ecdcf0de6ce";
-        src = pkgs.fetchFromGitHub {
-          owner = "knownasnaffy";
-          repo = "himalaya.nvim";
-          rev = version;
-          hash = "sha256-kamZKE/VpMPenc67JtjS/jsmNrYx7VgxJAuBbvKJ1aM=";
-        };
-        dependencies = [ pkgs.vimPlugins.nui-nvim ];
-      })
+      pkgs.vimPlugins.himalaya-vim
     ];
   };
 }
