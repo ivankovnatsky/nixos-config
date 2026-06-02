200ce5ea689882adbfffd31dfb8a1e7f72fad22c
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-02 15:11:39 +0300

    m/a3/nixos/desktop: add udev rule to auto-authorize TB devices
    NixOS does not authorize Thunderbolt devices by default. Without this rule
    the CalDigit dock PCIe tunneling (display, ethernet) never activates even
    with thunderbolt/typec_ucsi/ucsi_acpi modules loaded.

--- machines/a3/nixos/desktop/thunderbolt.nix
+++ machines/a3/nixos/desktop/thunderbolt.nix
@@ -8,4 +8,10 @@
     "typec_ucsi"
     "ucsi_acpi"
   ];
+
+  # NixOS doesn't auto-authorize TB devices; without this the CalDigit dock's
+  # PCIe tunneling (display, ethernet) never activates even with modules loaded.
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="thunderbolt", ATTRS{iommu_dma_protection}=="1", ATTR{authorized}=="0", ATTR{authorized}="1"
+  '';
 }
