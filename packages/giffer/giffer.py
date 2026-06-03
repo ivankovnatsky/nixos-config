7d86e392b817a9d54d253faf44ad7a4d3db3a3f9
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-03 12:40:13 +0300

    packages/giffer: drop

--- packages/giffer/giffer.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""giffer - A wrapper for yt-dlp and gallery-dl with optional video splitting."""
-
-from cli import cli
-
-if __name__ == "__main__":
-    cli(prog_name="giffer")
