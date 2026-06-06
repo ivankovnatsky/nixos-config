012566bf50f4acdb50d57349a54652929452d8fa
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-06 11:36:50 +0300

    packages/nvim-find-edit: use configured nvim instead of bare neovim
    Prepending pkgs.neovim shadowed the nixvim nvim, loading its
    init.lua without plugins on the runtimepath.

--- packages/nvim-find-edit/default.nix
+++ packages/nvim-find-edit/default.nix
@@ -4,7 +4,6 @@ pkgs.writeShellScriptBin "nvim-find-edit" ''
   export PATH="${
     pkgs.lib.makeBinPath [
       pkgs.ripgrep
-      pkgs.neovim
     ]
   }:$PATH"
   exec ${pkgs.bash}/bin/bash ${./nvim-find-edit.sh} "$@"
