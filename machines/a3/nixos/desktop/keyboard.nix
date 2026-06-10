5b62c41c46055ae3cc868b1aa664c2ff2222a542
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-10 21:16:12 +0300

    machines/a3/nixos/desktop/keyboard: untag Ergodox as joystick
    Sekiro/DS2 grab the first enumerated joystick and ignore the real
    controller; the Ergodox System Control interface was tagged
    ID_INPUT_JOYSTICK=1, forcing a keyboard unplug to play. Clear the tag
    via udev rule; keyboard input is unaffected.

--- machines/a3/nixos/desktop/keyboard.nix
+++ machines/a3/nixos/desktop/keyboard.nix
@@ -1,3 +1,13 @@
 {
   hardware.keyboard.zsa.enable = true;
+
+  # Ergodox EZ exposes a "System Control" HID interface that udev tags as a
+  # joystick (ID_INPUT_JOYSTICK=1). Sekiro/DS2 grab the first joystick they
+  # enumerate and ignore the real controller, forcing a keyboard unplug to
+  # play. Clear the tag -- normal keyboard input is unaffected.
+  # https://steamcommunity.com/app/814380/discussions/0/592908590021573765/
+  # https://www.reddit.com/r/linux_gaming/comments/1s6owdn/controller_not_working_in_sekiro_shadows_die_twice/
+  services.udev.extraRules = ''
+    SUBSYSTEM=="input", ATTRS{idVendor}=="3297", ATTRS{idProduct}=="2030", ENV{ID_INPUT_JOYSTICK}=""
+  '';
 }
