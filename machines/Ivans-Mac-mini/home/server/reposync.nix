1740f5282b2d423c4acea7adfa8deb5e67018c9a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-15 17:40:48 +0300

    machines/mini/home/server/reposync: add back notes

--- machines/Ivans-Mac-mini/home/server/reposync.nix
+++ machines/Ivans-Mac-mini/home/server/reposync.nix
@@ -1,4 +1,11 @@
 { config, ... }:
+let
+  forgejoIdentity = {
+    name = "@username@";
+    email = "@username@@@domain@";
+    signingKey = "@username@@@domain@";
+  };
+in
 {
   sops.secrets.discord-webhook-reposync = {
     key = "discord/webhooks/monitoringRepoSync";
@@ -10,26 +17,34 @@
     domainFile = config.sops.secrets.external-domain.path;
     usernameFile = config.sops.secrets.forgejo-user-name.path;
     discordWebhookFile = config.sops.secrets.discord-webhook-reposync.path;
-  };
 
-  local.services.reposync.repositories = [
-    {
-      name = "home";
-      path = config.home.homeDirectory;
-      remote = "origin";
-      remoteUrl = "https://forgejo.@domain@/@username@/home.git";
-      branch = "main";
-      identity = {
-        name = "@username@";
-        email = "@username@@@domain@";
-        signingKey = "@username@@@domain@";
-      };
-    }
-    {
-      path = "${config.flags.homeWorkPath}/Sources/github.com/ivankovnatsky/nix-config";
-      remote = "origin";
-      remoteUrl = "https://github.com/ivankovnatsky/nix-config.git";
-      branch = "main";
-    }
-  ];
+    repositories = [
+      {
+        name = "home";
+        path = config.home.homeDirectory;
+        remote = "origin";
+        remoteUrl = "https://forgejo.@domain@/@username@/home.git";
+        branch = "main";
+        identity = {
+          name = "@username@";
+          email = "@username@@@domain@";
+          signingKey = "@username@@@domain@";
+        };
+      }
+      {
+        path = "${config.flags.homeWorkPath}/Sources/github.com/ivankovnatsky/nix-config";
+        remote = "origin";
+        remoteUrl = "https://github.com/ivankovnatsky/nix-config.git";
+        branch = "main";
+      }
+      {
+        path = "${config.home.homeDirectory}/Notes";
+        remote = "origin";
+        remoteUrl = "https://forgejo.@domain@/@username@/notes.git";
+        branch = "main";
+        autoStage = true;
+        identity = forgejoIdentity;
+      }
+    ];
+  };
 }
