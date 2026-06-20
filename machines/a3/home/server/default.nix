d991cc7bd0ce905aa4e32cccfc795cddafe3a40e
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-20 08:49:00 +0300

    machines/a3/home/server: enable 21:00 digest flush

--- machines/a3/home/server/default.nix
+++ machines/a3/home/server/default.nix
@@ -1,10 +1,12 @@
 {
   imports = [
     ../../../../modules/home/launchd
+    ../../../../modules/home/notifications
     ../../../../modules/home/reposync
     ./arr-mgmt.nix
     ./beszel-mgmt.nix
     ./miniserve.nix
+    ./notifications.nix
     ./reposync.nix
     ./syncthing.nix
     ./uptime-kuma.nix
