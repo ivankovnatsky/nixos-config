1adcf91e539a86dd721349cdfdd64ab6602cff6b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-20 08:49:00 +0300

    machines/mini/home/server: enable 21:00 digest flush

--- machines/Ivans-Mac-mini/home/server/default.nix
+++ machines/Ivans-Mac-mini/home/server/default.nix
@@ -1,8 +1,10 @@
 {
   imports = [
     ../../../../modules/home/launchd
+    ../../../../modules/home/notifications
     ../../../../modules/home/tools
     ../../../../modules/home/unison
+    ./notifications.nix
     ./reposync.nix
     ./syncthing-mgmt.nix
     ./unison.nix
---
+++ machines/Ivans-Mac-mini/home/server/notifications.nix
@@ -0,0 +1,6 @@
+{
+  local.services.notifications.digest = {
+    enable = true;
+    at = "21:00";
+  };
+}
