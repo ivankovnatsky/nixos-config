1581e5353a56d08f2e1ff77505d895eecd86721a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-22 14:20:12 +0300

    packages/go-lint-wrapper: use .go GOPATH unconditionally

--- packages/go-lint-wrapper/go-lint-wrapper.sh
+++ packages/go-lint-wrapper/go-lint-wrapper.sh
@@ -6,10 +6,7 @@
 set -euo pipefail
 
 if [[ -z "${GOPATH:-}" ]]; then
-  case "$(uname -s)" in
-  Linux) export GOPATH="$HOME/.go" ;;
-  *) export GOPATH="$HOME/go" ;;
-  esac
+  export GOPATH="$HOME/.go"
 fi
 export GO111MODULE=off
 
