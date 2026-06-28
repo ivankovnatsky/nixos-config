fa517fa909d56f36f3be54c67ee16b681158f9a8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-06-28 13:53:11 +0300

    home/ranger: enable image and video previews
    Add kitty image previews and point preview_script at pkgs.ranger-scope
    for video thumbnails; add ffmpegthumbnailer.

--- home/ranger.nix
+++ home/ranger.nix
@@ -1,10 +1,16 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.ranger ];
+  home.packages = [
+    pkgs.ranger
+    pkgs.ffmpegthumbnailer
+  ];
   home.file = {
     ".config/ranger/rc.conf" = {
       text = ''
         set show_hidden true
+        set preview_images true
+        set preview_images_method kitty
+        set preview_script ${pkgs.ranger-scope}/scope.sh
       '';
     };
   };
