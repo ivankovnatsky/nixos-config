2e1cd8d3383f45176d2f98d30c29bf7569fdf81d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-07 12:38:11 +0300

    m/a3/*/storage0-disk: add storage0 backup bind mount and tmpfiles rules

--- machines/a3/nixos/server/storage0-disk.nix
+++ machines/a3/nixos/server/storage0-disk.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   boot.initrd.luks.devices."crypted-storage0" = {
     device = "/dev/disk/by-uuid/5fcf939d-159c-4d28-b748-4e83281422af";
@@ -9,7 +11,43 @@
     fsType = "ext4";
   };
 
+  fileSystems."/storage/data/backup" = {
+    device = "/storage0/data/backup";
+    fsType = "none";
+    options = [
+      "bind"
+      "x-systemd.requires-mounts-for=/storage0/data/backup"
+      "x-systemd.requires=storage-data-backup-bind-source.service"
+      "x-systemd.after=storage-data-backup-bind-source.service"
+    ];
+  };
+
+  systemd.services.storage-data-backup-bind-source = {
+    description = "Prepare /storage/data/backup bind mount source";
+    requiredBy = [ "storage-data-backup.mount" ];
+    before = [ "storage-data-backup.mount" ];
+    requires = [
+      "storage.mount"
+      "storage0.mount"
+    ];
+    after = [
+      "storage.mount"
+      "storage0.mount"
+    ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      ${pkgs.coreutils}/bin/install -d -o ivan -g users -m 0755 /storage0/data/backup
+      ${pkgs.coreutils}/bin/install -d -o ivan -g users -m 0755 /storage0/data/backup/Machines
+      ${pkgs.coreutils}/bin/install -d -o root -g users -m 0755 /storage/data/backup
+    '';
+  };
+
   systemd.tmpfiles.rules = [
     "d /storage0/data 0755 root users -"
+    # miniserve (home-manager user unit) uploads here; needs ivan-owned dir.
+    "d /storage0/data/backup 0755 ivan users -"
+    "d /storage0/data/backup/Machines 0755 ivan users -"
+    # rclone sync target for iCloud Drive contents.
+    "d /storage0/data/iclouddrive 0755 ivan users -"
   ];
 }
