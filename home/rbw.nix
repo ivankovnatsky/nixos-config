753a5bd8f50257edff063b927c7f0c0986299272
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-07 10:45:30 +0300

    home/rbw: fix secret config

--- home/rbw.nix
+++ home/rbw.nix
@@ -1,20 +1,33 @@
 {
   config,
+  lib,
   pkgs,
-  super,
   ...
 }:
 
-{
-  programs.rbw = {
-    enable = true;
+let
+  inherit (pkgs.stdenv.targetPlatform) isDarwin;
 
-    settings = {
-      email = "${config.secrets.email}";
-      lock_timeout = 2419200;
-      inherit (pkgs) pinentry;
-    };
+  configFile = "${config.home.homeDirectory}/Library/Application Support/rbw/config.json";
+  pinentryPackage = if isDarwin then pkgs.pinentry_mac else pkgs.pinentry-tty;
+in
+{
+  programs.rbw.enable = true;
+
+  sops.templates."rbw-config.json".content = builtins.toJSON {
+    email = config.sops.placeholder.email;
+    lock_timeout = 2419200;
+    pinentry = lib.getExe pinentryPackage;
   };
 
-  inherit (super) secrets;
+  home.activation.linkRbwConfig =
+    lib.hm.dag.entryAfter
+      [
+        "writeBoundary"
+        "sops-nix"
+      ]
+      ''
+        run mkdir -p "$(dirname "${configFile}")"
+        run ln -sf ${config.sops.templates."rbw-config.json".path} "${configFile}"
+      '';
 }
