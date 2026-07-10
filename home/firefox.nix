7e85c0418cf70e8997ddc3b1a62c9d680ddd4680
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-10 15:28:42 +0300

    hm/firefox: firefox: block notifs, hide bookmarks, disable pip

--- home/firefox.nix
+++ home/firefox.nix
@@ -20,6 +20,17 @@
   # Block geolocation prompts globally (2 = block)
   "permissions.default.geo" = 2;
 
+  # Block notification prompts globally (2 = block)
+  "permissions.default.desktop-notification" = 2;
+
   # Never offer to save passwords (using Bitwarden instead)
   "signon.rememberSignons" = false;
+
+  # Never show the bookmarks toolbar
+  "browser.toolbars.bookmarks.visibility" = "never";
+
+  # Disable picture-in-picture hover toggle on videos
+  "media.videocontrols.picture-in-picture.enabled" = false;
+  "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
+  "media.videocontrols.picture-in-picture.video-toggle.has-used" = false;
 }
