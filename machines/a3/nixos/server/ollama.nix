3b28e547f30a7ceb911ebf653727dcb43365b7c6
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-10 11:39:28 +0300

    machines/a3/nixos/server/ollama: rename nixpkgs-nixos-master ref

--- machines/a3/nixos/server/ollama.nix
+++ machines/a3/nixos/server/ollama.nix
@@ -2,7 +2,7 @@
 {
   services.ollama = {
     enable = true;
-    package = pkgs.nixpkgs-nixos-master-ollama.ollama-cuda;
+    package = pkgs.nixpkgs-nixos-master.ollama-cuda;
     host = "0.0.0.0";
     openFirewall = true;
     environmentVariables = {
