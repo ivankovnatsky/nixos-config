47477bc4c396f80d698f1f81e1cc67cf4d00a2c6
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-12 12:03:57 +0300

    machines/mini/home/server: remove unison

--- machines/Ivans-Mac-mini/home/server/default.nix
+++ machines/Ivans-Mac-mini/home/server/default.nix
@@ -1,10 +1,8 @@
 {
   imports = [
-    ../../../../darwin/unison.nix
     ../../../../modules/home/launchd
     ../../../../modules/home/notifications
     ../../../../modules/home/tools
-    ../../../../modules/home/unison
     ./notifications.nix
     ./reposync.nix
     ./syncthing-mgmt.nix
