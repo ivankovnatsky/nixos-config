703da844f4a04ea81334659cec871781b033c98d
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-15 11:25:59 +0300

    packages/pass-fzf: use gopass entries

--- packages/pass-fzf/pass-fzf.sh
+++ packages/pass-fzf/pass-fzf.sh
@@ -2,15 +2,12 @@
 
 set -euo pipefail
 
-PASSWORD_STORE_DIR="${PASSWORD_STORE_DIR:-$HOME/.password-store}"
-
 selected=$(
-  find "$PASSWORD_STORE_DIR" -name "*.gpg" -type f |
-    sed "s|.*/${PASSWORD_STORE_DIR##*/}/||; s|\.gpg$||" |
+  gopass list --flat |
     fzf --prompt="Select item: " --height=40% --border
 )
 
 if [[ -n "$selected" ]]; then
   echo "$selected"
-  pass show -c "$selected"
+  gopass show --clip "$selected"
 fi
