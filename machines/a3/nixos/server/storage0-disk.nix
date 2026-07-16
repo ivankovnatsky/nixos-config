e810dfd15d87ed3225976de7b55986eebdae8ffd
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-16 17:42:08 +0300

    machines/a3/nixos/server/storage0-disk: remove `vault/`

--- machines/a3/nixos/server/storage0-disk.nix
+++ machines/a3/nixos/server/storage0-disk.nix
@@ -24,7 +24,6 @@
     "d /storage0/data 0755 root users -"
     # miniserve (home-manager user unit) uploads here; needs ivan-owned dir.
     "d /storage0/data/backup 0755 ivan users -"
-    "d /storage0/data/vault 0755 ivan users -"
     "d /storage0/data/.scripts 0755 ivan users -"
   ];
 }
