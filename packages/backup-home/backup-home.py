2ef1983c6f469e53017409adad681831dd2e7a03
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-24 11:15:36 +0300

    packages/backup-home: remove ungoogled ch path

--- packages/backup-home/backup-home.py
+++ packages/backup-home/backup-home.py
@@ -98,7 +98,6 @@ EXCLUDE_PATTERNS: dict[str, list[str]] = {
         "**/.cache/nix/**",
         "**/.cache/uv/archive-v0/**",
         "**/Library/Caches/Google/Chrome/**",
-        "**/Library/Caches/Ungoogled Chromium/Chrome/**",
         "**/Library/Caches/Firefox/Profiles/**",
         "**/Library/Caches/go-build/**",
         "**/Library/Caches/pypoetry/**",
@@ -133,7 +132,6 @@ EXCLUDE_PATTERNS: dict[str, list[str]] = {
     "app_data": [
         "**/Library/Application Support/Firefox/**",
         "**/Library/Application Support/Chromium/**",
-        "**/Library/Application Support/Ungoogled Chromium/**",
         "**/Library/Application Support/Google/Chrome/**",
         "**/Library/Application Support/Vivaldi/**",
         "**/Library/Application Support/Cursor/**",
