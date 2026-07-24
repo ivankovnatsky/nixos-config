b94bf1535e991d0a95a151f2da1fb3cd140df425
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-07-24 21:31:54 +0300

    home/ranger: set column ratios to match yazi

--- home/ranger.nix
+++ home/ranger.nix
@@ -7,6 +7,8 @@
   home.file = {
     ".config/ranger/rc.conf" = {
       text = ''
+        # ratio = parent, current, preview (matches yazi mgr.ratio)
+        set column_ratios 1,2,4
         set show_hidden true
         set preview_images true
         set preview_images_method kitty
