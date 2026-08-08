42ec3253e0d977858a6e3c328acc4343a8d8f507
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-08-08 20:48:53 +0300

    m/mini/drw/desktop/losslessswitcher: add losslessswitcher preferences

---
+++ machines/Ivans-Mac-mini/darwin/desktop/losslessswitcher.nix
@@ -0,0 +1,7 @@
+{
+  system.defaults.CustomUserPreferences."com.vincent-neo.LosslessSwitcher" = {
+    "com.vincent-neo.LosslessSwitcher-Key-UserPreferIconStatusBarItem" = false;
+    "com.vincent-neo.LosslessSwitcher-Key-BitDepthDetection" = true;
+    "PreferSampleRateMultiples" = false;
+  };
+}
