aa8e0aff87fd265588fb2e205a31475af9b158b6
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-08-20 18:12:09 +0300

    home/home-session-vars: add OLLAMA_BASE_URL

--- home/home-session-vars.nix
+++ home/home-session-vars.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 {
   home.sessionVariables = {
+    OLLAMA_BASE_URL = "http://${config.inventory.a3Ip}:11434/v1";
     EDITOR = config.flags.editor;
     VISUAL = config.flags.editor;
     LESS = "-R";
